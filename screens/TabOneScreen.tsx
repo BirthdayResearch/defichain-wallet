0bf11085e36057fa95553dff8a56497b2f51ba98
Fuxing Loh <git@fuxing.dev>
2021-04-01 18:43:03 +0800

    setup internationalisation

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -4,12 +4,15 @@ import EditScreenInfo from '../components/EditScreenInfo'
 import { Text, View } from '../components/Themed'
 import tailwind from 'tailwind-rn'
 import IconDFI from '../components/icons/IconDFI'
+import { translate } from '../translations'
 
 export default function TabOneScreen (): JSX.Element {
   return (
     <View style={tailwind('flex-1 items-center justify-center')}>
       <IconDFI />
-      <Text style={tailwind('text-xl font-bold')}>Tab One</Text>
+      <Text style={tailwind('text-xl font-bold')}>
+        {translate('screens/TabOneScreen', 'Tab One TEST')}
+      </Text>
       <View style={tailwind('w-4/5 h-px my-8')} lightColor='#eee' darkColor='rgba(255,255,255,0.1)' />
       <EditScreenInfo path='/screens/TabOneScreen.tsx' />
     </View>
---
+++ translations/index.ts
@@ -0,0 +1,52 @@
+import i18n from 'i18n-js'
+import * as Localization from 'expo-localization'
+
+import zh_hans from './zh-Hans'
+
+/**
+ * Internationalisation for DeFi Fiber, DeFi Blockchain Light Wallet.
+ *
+ * Translates matrix should be keyed as such:
+ *
+ * en | file location        | text/key | translation
+ * ---|----------------------|----------|--------------
+ * zh | screens/ScreenName   | Hello    | 你好
+ * zh | screens/ScreenName   | Bye      | 再见
+ * de | screens/ScreenName   | Bye      | Tschüss
+ *
+ * In this matrix, the key will always be the english representation of the text. Hence the 'key' it must be verbose.
+ * Similarly following the Android Native String resources design.
+ *
+ * The benefits of this design allows:
+ *
+ * 1. Improved developer experience:
+ * The components/screens will always have up to date english presentation of what you see on the app.
+ * This allow developers with all levels of experience, to CMD/CTRL + SHIFT + F and find responsible code easily.
+ *
+ * 2. Improved translation experience:
+ * Translator will be translating from the english text instead of 'keys' which is suboptimal.
+ * When the english text change [context/meaning], it will always require all translations to be updated.
+ *
+ * @see translate
+ */
+export function initI18n (): void {
+  i18n.translations = {
+    en: {},
+    'zh-Hans': zh_hans
+  }
+
+  i18n.locale = Localization.locale
+  i18n.fallbacks = true
+  i18n.missingBehaviour = 'guess'
+}
+
+/**
+ * @param path translation path, can follow file location
+ * @param text english text for internationalisation
+ */
+export function translate (path: string, text: string): string {
+  return i18n.translate(`${path}.${text}`)
+}
+
+// TODO(fuxingloh): add CI workflow to find missing translation and report it
+//  simple regex will be able to detect. ideally to be done in TypeScript for translations comparison
