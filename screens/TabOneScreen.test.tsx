314d492aa89ffe4110aa36921fadd5cee5543504
Fuxing Loh <git@fuxing.dev>
2021-04-01 21:10:47 +0800

    setup unit test

---
+++ screens/TabOneScreen.test.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import { fireEvent, render } from 'react-native-testing-library'
+
+import TabOneScreen from './TabOneScreen'
+
+it('<TabOneScreen/> snapshot', () => {
+  const tree = render(<TabOneScreen />).toJSON()
+
+  expect(tree).toMatchSnapshot()
+})
+
+it('<TabOneScreen/> click should increment counter', () => {
+  const { getByText } = render(<TabOneScreen />)
+
+  expect(getByText('Count: 0')).toBeTruthy()
+  fireEvent.press(getByText('Click'))
+  expect(getByText('Count: 2')).toBeTruthy()
+})
