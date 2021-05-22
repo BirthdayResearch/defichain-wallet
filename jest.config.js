99848537dc008907db2c4588bd2285dbf5b97b1f
Fuxing Loh <git@fuxing.dev>
2021-05-22 17:21:04 +0800

    ignore cypress for unit test

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'jest-expo',
+  testPathIgnorePatterns: [
+    'cypress/.*'
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!**/babel.config.js',
+    '!**/jest.setup.js'
+  ]
+}
