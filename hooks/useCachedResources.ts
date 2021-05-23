22d6c045e938f199513af179e9d1ac2cad931677
Fuxing Loh <git@fuxing.dev>
2021-05-23 17:01:24 +0800

    fixed lint issue

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -29,7 +29,7 @@ export default function useCachedResources (): boolean {
       }
     }
 
-    void loadResourcesAndDataAsync()
+    loadResourcesAndDataAsync()
   }, [])
 
   return isLoadingComplete
