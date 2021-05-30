678dcd4a6137333cb25e3af38713388220253a9f
Fuxing Loh <git@fuxing.dev>
2021-05-31 00:08:16 +0800

    added readable-stream alias

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      {
+        aliases: { stream: 'readable-stream' }
+      }
+    ]
   }
 }
