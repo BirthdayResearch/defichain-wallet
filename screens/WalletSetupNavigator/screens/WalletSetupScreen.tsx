bc1d4d4773d31188b650b7c6ec118f8e67bb7133
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-06-10 15:30:58 +0800

    Update WalletSetupScreen.tsx

--- screens/WalletSetupNavigator/screens/WalletSetupScreen.tsx
+++ screens/WalletSetupNavigator/screens/WalletSetupScreen.tsx
@@ -2,24 +2,13 @@ import { Text, View } from '../../../components/Themed'
 import tailwind from 'tailwind-rn'
 import { translate } from '../../../translations'
 import * as React from 'react'
-import { PlaygroundAction } from '../../../playground/Playground.Action'
-import { useWalletAPI } from '../../../hooks/wallet/WalletAPI'
-import { useDispatch } from 'react-redux'
 
 export function WalletSetupScreen (): JSX.Element {
-  const WalletAPI = useWalletAPI()
-  const dispatch = useDispatch()
   return (
     <View style={tailwind('flex-1 items-center justify-center')}>
       <Text style={tailwind('text-xl font-bold')} testID='wallet_setup'>
         {translate('screens/WalletSetupScreen', 'Wallet Setup')}
       </Text>
-      {/* Temporary Button to create wallet on native app */}
-      <PlaygroundAction
-        testID='playground_wallet_random'
-        title='Create Wallet'
-        onPress={() => WalletAPI.randomMnemonic(dispatch)}
-      />
     </View>
   )
 }
