d792010ca895a43756aac30798c3caa058c4665c
Fuxing Loh <git@fuxing.dev>
2021-06-11 12:10:31 +0800

    updated snapshot due to test failure removed a key component to include in another PR

--- screens/WalletNavigator/screens/WalletOnboarding.tsx
+++ screens/WalletNavigator/screens/WalletOnboarding.tsx
@@ -1,17 +1,14 @@
 import * as React from 'react'
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import tailwind from 'tailwind-rn'
 import { Ionicons } from '@expo/vector-icons'
 import { translate } from '../../../translations'
-import { PrimaryColor } from '../../../constants/Theme'
-import { useNavigation } from '@react-navigation/native'
 import { useWalletAPI } from '../../../hooks/wallet/WalletAPI'
 import { useDispatch } from 'react-redux'
 
 export function WalletOnboarding (): JSX.Element {
   const WalletAPI = useWalletAPI()
   const dispatch = useDispatch()
-  const navigator = useNavigation()
 
   function onDebugPress (): void {
     // TODO(fuxingloh): this should only be available in debug mode
@@ -32,36 +29,7 @@ export function WalletOnboarding (): JSX.Element {
         </Text>
       </View>
 
-      <View style={tailwind('mt-8')}>
-        <WalletOptionRow
-          onPress={() => navigator.navigate('WalletMnemonicCreate')}
-          text='Create new mnemonic wallet' icon='time'
-        />
-        <View style={tailwind('h-px bg-gray-100')} />
-        <WalletOptionRow
-          onPress={() => navigator.navigate('WalletMnemonicRestore')}
-          text='Restore mnemonic wallet' icon='document-text'
-        />
-      </View>
+      <View style={tailwind('mt-8')} />
     </ScrollView>
-  )
-}
-
-function WalletOptionRow (props: { text: string, icon: string, onPress: () => void }): JSX.Element {
-  return (
-    <TouchableOpacity
-      onPress={props.onPress}
-      style={tailwind('flex-row items-center justify-between px-4 bg-white')}
-    >
-      <View style={tailwind('flex-row items-center')}>
-        <Ionicons name={props.icon as any} size={18} color={PrimaryColor} />
-        <Text style={tailwind('font-medium ml-3 py-4')}>
-          {props.text}
-        </Text>
-      </View>
-      <View>
-        <Ionicons name='chevron-forward' size={20} />
-      </View>
-    </TouchableOpacity>
   )
 }
