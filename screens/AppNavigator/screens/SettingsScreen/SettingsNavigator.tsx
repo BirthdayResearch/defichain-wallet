fdc5b8e122824c63711bf5464a87bf6d68bc7e92
thedoublejay <josejaimecareers@gmail.com>
2021-06-11 16:03:40 +0800

    Help Screen

--- screens/AppNavigator/screens/SettingsScreen/SettingsNavigator.tsx
+++ screens/AppNavigator/screens/SettingsScreen/SettingsNavigator.tsx
@@ -1,7 +1,13 @@
+import { useNavigation } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { TouchableOpacity } from 'react-native'
+import tailwind from 'tailwind-rn'
+import { PrimaryColor } from '../../../../constants/Theme'
 import { translate } from '../../../../translations'
 import * as React from 'react'
+import { HelpScreen } from '../HelpScreen/HelpScreen'
 import { SettingsScreen } from './SettingsScreen'
+import { Ionicons } from '@expo/vector-icons'
 
 export interface SettingsParamList {
   SettingsScreen: undefined
@@ -17,8 +23,27 @@ export function SettingsNavigator (): JSX.Element {
       <SettingsStack.Screen
         name='settings'
         component={SettingsScreen}
-        options={{ headerTitle: translate('screens/SettingsScreen', 'Settings') }}
+        options={{
+          headerTitle: translate('screens/SettingsScreen', 'Settings'),
+          headerRight: () => <HelpButton />
+        }}
+      />
+      <SettingsStack.Screen
+        name='help'
+        component={HelpScreen}
+        options={{
+          headerTitle: translate('screens/HelpScreen', 'Help')
+        }}
       />
     </SettingsStack.Navigator>
+  )
+}
+
+function HelpButton (): JSX.Element {
+  const navigation = useNavigation()
+  return (
+    <TouchableOpacity style={tailwind('m-2')} onPress={() => navigation.navigate('help')}>
+      <Ionicons name='help-circle-outline' size={28} color={PrimaryColor} />
+    </TouchableOpacity>
   )
 }
