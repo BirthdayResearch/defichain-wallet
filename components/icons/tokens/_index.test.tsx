39e6c7e25da94dd7730ed8cfeb5917875084d3f8
thedoublejay <josejaimecareers@gmail.com>
2021-06-18 12:13:06 +0800

    updated tests

--- components/icons/tokens/_index.test.tsx
+++ components/icons/tokens/_index.test.tsx
@@ -2,49 +2,14 @@ import * as React from 'react'
 import { getTokenIcon } from "./_index";
 import { render } from "@testing-library/react-native";
 
-it('getTokenIcon("DFI") should get <IconDFI /> snapshot', () => {
-  const Icon = getTokenIcon('DFI')
-  const tree = render(<Icon />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
+const icons = ['DFI', 'FAKE', 'BCH', 'BTC', 'DOGE', 'ETH', 'LTC', 'USDT']
 
-it('getTokenIcon("FAKE") should get <IconDefault /> snapshot', () => {
-  const Icon = getTokenIcon('FAKE')
-  expect(Icon).toMatchSnapshot()
-})
-
-it('getTokenIcon("BCH") should get <IconBCH /> snapshot', () => {
-  const Icon = getTokenIcon('BCH')
-  const tree = render(<Icon />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
-
-it('getTokenIcon("BTC") should get <IconBTC /> snapshot', () => {
-  const Icon = getTokenIcon('BTC')
-  const tree = render(<Icon />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
-
-it('getTokenIcon("DOGE") should get <IconDOGE /> snapshot', () => {
-  const Icon = getTokenIcon('DOGE')
-  const tree = render(<Icon />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
-
-it('getTokenIcon("ETH") should get <IconETH /> snapshot', () => {
-  const Icon = getTokenIcon('ETH')
-  const tree = render(<Icon />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
-
-it('getTokenIcon("LTC") should get <IconLTC /> snapshot', () => {
-  const Icon = getTokenIcon('LTC')
-  const tree = render(<Icon />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
-
-it('getTokenIcon("USDT") should get <IconUSDT /> snapshot', () => {
-  const Icon = getTokenIcon('USDT')
-  const tree = render(<Icon />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
+describe('token icons', () => {
+  icons.forEach(icon => {
+    it(`getTokenIcon("${icon}") should get <Icon${icon} /> snapshot`, () => {
+      const Icon = getTokenIcon(icon)
+      const tree = render(<Icon />).toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+  })
+});
