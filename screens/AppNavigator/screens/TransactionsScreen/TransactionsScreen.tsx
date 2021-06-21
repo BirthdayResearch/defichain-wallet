05d2e0c9fd801d9bcb6dbee17a0113200104dff0
ivan <ivan@zynesis.com>
2021-06-21 10:10:56 +0800

    factor out data reducer and TransactionRow render as separate function

--- screens/AppNavigator/screens/TransactionsScreen/TransactionsScreen.tsx
+++ screens/AppNavigator/screens/TransactionsScreen/TransactionsScreen.tsx
@@ -10,12 +10,28 @@ import { Ionicons } from '@expo/vector-icons'
 import BigNumber from 'bignumber.js'
 import { Text, NumberText } from '../../../../components'
 
+interface TransactionRowModel {
+  id: string
+  desc: string
+  iconName: string
+  color: string
+  amount: string
+  block: number
+  token: string
+}
+
+interface ReducedPageState {
+  txRows: TransactionRowModel[]
+  hasNext: boolean
+  nextToken: string|undefined
+}
+
 export function TransactionsScreen (): JSX.Element {
   const whaleApiClient = useWhaleApiClient()
   const account = useWalletAPI().getWallet().get(0)
 
   // main data
-  const [activities, setAddressActivities] = useState<AddressActivity[]>([])
+  const [activities, setAddressActivities] = useState<TransactionRowModel[]>([])
   const [status, setStatus] = useState('initial') // page status
 
   // TODO(@ivan-zynesis): standardize how to display error (some base component)?
@@ -36,12 +52,31 @@ export function TransactionsScreen (): JSX.Element {
     }
 
     setStatus('loading')
+
+    /**
+     * promise chain of
+     * 1. API call to whale
+     * 2. reducer
+     * 3. dispatch
+     */
     account.getAddress().then(async address => {
       return await whaleApiClient.address.listTransaction(address, undefined, nextToken)
     }).then(async addActivities => {
-      setHasNext(addActivities.hasNext)
-      setNextToken(addActivities.nextToken as string|undefined)
-      setAddressActivities([...activities, ...addActivities])
+      const newRows = []
+      for (let i = 0; i < addActivities.length; i++) {
+        const act = addActivities[i]
+        newRows.push(activityToTxRowReducer(act))
+      }
+
+      return {
+        txRows: [...activities, ...newRows],
+        hasNext: addActivities.hasNext,
+        nextToken: addActivities.nextToken as string|undefined
+      }
+    }).then(async (reduced: ReducedPageState) => {
+      setHasNext(reduced.hasNext)
+      setNextToken(reduced.nextToken)
+      setAddressActivities(reduced.txRows)
       setStatus('idle')
     }).catch((e) => {
       setError(e)
@@ -52,78 +87,6 @@ export function TransactionsScreen (): JSX.Element {
   useEffect(() => {
     loadData()
   }, [])
-
-  const TransactionRow = (row: { item: AddressActivity }): JSX.Element => {
-    let iconName: 'arrow-up' | 'arrow-down'
-    let color: '#02B31B'|'gray'
-    let desc = ''
-    const isPositive = row.item.vin === undefined
-
-    // TODO(@ivan-zynesis): fix when other token transaction can be included
-    const TOKEN_NAME: { [key in number]: string } = {
-      0: 'DFI',
-      1: 'tBTC'
-    }
-
-    const tokenId = TOKEN_NAME[row.item.tokenId as number]
-    let amount = new BigNumber(row.item.value)
-
-    if (isPositive) {
-      color = '#02B31B' // green
-      // TODO(@ivan-zynesis): Simplified, more complicated token transaction should have different icon and desc
-      iconName = 'arrow-down'
-      desc = 'Received'
-    } else {
-      color = 'gray'
-      iconName = 'arrow-up'
-      desc = 'Sent'
-      amount = amount.negated()
-    }
-
-    const option = {
-      id: row.item.id,
-      desc,
-      iconName,
-      color,
-      amount: amount.toFixed(),
-      block: row.item.block.height,
-      token: tokenId
-    }
-
-    const txIcon = <Ionicons name='arrow-down' size={24} color={color} />
-    const amountText = <NumberText value={option.amount} style={{ color }} />
-
-    return (
-      <View key={row.item.id} style={tailwind('bg-white p-2 border-b border-gray-200 flex-row items-center w-full h-16')}>
-        <View style={tailwind('w-full flex-row flex-initial')}>
-          <View style={tailwind('w-8 justify-center')}>
-            {txIcon}
-          </View>
-          <View style={tailwind('flex-auto flex-row justify-center items-center')}>
-            <View style={tailwind('flex-auto flex-col ml-3 justify-center')}>
-              <Text style={tailwind('font-medium')}>
-                {translate('screens/TransactionsScreen', option.desc)}
-              </Text>
-              <Text style={tailwind('font-medium')}>
-                {translate('screens/TransactionsScreen', 'block')}: {option.block}
-              </Text>
-            </View>
-            <View style={tailwind('flex-row ml-3 items-center')}>
-              {amountText}
-              <View style={tailwind('w-16 ml-2 items-start')}>
-                <Text style={tailwind('flex-shrink font-medium text-gray-600')}>
-                  {option.token}
-                </Text>
-              </View>
-            </View>
-          </View>
-          <View style={tailwind('w-8 flex-grow-0 justify-center flex-end')}>
-            <Ionicons name='chevron-forward' size={24} color='gray' />
-          </View>
-        </View>
-      </View>
-    )
-  }
 
   const LoadMore = (): JSX.Element|null => {
     if (hasNext !== true) {
@@ -145,4 +108,86 @@ export function TransactionsScreen (): JSX.Element {
       ListFooterComponent={LoadMore}
     />
   )
+}
+
+// TODO(@ivan-zynesis): refactoring as a separate file (doing it later because transaction detail implementation branch has a lot file hierarchy changes)
+function TransactionRow (row: { item: TransactionRowModel }): JSX.Element {
+  const {
+    color,
+    amount,
+    desc,
+    block,
+    token
+  } = row.item
+  const txIcon = <Ionicons name='arrow-down' size={24} color={color} />
+  const amountText = <NumberText value={amount} style={{ color }} />
+
+  return (
+    <View key={row.item.id} style={tailwind('bg-white p-2 border-b border-gray-200 flex-row items-center w-full h-16')}>
+      <View style={tailwind('w-full flex-row flex-initial')}>
+        <View style={tailwind('w-8 justify-center')}>
+          {txIcon}
+        </View>
+        <View style={tailwind('flex-auto flex-row justify-center items-center')}>
+          <View style={tailwind('flex-auto flex-col ml-3 justify-center')}>
+            <Text style={tailwind('font-medium')}>
+              {translate('screens/TransactionsScreen', desc)}
+            </Text>
+            <Text style={tailwind('font-medium')}>
+              {translate('screens/TransactionsScreen', 'block')}: {block}
+            </Text>
+          </View>
+          <View style={tailwind('flex-row ml-3 items-center')}>
+            {amountText}
+            <View style={tailwind('w-16 ml-2 items-start')}>
+              <Text style={tailwind('flex-shrink font-medium text-gray-600')}>
+                {token}
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={tailwind('w-8 flex-grow-0 justify-center flex-end')}>
+          <Ionicons name='chevron-forward' size={24} color='gray' />
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const activityToTxRowReducer = (activity: AddressActivity): TransactionRowModel => {
+  let iconName: 'arrow-up' | 'arrow-down'
+  let color: '#02B31B'|'gray'
+  let desc = ''
+  const isPositive = activity.vin === undefined
+
+  // TODO(@ivan-zynesis): fix when other token transaction can be included
+  const TOKEN_NAME: { [key in number]: string } = {
+    0: 'DFI',
+    1: 'tBTC'
+  }
+
+  const tokenId = TOKEN_NAME[activity.tokenId as number]
+  let amount = new BigNumber(activity.value)
+
+  if (isPositive) {
+    color = '#02B31B' // green
+    // TODO(@ivan-zynesis): Simplified, more complicated token transaction should have different icon and desc
+    iconName = 'arrow-down'
+    desc = 'Received'
+  } else {
+    color = 'gray'
+    iconName = 'arrow-up'
+    desc = 'Sent'
+    amount = amount.negated()
+  }
+
+  return {
+    id: activity.id,
+    desc,
+    iconName,
+    color,
+    amount: amount.toFixed(),
+    block: activity.block.height,
+    token: tokenId
+  }
 }
