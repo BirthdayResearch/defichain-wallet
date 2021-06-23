9521e0146b55847aed343d9a5e5c7162a5baadec
Fuxing Loh <git@fuxing.dev>
2021-06-23 15:37:05 +0800

    fixed DEX merge conflict and rename from Liquidity* to Dex*

---
+++ screens/AppNavigator/screens/Dex/DexNavigator.tsx
@@ -0,0 +1,24 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import * as React from 'react'
+import { translate } from '../../../../translations'
+import { DexScreen } from './DexScreen'
+
+export interface DexParamList {
+  DexScreen: undefined
+
+  [key: string]: undefined | object
+}
+
+const DexStack = createStackNavigator<DexParamList>()
+
+export function DexNavigator (): JSX.Element {
+  return (
+    <DexStack.Navigator>
+      <DexStack.Screen
+        name='DexScreen'
+        component={DexScreen}
+        options={{ headerTitle: translate('screens/DexScreen', 'Decentralized Exchange') }}
+      />
+    </DexStack.Navigator>
+  )
+}
