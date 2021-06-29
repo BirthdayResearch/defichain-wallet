8a52ffcad8dc87357a7e79d077dcafb7f481ac0c
thedoublejay <josejaimecareers@gmail.com>
2021-06-30 07:32:17 +0800

    removed QR code features

--- screens/AppNavigator/screens/BalancesScreen/SendScreen/SendScreen.tsx
+++ screens/AppNavigator/screens/BalancesScreen/SendScreen/SendScreen.tsx
@@ -2,9 +2,8 @@ import { AddressToken } from '@defichain/whale-api-client/dist/api/address'
 import { MaterialIcons } from '@expo/vector-icons'
 import { StackScreenProps } from '@react-navigation/stack'
 import React, { useState } from 'react'
-import { ScrollView, TouchableOpacity, View, StyleSheet, Modal } from 'react-native'
+import { ScrollView, TouchableOpacity, View } from 'react-native'
 import { useForm, Controller, Control } from 'react-hook-form'
-import { BarCodeScanner } from 'expo-barcode-scanner'
 import NumberFormat from 'react-number-format'
 import tailwind from 'tailwind-rn'
 import { Text, TextInput } from '../../../../../components'
@@ -14,12 +13,6 @@ import { PrimaryColor, PrimaryColorStyle } from '../../../../../constants/Theme'
 import { translate } from '../../../../../translations'
 import { BalanceParamList } from '../BalancesNavigator'
 
-interface QRScannerProps {
-  onScan: (code: string) => void
-  isQRScanning: boolean
-  onCloseModal: () => void
-}
-
 interface AmountForm {
   control: Control
   token: AddressToken
@@ -28,14 +21,12 @@ interface AmountForm {
 
 interface AddressForm {
   control: Control
-  openQRScanner: () => void
 }
 
 type Props = StackScreenProps<BalanceParamList, 'SendScreen'>
 
 export function SendScreen ({ route }: Props): JSX.Element {
   const [token] = useState(route.params.token)
-  const [isQRScanning, setIsQRScanning] = useState(false)
   const { control, setValue, formState: { isValid }, getValues, trigger } = useForm({ mode: 'onChange' })
 
   const onSubmit = (): void => {
@@ -45,14 +36,7 @@ export function SendScreen ({ route }: Props): JSX.Element {
 
   return (
     <ScrollView style={tailwind('bg-gray-100')}>
-      <QRScanner
-        onScan={async (value: string) => {
-          setValue('address', value)
-          await trigger('address')
-          setIsQRScanning(false)
-        }} onCloseModal={() => setIsQRScanning(false)} isQRScanning={isQRScanning}
-      />
-      <AddressRow control={control} openQRScanner={() => setIsQRScanning(true)} />
+      <AddressRow control={control} />
       <AmountRow
         token={token} control={control} onMaxPress={async (amount) => {
           setValue('amount', amount)
@@ -66,7 +50,7 @@ export function SendScreen ({ route }: Props): JSX.Element {
   )
 }
 
-function AddressRow ({ control, openQRScanner }: AddressForm): JSX.Element {
+function AddressRow ({ control }: AddressForm): JSX.Element {
   return (
     <>
       <Text
@@ -89,7 +73,7 @@ function AddressRow ({ control, openQRScanner }: AddressForm): JSX.Element {
               placeholder={translate('screens/SendScreen', 'Enter an address')}
             />
             <TouchableOpacity
-              style={tailwind('p-4 bg-white')} onPress={openQRScanner}
+              style={tailwind('p-4 bg-white')}
             >
               <MaterialIcons name='qr-code-scanner' size={24} color={PrimaryColor} />
             </TouchableOpacity>
@@ -155,53 +139,3 @@ function AmountRow ({ token, control, onMaxPress }: AmountForm): JSX.Element {
     </>
   )
 }
-
-function QRScanner ({ onScan, isQRScanning, onCloseModal }: QRScannerProps): JSX.Element {
-  const [scanned, setScanned] = useState(false)
-
-  const handleBarCodeScanned = ({ data }: { data: string }): void => {
-    setScanned(true)
-    onScan(data)
-  }
-
-  return (
-    <Modal
-      animationType='slide'
-      transparent
-      visible={isQRScanning}
-    >
-      <View style={tailwind('flex-1 items-center justify-center')}>
-        <View style={[styles.modalView, tailwind('bg-white rounded-xl w-9/12 h-3/5 relative items-center')]}>
-          <BarCodeScanner
-            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-            barCodeTypes={['qr']}
-            style={tailwind('flex-1 w-full')}
-          />
-
-          <TouchableOpacity
-            style={[tailwind('mt-2 px-2 py-1.5 ml-3 flex-row items-center border border-gray-300 rounded')]}
-            onPress={onCloseModal}
-          >
-            <Text style={[tailwind('mx-1'), PrimaryColorStyle.text]}>
-              {translate('screens/SendScreen', 'Close Modal')}
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </Modal>
-  )
-}
-
-const styles = StyleSheet.create({
-  modalView: {
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    padding: 10
-  }
-})
