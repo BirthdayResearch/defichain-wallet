a27efed2f74a433d219be3b8b12bb033d855c5c7
Fuxing Loh <git@fuxing.dev>
2021-06-30 15:48:22 +0800

    added environment and network prefixed storage

--- app/storage.test.ts
+++ app/storage.test.ts
@@ -1,34 +1,61 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { EnvironmentNetwork } from "./environment";
-import { getNetwork, setNetwork } from "./storage";
+import * as storage from "./storage";
 
 const getItem = jest.spyOn(AsyncStorage, 'getItem')
 const setItem = jest.spyOn(AsyncStorage, 'setItem')
+const removeItem = jest.spyOn(AsyncStorage, 'removeItem')
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
 
 describe('network', () => {
   it('should default to Remote Playground', async () => {
-    getItem.mockClear()
-    expect(await getNetwork()).toBe(EnvironmentNetwork.RemotePlayground)
+    expect(await storage.getNetwork()).toBe(EnvironmentNetwork.RemotePlayground)
     expect(getItem).toBeCalled()
   });
 
   it('should call setItem', async () => {
-    await setNetwork(EnvironmentNetwork.RemotePlayground)
+    await storage.setNetwork(EnvironmentNetwork.RemotePlayground)
     expect(setItem).toBeCalled()
   });
 
   it('should get Local Playground', async () => {
     getItem.mockResolvedValue(EnvironmentNetwork.LocalPlayground)
-    expect(await getNetwork()).toBe(EnvironmentNetwork.LocalPlayground)
+    expect(await storage.getNetwork()).toBe(EnvironmentNetwork.LocalPlayground)
   });
 
   it('should get Local Playground', async () => {
     getItem.mockResolvedValue(EnvironmentNetwork.RemotePlayground)
-    expect(await getNetwork()).toBe(EnvironmentNetwork.RemotePlayground)
+    expect(await storage.getNetwork()).toBe(EnvironmentNetwork.RemotePlayground)
   });
 
   it('should errored as network is not part of environment', async () => {
-    await expect(setNetwork(EnvironmentNetwork.MainNet))
+    await expect(storage.setNetwork(EnvironmentNetwork.MainNet))
       .rejects.toThrow('network is not part of environment')
   });
+})
+
+describe('item', () => {
+  beforeEach(() => {
+    getItem.mockResolvedValue(EnvironmentNetwork.RemotePlayground)
+  })
+
+  it('should getItem with environment and network prefixed key', async () => {
+    await storage.getItem('get')
+    expect(getItem).toBeCalledTimes(2)
+    expect(getItem).toBeCalledWith('Development.NETWORK')
+    expect(getItem).toBeCalledWith('Development.Remote Playground.get')
+  })
+
+  it('should setItem with environment and network prefixed key', async () => {
+    await storage.setItem('set', 'value')
+    expect(setItem).toBeCalledWith('Development.Remote Playground.set', 'value')
+  })
+
+  it('should removeItem with environment and network prefixed key', async () => {
+    await storage.removeItem('remove')
+    expect(removeItem).toBeCalledWith('Development.Remote Playground.remove')
+  })
 })
