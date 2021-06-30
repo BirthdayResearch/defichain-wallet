c0d21fd1d2b846442badecad192f56c8d4af0db1
thedoublejay <josejaimecareers@gmail.com>
2021-06-30 17:11:21 +0800

    initial commit for poolswap

--- screens/AppNavigator/screens/Dex/DexNavigator.tsx
+++ screens/AppNavigator/screens/Dex/DexNavigator.tsx
@@ -1,10 +1,13 @@
+import { PoolPairData } from '@defichain/whale-api-client/dist/api/poolpair'
 import { createStackNavigator } from '@react-navigation/stack'
 import * as React from 'react'
 import { translate } from '../../../../translations'
 import { DexScreen } from './DexScreen'
+import { PoolSwapScreen } from './PoolSwap/PoolSwapScreen'
 
 export interface DexParamList {
   DexScreen: undefined
+  PoolSwapScreen: { poolpair: PoolPairData }
 
   [key: string]: undefined | object
 }
@@ -17,6 +20,11 @@ export function DexNavigator (): JSX.Element {
       <DexStack.Screen
         name='DexScreen'
         component={DexScreen}
+        options={{ headerTitle: translate('screens/DexScreen', 'Decentralized Exchange') }}
+      />
+      <DexStack.Screen
+        name='PoolSwap'
+        component={PoolSwapScreen}
         options={{ headerTitle: translate('screens/DexScreen', 'Decentralized Exchange') }}
       />
     </DexStack.Navigator>
