aaea6670a2367d561028fc48072035dd3dc11c84
ivan <ivan@zynesis.com>
2021-07-06 17:15:44 +0800

    quick save

--- cypress/integration/functional/balances/convert/convert_account_to_utxos.spec.ts
+++ cypress/integration/functional/balances/convert/convert_account_to_utxos.spec.ts
@@ -3,7 +3,9 @@ context('wallet/balances/convert - accountToUtxos: invalid input', () => {
     cy.createEmptyWallet(true)
 
     cy.getByTestID('bottom_tab_settings').click()
-    cy.sendDFItoWallet().wait(4000)
+    cy.sendDFItoWallet()
+      // .sendTokenToWallet(['DFI'])
+      .wait(4000)
     cy.getByTestID('playground_wallet_fetch_balances').click()
 
     cy.getByTestID('bottom_tab_balances').click()
@@ -30,4 +32,11 @@ context('wallet/balances/convert - accountToUtxos: invalid input', () => {
     cy.getByTestID('text_row_output_to_rhs').should('contain', '11.1')
     cy.getByTestID('button_continue_convert').should('not.be.enabled')
   })
+
+  // it('should insert balance - 0.001 as amount on MAX pressed', function () {
+  //   cy.getByTestID('button_max_convert_from').click()
+
+  //   cy.getByTestID('text_row_output_to_rhs').should('contain', '10')
+  //   cy.getByTestID('button_continue_convert').should('not.be.disabled')
+  // })
 })
