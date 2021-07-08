e5449435b7fee3ff33bea5481bda7d1ecb5dbc3c
thedoublejay <josejaimecareers@gmail.com>
2021-07-08 12:51:03 +0800

    removed `useEffect`

--- app/components/icons/tokens/IconDefault.tsx
+++ app/components/icons/tokens/IconDefault.tsx
@@ -1,15 +1,10 @@
 import randomColor from 'randomcolor'
 import * as React from 'react'
-import { useEffect, useState } from 'react'
 import Svg, { Circle, SvgProps, Text } from 'react-native-svg'
 
 export function IconDefault (symbol: string): (props: SvgProps) => JSX.Element {
   return (props: SvgProps): JSX.Element => {
-    const [color, setColor] = useState('')
-    useEffect(() => {
-      const c = randomColor({ luminosity: 'dark', seed: symbol })
-      setColor(c)
-    }, [symbol])
+    const color = randomColor({ luminosity: 'dark', seed: symbol })
     return (
       <Svg width={32} height={32} viewBox='0 0 32 32' {...props}>
         <Circle cx={16} cy={16} r={16} fill={color} />
