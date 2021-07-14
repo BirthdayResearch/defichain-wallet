3731cf876cffbd080a9e136b82534b987ea21428
Fuxing Loh <git@fuxing.dev>
2021-07-14 17:57:48 +0800

    added NODE_ENV.E2E

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: process.env.NODE_ENV === 'E2E' ? ['istanbul'] : []
   }
 }
