4382835dcbf30674104753afd4a4bbe8c0109a44
Fuxing Loh <git@fuxing.dev>
2021-07-14 17:24:46 +0800

    cypress config changes

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -16,9 +16,6 @@
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions): any => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   require('@cypress/code-coverage/task')(on, config)
   return config
