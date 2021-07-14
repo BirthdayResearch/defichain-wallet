89e7df22d27430d8b9f08e798059ca72fcb75452
thedoublejay <josejaimecareers@gmail.com>
2021-07-14 12:05:10 +0800

    test

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,6 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    env: {
-      test: {
-        plugins: []
-      },
-      e2e: {
-        plugins: ['istanbul']
-      }
-    }
+    plugins: ['istanbul']
   }
 }
