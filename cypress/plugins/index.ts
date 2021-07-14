8d17311dda6572f6b2078c0d027212e64091c333
Fuxing Loh <git@fuxing.dev>
2021-07-14 14:10:38 +0800

    Fixed Cypress Coverage

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -18,14 +18,8 @@
 module.exports = (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions): any => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
 
-module.exports = (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   require('@cypress/code-coverage/task')(on, config)
-  // include any other plugin code...
-
-  // It's IMPORTANT to return the config object
-  // with any changed environment variables
   return config
 }
