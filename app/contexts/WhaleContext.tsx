fe5d01219adf64e81ced82553a9574e83f8e010d
JJ Adonis <josejaimecareers@gmail.com>
2021-07-15 11:01:07 +0800

    PR updates

--- app/contexts/WhaleContext.tsx
+++ app/contexts/WhaleContext.tsx
@@ -1,7 +1,7 @@
 import { WhaleApiClient } from '@defichain/whale-api-client'
 import React, { createContext, useContext, useMemo } from 'react'
 import { EnvironmentNetwork } from '../environment'
-import { networkMapper, useNetworkContext } from './NetworkContext'
+import { useNetworkContext } from './NetworkContext'
 
 const WhaleApiClientContext = createContext<WhaleApiClient>(undefined as any)
 
@@ -24,15 +24,14 @@ export function WhaleProvider (props: React.PropsWithChildren<any>): JSX.Element
 }
 
 function newWhaleClient (network: EnvironmentNetwork): WhaleApiClient {
-  const whaleOpts = { network: networkMapper(network) }
   switch (network) {
     case EnvironmentNetwork.MainNet:
-      return new WhaleApiClient({ url: 'https://ocean.defichain.com', ...whaleOpts })
+      return new WhaleApiClient({ url: 'https://ocean.defichain.com', network: 'mainnet' })
     case EnvironmentNetwork.TestNet:
-      return new WhaleApiClient({ url: 'https://ocean.defichain.com', ...whaleOpts })
+      return new WhaleApiClient({ url: 'https://ocean.defichain.com', network: 'testnet' })
     case EnvironmentNetwork.RemotePlayground:
-      return new WhaleApiClient({ url: 'https://playground.defichain.com', ...whaleOpts })
+      return new WhaleApiClient({ url: 'https://playground.defichain.com', network: 'regtest' })
     case EnvironmentNetwork.LocalPlayground:
-      return new WhaleApiClient({ url: 'http://localhost:19553', ...whaleOpts })
+      return new WhaleApiClient({ url: 'http://localhost:19553', network: 'regtest' })
   }
 }
