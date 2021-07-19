7fc642f78209ffe11b02beff4184cf90bc8c0f32
thedoublejay <josejaimecareers@gmail.com>
2021-07-19 18:16:16 +0800

    breaking build

--- app/screens/ErrorBoundary/ErrorBoundary.tsx
+++ app/screens/ErrorBoundary/ErrorBoundary.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react'
-import { tailwind } from '../../../tailwind'
+import tailwind from 'tailwind-rn'
 import { Text, View } from '../../components'
 import { AppIcon } from '../../components/icons/AppIcon'
 import { translate } from '../../translations'
