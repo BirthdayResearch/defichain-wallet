cad284a725ce0da63df659597602078fee79ef76
Fuxing Loh <git@fuxing.dev>
2021-07-23 15:30:48 +0800

    moved all network code into network.ts

--- app/api/wallet/network.ts
+++ app/api/wallet/network.ts
@@ -1,7 +1,8 @@
 import { MainNet, Network, RegTest, TestNet } from '@defichain/jellyfish-network'
+import { Bip32Options } from '@defichain/jellyfish-wallet-mnemonic'
 import { EnvironmentNetwork } from '../../environment'
 
-export function getJellyfishNetwork (network: EnvironmentNetwork): Network {
+function asJellyfishNetwork (network: EnvironmentNetwork): Network {
   switch (network) {
     case EnvironmentNetwork.MainNet:
       return MainNet
@@ -10,5 +11,16 @@ export function getJellyfishNetwork (network: EnvironmentNetwork): Network {
     case EnvironmentNetwork.LocalPlayground:
     case EnvironmentNetwork.RemotePlayground:
       return RegTest
+  }
+}
+
+export function getBip32Option (envNetwork: EnvironmentNetwork): Bip32Options {
+  const network = asJellyfishNetwork(envNetwork)
+  return {
+    bip32: {
+      public: network.bip32.publicPrefix,
+      private: network.bip32.privatePrefix
+    },
+    wif: network.wifPrefix
   }
 }
