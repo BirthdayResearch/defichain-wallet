1437087c308ff525286402ff6f57169488b401eb
JJ Adonis <josejaimecareers@gmail.com>
2021-07-25 19:05:36 +0800

    updated comments

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -7,19 +7,18 @@ export type ButtonFillTypes = 'fill' | 'outline' | 'flat'
 export type ButtonColor = 'primary' | 'secondary'
 
 export function Button (props: React.PropsWithChildren<ButtonProps> & { color?: ButtonColor, fill?: ButtonFillTypes, label?: string }): JSX.Element {
-  const buttonColor = props.color ?? 'primary'
-  const buttonFill = props.fill ?? 'fill'
-  const buttonStyle = `${buttonFill === 'flat' ? 'border-none' : `border border-${buttonColor} border-opacity-20`}
-                    ${buttonFill === 'fill' ? `bg-${buttonColor} bg-opacity-10` : 'bg-transparent'}`
+  const {
+    color = 'primary',
+    fill = 'fill'
+  } = props
+  const buttonStyle = `${fill === 'flat' ? 'border-none' : `border border-${color} border-opacity-20`}
+                    ${fill === 'fill' ? `bg-${color} bg-opacity-10` : 'bg-transparent'}`
   const disabledStyle = 'bg-black bg-opacity-20 text-white text-opacity-5 border-0'
 
-  const textStyle = `${props.disabled === true ? 'text-white text-opacity-20' : `text-${buttonColor}`}`
+  const textStyle = `${props.disabled === true ? 'text-white text-opacity-20' : `text-${color}`}`
   return (
     <TouchableOpacity
       {...props}
-      testID={props.testID}
-      disabled={props.disabled}
-      onPress={props.onPress}
       style={[tailwind(`m-4 mt-8 p-3 rounded flex-row justify-center ${buttonStyle} ${props.disabled === true ? disabledStyle : ''}`)]}
     >
       {
