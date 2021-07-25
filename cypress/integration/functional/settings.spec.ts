b4af5940f898a05d2a13f8b36df553eed1f81515
thedoublejay <josejaimecareers@gmail.com>
2021-07-25 11:00:05 +0800

    settings failing

--- cypress/integration/functional/settings.spec.ts
+++ cypress/integration/functional/settings.spec.ts
@@ -4,9 +4,12 @@ context('wallet/settings', () => {
     cy.getByTestID('bottom_tab_settings').click()
   })
 
-  it('should change network and exit wallet when clicked', function () {
+  it('should change network when clicked', function () {
     cy.getByTestID('button_network_RemotePlayground').click()
     cy.getByTestID('button_network_RemotePlayground_check').should('exist')
+  })
+
+  it('should exit wallet when clicked', function () {
     cy.getByTestID('setting_exit_wallet').click()
     cy.getByTestID('wallet_onboarding').contains('No wallets')
   })
