4d5e02f9b641b057045f6f1859aace3bd4cb7025
thedoublejay <josejaimecareers@gmail.com>
2021-07-25 12:39:49 +0800

    fixed failing tests

--- cypress/integration/functional/dex/poolswap.spec.ts
+++ cypress/integration/functional/dex/poolswap.spec.ts
@@ -8,7 +8,7 @@ context('poolswap without balance', () => {
 
   it('should be able to validate empty form', function () {
     cy.getByTestID('bottom_tab_dex').click()
-    cy.getByTestID('button_swap_DFI-ETH').click()
+    cy.getByTestID('button_swap_DFI-LTC').click()
     cy.getByTestID('button_submit').should('have.attr', 'disabled')
   })
 })
@@ -17,19 +17,19 @@ context('poolswap with values', () => {
   before(function () {
     cy.createEmptyWallet(true)
     cy.getByTestID('bottom_tab_settings').click()
-    cy.sendDFItoWallet().sendDFITokentoWallet().sendTokenToWallet(['ETH']).wait(10000)
+    cy.sendDFItoWallet().sendDFITokentoWallet().sendTokenToWallet(['LTC']).wait(10000)
     cy.getByTestID('playground_wallet_fetch_balances').click()
     cy.getByTestID('bottom_tab_balances').click()
     cy.getByTestID('bottom_tab_dex').click()
-    cy.getByTestID('button_swap_DFI-ETH').click()
+    cy.getByTestID('button_swap_DFI-LTC').click()
   })
 
   it('should be able to click swap button', function () {
     cy.getByTestID('text_balance_tokenA').contains('10.00000000 DFI')
-    cy.getByTestID('text_balance_tokenB').contains('10.00000000 ETH')
+    cy.getByTestID('text_balance_tokenB').contains('10.00000000 LTC')
     cy.getByTestID('swap_button').click().wait(7000)
     cy.getByTestID('text_balance_tokenB').contains('10.00000000 DFI')
-    cy.getByTestID('text_balance_tokenA').contains('10.00000000 ETH')
+    cy.getByTestID('text_balance_tokenA').contains('10.00000000 LTC')
   })
 
   it('should be able to validate form', function () {
@@ -37,7 +37,7 @@ context('poolswap with values', () => {
     // Valid form
     cy.getByTestID('text_input_tokenA').type('1')
     cy.getByTestID('text_price_row_price_1').then(($txt: any) => {
-      const tokenValue = $txt[0].textContent.replace(' ETH per DFI', '').replace(',', '')
+      const tokenValue = $txt[0].textContent.replace(' LTC per DFI', '').replace(',', '')
       cy.getByTestID('text_input_tokenB').should('have.value', new BigNumber(tokenValue).toFixed(8))
       cy.getByTestID('button_submit').should('not.have.attr', 'disabled')
 
@@ -57,21 +57,21 @@ context('poolswap with values', () => {
     cy.getByTestID('max_button_token_a').click().wait(3000)
     cy.getByTestID('text_input_tokenA').should('have.value', '10.00000000')
     cy.getByTestID('text_price_row_minimum_0').then(($txt: any) => {
-      const tokenValue = $txt[0].textContent.replace(' ETH', '').replace(',', '')
+      const tokenValue = $txt[0].textContent.replace(' LTC', '').replace(',', '')
       cy.getByTestID('text_input_tokenB').should('have.value', new BigNumber(tokenValue).toFixed(8))
     })
   })
 
   it('should be able to swap', function () {
     cy.getByTestID('text_price_row_minimum_0').then(($txt: any) => {
-      const tokenValue = $txt[0].textContent.replace(' ETH', '').replace(',', '')
+      const tokenValue = $txt[0].textContent.replace(' LTC', '').replace(',', '')
       cy.getByTestID('button_submit').click()
       cy.wait(5000).getByTestID('oceanInterface_close').click().wait(5000)
       cy.getByTestID('playground_wallet_fetch_balances').click()
       cy.getByTestID('bottom_tab_balances').click()
-      cy.getByTestID('balances_row_2').should('exist')
-      cy.getByTestID('balances_row_2_amount').then(($txt: any) => {
-        const balanceAmount = $txt[0].textContent.replace(' ETH', '').replace(',', '')
+      cy.getByTestID('balances_row_4').should('exist')
+      cy.getByTestID('balances_row_4_amount').then(($txt: any) => {
+        const balanceAmount = $txt[0].textContent.replace(' LTC', '').replace(',', '')
         expect(new BigNumber(balanceAmount).toNumber()).be.gte(new BigNumber(tokenValue).toNumber())
       })
 
--- cypress/integration/functional/settings.spec.ts
+++ cypress/integration/functional/settings.spec.ts
@@ -1,15 +1,14 @@
 context('wallet/settings', () => {
-  beforeEach(function () {
+  it('should change network when clicked', function () {
     cy.createEmptyWallet(true).wait(10000)
     cy.getByTestID('bottom_tab_settings').click()
-  })
-
-  it('should change network when clicked', function () {
     cy.getByTestID('button_network_RemotePlayground').click()
     cy.getByTestID('button_network_RemotePlayground_check').should('exist')
   })
 
   it('should exit wallet when clicked', function () {
+    cy.createEmptyWallet(true).wait(10000)
+    cy.getByTestID('bottom_tab_settings').click()
     cy.getByTestID('setting_exit_wallet').click()
     cy.getByTestID('wallet_onboarding').contains('No wallets')
   })
