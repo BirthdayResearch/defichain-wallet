8693538dcb13d831eb39ce7495fc00b4a1da0274
JJ Adonis <josejaimecareers@gmail.com>
2021-07-25 19:21:20 +0800

    updated test id

--- cypress/integration/functional/settings.spec.ts
+++ cypress/integration/functional/settings.spec.ts
@@ -6,7 +6,7 @@ context('wallet/settings', () => {
 
   it('should change network when clicked', function () {
     cy.getByTestID('button_network_Local_check').should('exist')
-    cy.getByTestID('button_network_Remote').click()
+    cy.getByTestID('button_network_Playground').click()
   })
 
   it('should exit wallet when clicked', function () {
