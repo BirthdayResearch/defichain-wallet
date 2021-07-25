53912aac37d160ce90d5d69b03dff0163116c196
thedoublejay <josejaimecareers@gmail.com>
2021-07-25 11:13:08 +0800

    updated remove liq spec

--- cypress/integration/functional/dex/remove_liquidity.spec.ts
+++ cypress/integration/functional/dex/remove_liquidity.spec.ts
@@ -14,13 +14,16 @@ context('app/dex/removeLiquidity', () => {
     const row = list.getByTestID('pool_pair_row_your').first()
     row.invoke('text').should(text => expect(text).to.contains('10.00 DFI-ETH'))
 
-    cy.getByTestID('pool_pair_remove_DFI-ETH').click().wait(100)
+    cy.getByTestID('pool_pair_remove_DFI-ETH').click().wait(1000)
 
     cy.getByTestID('text_coin_amount_DFI').contains('0')
     cy.getByTestID('text_coin_amount_ETH').contains('0')
+    cy.getByTestID('remove_liq_symbol_DFI').contains('DFI')
+    cy.getByTestID('remove_liq_symbol_ETH').contains('ETH')
   })
 
-  it.skip('should display price based on pool tokenA:tokenB ratio regardless removal amount', function () {
+  it('should display price based on pool tokenA:tokenB ratio regardless removal amount', function () {
+    cy.wait(1000)
     cy.getByTestID('text_a_to_b_price').contains('0.1 DFI')
     cy.getByTestID('text_b_to_a_price').contains('10 ETH')
   })
@@ -28,26 +31,28 @@ context('app/dex/removeLiquidity', () => {
   // // unable to trigger slider change event for react: https://github.com/cypress-io/cypress/issues/1570
   // it('Slider - should be draggable with 0.01% precision', function () {
   //   cy.getByTestID('slider_remove_liq_percentage').invoke('val', '11.1').trigger('change').wait(100)
-  //   cy.getByTestID('text_slider_pencentage').invoke('text').should(t => expect(t).equals('11.10 %'))
+  //   cy.getByTestID('text_slider_percentage').invoke('text').should(t => expect(t).equals('11.10 %'))
 
   //   cy.getByTestID('slider_remove_liq_percentage').invoke('val', '99.9949999').trigger('change').wait(100)
-  //   cy.getByTestID('text_slider_pencentage').invoke('text').should(t => expect(t).equals('99.99 %'))
+  //   cy.getByTestID('text_slider_percentage').invoke('text').should(t => expect(t).equals('99.99 %'))
   // })
 
   it('Slider "None" / "All" button', function () {
-    cy.getByTestID('button_slider_max').click().wait(100)
-    cy.getByTestID('text_slider_pencentage').contains('100.00 %')
-    cy.getByTestID('text_a_to_b_price').contains('0.1')
-    cy.getByTestID('text_b_to_a_price').contains('10')
+    cy.getByTestID('button_slider_max').click().wait(1000)
+    cy.getByTestID('text_slider_percentage').contains('100.00 %')
+    cy.getByTestID('text_coin_amount_DFI').contains('1')
+    cy.getByTestID('text_coin_amount_ETH').contains('100')
 
-    cy.getByTestID('button_slider_min').click().wait(100)
-    cy.getByTestID('text_slider_pencentage').contains('0.00 %')
-    cy.getByTestID('text_a_to_b_price').contains('0')
-    cy.getByTestID('text_b_to_a_price').contains('0')
+    cy.getByTestID('button_slider_min').click().wait(1000)
+    cy.getByTestID('text_slider_percentage').contains('0.00 %')
+    cy.getByTestID('text_coin_amount_DFI').contains('0')
+    cy.getByTestID('text_coin_amount_DFI').contains('0')
+
+    cy.getByTestID('button_continue_remove_liq').should('have.attr', 'disabled')
   })
 
   it('Should be able to remove liquidity', function () {
-    cy.getByTestID('button_slider_max').click().wait(100)
+    cy.getByTestID('button_slider_max').click().wait(1000)
     cy.getByTestID('button_continue_remove_liq').click().wait(4000)
 
     // redirected back to dex root page
