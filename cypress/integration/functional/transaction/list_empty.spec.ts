22341bf40f595b80ec5cff864a2c285055dc318a
Keng Ye <kengyeleow@gmail.com>
2021-07-26 19:20:28 +0800

    test: create randomized wallet
    When running test on empty transaction list
    
    Ref: Issue #244

--- cypress/integration/functional/transaction/list_empty.spec.ts
+++ cypress/integration/functional/transaction/list_empty.spec.ts
@@ -1,12 +1,12 @@
 context('app/transactions/list', () => {
   describe('new wallet', function () {
     before(() => {
-      cy.createEmptyWallet()
+      cy.createEmptyWallet(true)
       cy.getByTestID('bottom_tab_transactions').click()
     })
 
     it('should display 0 row', () => {
-      cy.getByTestID('transactions_screen_list').should('exist')
+      cy.getByTestID('transactions_screen_list').should('not.exist')
       cy.getByTestID('transaction_row_0').should('not.exist')
     })
 
