e2d51569b8a8f75b74a2d8d17933933998368189
thedoublejay <josejaimecareers@gmail.com>
2021-07-26 20:51:00 +0800

    create wallet guidelines

---
+++ app/screens/WalletNavigator/screens/CreateWallet/CreateWalletGuidelines.test.tsx
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react-native";
+import * as React from "react";
+import { CreateWalletGuidelines } from "./CreateWalletGuidelines";
+
+describe('create wallet guidelines', () => {
+  it('should match snapshot', () => {
+    const navigation: any = {
+      navigate: jest.fn(),
+    }
+    const route: any = {}
+    const rendered = render(<CreateWalletGuidelines navigation={navigation} route={route} />)
+    expect(rendered.toJSON()).toMatchSnapshot()
+  })
+})
