ec5da2e9404556df20e5cf51f0ab712b6f2daff9
ivan <ivan@zynesis.com>
2021-07-29 14:40:27 +0800

    implement scryptsy for web locally, dep removed from jellyfish

--- app/api/scrypt.ts
+++ app/api/scrypt.ts
@@ -1,4 +1,43 @@
-// import { Scrypt } from '@defichain/jellyfish-wallet-encrypted'
-import { scrypt as nativeScrypt } from './scrypt.native'
+import { Scrypt, ScryptProvider } from '@defichain/jellyfish-wallet-encrypted'
+import scryptsy from 'scryptsy'
 
-export const scrypt = nativeScrypt // new Scrypt(new SimpleScryptsy())
+export interface ScryptParams {
+  N: number
+  r: number
+  p: number
+}
+
+const DEFAULT_SCRYPT_PARAMS: ScryptParams = {
+  N: 16384,
+  r: 8,
+  p: 8
+}
+
+// web implementation, required crypto module under the hood
+export class SimpleScryptsy implements ScryptProvider {
+  constructor (private readonly params: ScryptParams = DEFAULT_SCRYPT_PARAMS) {}
+
+  /**
+   * Derive a specific length buffer via Scrypt implementation
+   * Recommended (by bip38) to serve as an private key encryption key
+   *
+   * @param {string} passphrase utf8 string
+   * @param {Buffer} salt
+   * @param {number} keyLength desired output buffer length
+   * @returns {Buffer}
+   */
+  async passphraseToKey (passphrase: string, salt: Buffer, keyLength: number): Promise<Buffer> {
+    const secret = Buffer.from(passphrase.normalize('NFC'), 'utf8')
+
+    return scryptsy(
+      secret,
+      salt,
+      this.params.N,
+      this.params.r,
+      this.params.p,
+      keyLength
+    )
+  }
+}
+
+export const scrypt = new Scrypt(new SimpleScryptsy())
