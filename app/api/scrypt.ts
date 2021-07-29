98456b0f3a1d436726f1ff1109319d52207613b2
ivan <ivan@zynesis.com>
2021-07-29 12:56:48 +0800

    try manual select module (by platform.os) for bundling

--- app/api/scrypt.ts
+++ app/api/scrypt.ts
@@ -1,3 +1,7 @@
 import { SimpleScryptsy, Scrypt } from '@defichain/jellyfish-wallet-encrypted'
+import { Platform } from 'react-native'
+import { scrypt as nativeScrypt } from './scrypt.native'
 
-export const scrypt = new Scrypt(new SimpleScryptsy())
+export const scrypt = Platform.OS === 'web'
+  ? new Scrypt(new SimpleScryptsy())
+  : nativeScrypt
