391d04bcccf51b9bd3aac6dcc8e547f93d960aa0
ivan <ivan@zynesis.com>
2021-07-29 13:49:57 +0800

    temp disable web scryptsy

--- app/api/scrypt.ts
+++ app/api/scrypt.ts
@@ -1,8 +1,4 @@
-import { Scrypt } from '@defichain/jellyfish-wallet-encrypted'
-import { Platform } from 'react-native'
+// import { Scrypt } from '@defichain/jellyfish-wallet-encrypted'
 import { scrypt as nativeScrypt } from './scrypt.native'
 
-export const scrypt = Platform.OS === 'web'
-  // eslint-disable-next-line
-  ? new Scrypt(new (require('@defichain/jellyfish-wallet-encrypted')).SimpleScryptsy())
-  : nativeScrypt
+export const scrypt = nativeScrypt // new Scrypt(new SimpleScryptsy())
