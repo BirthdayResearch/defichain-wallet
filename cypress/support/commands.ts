9c0a641dabfd562cf87317fd7828e5bc31eee202
Keng Ye <kengyeleow@gmail.com>
2021-07-30 11:55:46 +0800

    test: import testing-library/cypress commands

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,3 +1,4 @@
+import '@testing-library/cypress/add-commands'
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
