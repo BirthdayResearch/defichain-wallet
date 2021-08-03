c65554847d861a43cbb3134fdf91f4aaf07d5115
Keng Ye <kengyeleow@gmail.com>
2021-08-03 16:16:16 +0800

    test: add unit test getTxURLByNetwork

--- app/api/wallet/network.test.ts
+++ app/api/wallet/network.test.ts
@@ -1,5 +1,5 @@
 import { EnvironmentNetwork } from "../../environment";
-import { getBip32Option } from "./network";
+import { getBip32Option, getTxURLByNetwork } from "./network";
 
 it('should resolve bip32 option for MainNet', () => {
   const bip32Options = getBip32Option(EnvironmentNetwork.MainNet)
@@ -19,4 +19,24 @@ it('should resolve bip32 option for LocalPlayground', () => {
 it('should resolve bip32 option for RemotePlayground', () => {
   const bip32Options = getBip32Option(EnvironmentNetwork.RemotePlayground)
   expect(bip32Options).toMatchSnapshot()
+})
+
+it('should return correct tx url for MainNet', () => {
+	const txUrl = getTxURLByNetwork(EnvironmentNetwork.MainNet, '123')
+	expect(txUrl).toMatchSnapshot()
+})
+
+it('should return correct tx url for TestNet', () => {
+	const txUrl = getTxURLByNetwork(EnvironmentNetwork.TestNet, '123')
+	expect(txUrl).toMatchSnapshot()
+})
+
+it('should return correct tx url for LocalPlayground', () => {
+	const txUrl = getTxURLByNetwork(EnvironmentNetwork.LocalPlayground, '123')
+	expect(txUrl).toMatchSnapshot()
+})
+
+it('should return correct tx url for RemotePlayground', () => {
+	const txUrl = getTxURLByNetwork(EnvironmentNetwork.RemotePlayground, '123')
+	expect(txUrl).toMatchSnapshot()
 })
