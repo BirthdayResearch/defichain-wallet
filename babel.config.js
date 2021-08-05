c734302fdfae98f75e4d1fc47d13d352a52f1088
Keng Ye <kengyeleow@gmail.com>
2021-08-05 17:47:00 +0800

    build: use alias for import path
    To compile successfully, an alias has to be added in babel.config.js
    
    To provide ts code completion in IDE, alias has to be added in tsconfig.json
    
    Change import of OceanInterface as an example
    
    Ref: Issue #316

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,32 @@
 module.exports = function (api) {
   api.cache(true)
+  const plugins = [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@api': './app/api',
+          '@assets': './app/assets',
+          '@constants': './app/constants',
+          '@contexts': './app/contexts',
+          '@components': './app/components',
+          '@environment': './app/environment',
+          '@hooks': './app/hooks',
+          '@screens': './app/screens',
+          '@store': './app/store',
+          '@translations': './app/translations',
+          '@tailwind': './app/tailwind'
+        }
+      }
+    ]
+  ]
+
+  if (process.env.CYPRESS_E2E) {
+    plugins.push('istanbul')
+  }
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: process.env.CYPRESS_E2E ? ['istanbul'] : []
+    plugins: plugins
   }
 }
