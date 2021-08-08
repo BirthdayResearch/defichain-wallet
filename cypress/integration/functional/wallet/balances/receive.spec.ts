23bad958a316a9f3182b355110c63afb7e2ef235
thedoublejay <josejaimecareers@gmail.com>
2021-08-08 18:57:48 +0800

    breaking receive tests

--- cypress/integration/functional/wallet/balances/receive.spec.ts
+++ cypress/integration/functional/wallet/balances/receive.spec.ts
@@ -1,7 +1,7 @@
 context('Wallet - Receive', () => {
   before(function () {
     cy.createEmptyWallet(true)
-    cy.sendDFItoWallet()
+    cy.sendDFItoWallet().wait(10000)
     cy.fetchWalletBalance()
   })
 
