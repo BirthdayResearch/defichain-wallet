e5976095e82f845a9f7d8ea8795c0a6130d2fe4c
JJ Adonis <josejaimecareers@gmail.com>
2021-08-11 12:01:22 +0800

    Update app/screens/AppNavigator/screens/Settings/screens/ConfirmPinScreen.tsx

--- app/screens/AppNavigator/screens/Settings/screens/ConfirmPinScreen.tsx
+++ app/screens/AppNavigator/screens/Settings/screens/ConfirmPinScreen.tsx
@@ -36,7 +36,7 @@ export function ConfirmPinScreen ({ route }: Props): JSX.Element {
     }
 
     const copy = { words, network, pin }
-    setSpinnerMessage(translate('screens/PinConfirmation', 'Encrypting wallet...'))
+    setSpinnerMessage(translate('screens/PinConfirmation', 'Updating pin code...'))
     setTimeout(() => {
       MnemonicEncrypted.toData(copy.words, copy.network, copy.pin)
         .then(async encrypted => {
