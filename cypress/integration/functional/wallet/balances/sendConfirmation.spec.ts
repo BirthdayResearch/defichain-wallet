0fe090719341ffa642f1987c0bf901525158ebdf
thedoublejay <josejaimecareers@gmail.com>
2021-08-12 21:16:33 +0800

    update

--- cypress/integration/functional/wallet/balances/sendConfirmation.spec.ts
+++ cypress/integration/functional/wallet/balances/sendConfirmation.spec.ts
@@ -16,8 +16,6 @@ context('Wallet - Send Preview/Confirmation', () => {
     cy.getByTestID('text_destination').invoke('text').should(t => expect(t).equal('bcrt1q8rfsfny80jx78cmk4rsa069e2ckp6rn83u6ut9'))
     cy.getByTestID('text_network').invoke('text').should(t => expect(t).equal('Local'))
     cy.getByTestID('text_amount').invoke('text').should(t => expect(t).equal('1.23400000 BTC'))
-    cy.getByTestID('text_fee').invoke('text').should(t => expect(t).equal('0.00005000 DFI'))
-    cy.getByTestID('text_balance').invoke('text').should(t => expect(t).equal('8.76595000 BTC'))
     cy.getByTestID('button_confirm_send').should('not.have.attr', 'disabled')
   })
 })
