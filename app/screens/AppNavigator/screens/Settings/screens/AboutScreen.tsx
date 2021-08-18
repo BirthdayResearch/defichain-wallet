f164cca150c9aca11fcf3b52f0ca191b6dbe4225
kyleleow <40191153+kyleleow@users.noreply.github.com>
2021-08-18 17:13:42 +0800

    add social media links on about page (#658)
    * feat: add social link to about page
    
    Fix: #643
    
    * feat: add announcement telegram link
    
    * test: update unit test snapshot
    
    * test: update e2e test to include new link

--- app/screens/AppNavigator/screens/Settings/screens/AboutScreen.tsx
+++ app/screens/AppNavigator/screens/Settings/screens/AboutScreen.tsx
@@ -1,3 +1,4 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { nativeApplicationVersion } from 'expo-application'
 import React, { useCallback } from 'react'
 import { Linking, ScrollView, TouchableOpacity, View } from 'react-native'
@@ -13,6 +14,12 @@ interface AboutScreenLinks {
   url: string
 }
 
+interface AboutScreenSocialLinks {
+  testID: string
+  iconName: React.ComponentProps<typeof MaterialCommunityIcons>['name']
+  url: string
+}
+
 const ABOUT_LINKS: AboutScreenLinks[] = [
   { testID: 'white_paper', title: 'White Paper', url: 'https://defichain.com/white-paper/' },
   { testID: 'privacy_policy_button', title: 'Privacy Policy', url: 'https://defichain.com/privacy-policy/' },
@@ -21,6 +28,11 @@ const ABOUT_LINKS: AboutScreenLinks[] = [
     title: 'Licenses',
     url: 'https://app.fossa.com/projects/git%2Bgithub.com%2FDeFiCh%2Fwallet/refs/branch/main/eefb43ca2933838df8d16ad8c3b2b92db3278843/browse/licenses'
   }
+]
+
+const SOCIAL_LINKS: AboutScreenSocialLinks[] = [
+  { testID: 'youtube_social', iconName: 'youtube', url: 'https://www.youtube.com/channel/UCL635AjCJe6gNOD7Awlv4ug' },
+  { testID: 'twitter_social', iconName: 'twitter', url: 'https://twitter.com/defichain' }
 ]
 
 export function AboutScreen (): JSX.Element {
@@ -34,6 +46,11 @@ export function AboutScreen (): JSX.Element {
         <Text style={tailwind('text-base font-light text-black')}>
           {`v${nativeApplicationVersion ?? '0.0.0'}`}
         </Text>
+        <View style={tailwind('flex-row justify-center pt-5')}>
+          {
+            SOCIAL_LINKS.map((link) => <SocialIcon {...link} key={link.testID} />)
+          }
+        </View>
       </View>
       <SectionTitle text={translate('screens/AboutScreen', 'LINKS')} testID='links_title' />
       {
@@ -60,6 +77,25 @@ function LinkItemRow ({ title, url, testID }: AboutScreenLinks): JSX.Element {
       <Text style={tailwind('font-semibold text-primary')}>
         {translate('screens/AboutScreen', title)}
       </Text>
+    </TouchableOpacity>
+  )
+}
+
+function SocialIcon ({ iconName, url, testID }: AboutScreenSocialLinks): JSX.Element {
+  const handlePress = useCallback(async () => {
+    const supported = await Linking.canOpenURL(url)
+    if (supported) {
+      await Linking.openURL(url)
+    }
+  }, [url])
+
+  return (
+    <TouchableOpacity
+      style={tailwind('bg-primary justify-center items-center rounded-full w-8 h-8 mx-2')}
+      onPress={handlePress}
+      testID={testID}
+    >
+      <MaterialCommunityIcons name={iconName} size={20} style={tailwind('text-gray-100 pl-px')} />
     </TouchableOpacity>
   )
 }
