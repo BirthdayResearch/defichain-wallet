6d0352110777405755919a0dbe2cf1e37d403172
Keng Ye <kengyeleow@gmail.com>
2021-08-20 19:04:35 +0800

    feat: remove reload button
    useNetInfo hook will re-render when connection state changes

--- app/screens/ConnectionBoundary/ConnectionBoundary.tsx
+++ app/screens/ConnectionBoundary/ConnectionBoundary.tsx
@@ -2,27 +2,21 @@ import { MaterialIcons } from '@expo/vector-icons'
 import { useNetInfo } from '@react-native-community/netinfo'
 import React from 'react'
 import { Text, View } from '../../components'
-import { Button } from '../../components/Button'
 import { tailwind } from '../../tailwind'
 import { translate } from '../../translations'
 
 export default function ConnectionBoundary (props: React.PropsWithChildren<any>): JSX.Element | null {
   const netInfo = useNetInfo()
-
-  const onReload = (): void => {
-    // do nothing because useNetInfo hook would have caused a re-render if connection changes
-  }
-
   const noConnection = (): boolean => {
     return netInfo.isConnected === false || netInfo.isConnected === null
   }
 
   return (
-    noConnection() ? <ConnectionErrorComponent onPress={onReload} /> : props.children
+    noConnection() ? <ConnectionErrorComponent /> : props.children
   )
 }
 
-function ConnectionErrorComponent (props: {onPress: () => void}): JSX.Element {
+function ConnectionErrorComponent (): JSX.Element {
   return (
     <View
       testID='connection_error'
@@ -33,14 +27,8 @@ function ConnectionErrorComponent (props: {onPress: () => void}): JSX.Element {
         {translate('screens/ConnectionBoundary', 'Network error')}
       </Text>
       <Text style={tailwind('text-sm pb-16 text-center opacity-60')}>
-        {translate('screens/ErrorBoundary', 'Please check your internet connection and try reloading this page after a few minutes')}
+        {translate('screens/ErrorBoundary', 'Please check your internet connection and try again')}
       </Text>
-      <Button
-        testID='button_reload'
-        title='Reload'
-        onPress={props.onPress}
-        label={translate('screens/TransactionsScreen', 'RELOAD')}
-      />
     </View>
   )
 }
