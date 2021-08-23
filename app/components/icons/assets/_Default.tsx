fcbc3986b0cb6dd73eebe5aa7fede4d41df770b5
Harsh <harshrathi.dev@gmail.com>
2021-08-23 15:08:43 +0530

    Updated default token symbol to match light wallet (#672)

--- app/components/icons/assets/_Default.tsx
+++ app/components/icons/assets/_Default.tsx
@@ -1,21 +1,24 @@
-import randomColor from 'randomcolor'
 import * as React from 'react'
-import Svg, { Circle, SvgProps, Text } from 'react-native-svg'
+import randomColor from 'randomcolor'
+import { tailwind } from '../../../tailwind'
 
-export function _Default (symbol: string): (props: SvgProps) => JSX.Element {
-  return (props: SvgProps): JSX.Element => {
-    const color = randomColor({ luminosity: 'dark', seed: symbol })
-    const startingLetter = symbol?.substring(0, 1) ?? 'T'
+export function _Default (symbol: string): (props: React.SVGProps<SVGSVGElement>) => JSX.Element {
+  return (props: React.SVGProps<SVGSVGElement>): JSX.Element => {
+    const { style } = props
+    const height = '32px'
+    const width = '32px'
+    const bg = randomColor({ luminosity: 'bright', format: 'rgba', seed: symbol, alpha: 0.2 })
+    const text = randomColor({ luminosity: 'dark', format: 'rgba', seed: symbol, alpha: 100 })
+    const first = symbol?.substring(0, 1)
+
     return (
-      <Svg width={32} height={32} viewBox='0 0 32 32' {...props}>
-        <Circle cx={16} cy={16} r={16} fill={color} />
-        <Text
-          x='50%' y='50%' alignmentBaseline='central' textAnchor='middle' fontWeight='bolder'
-          fontSize='24' fill='#ffffff'
-        >
-          {startingLetter}
-        </Text>
-      </Svg>
+      <div style={{ height, width, ...style }}>
+        <div style={{ backgroundColor: bg, ...tailwind('rounded-full w-full h-full') }}>
+          <div style={tailwind('w-full h-full flex items-center')}>
+            <div style={{ color: text, ...tailwind('flex-1 w-1 text-center font-semibold') }}>{first}</div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
