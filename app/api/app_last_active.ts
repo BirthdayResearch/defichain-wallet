c78fd10f9ac32cd43260d5e60cdaed3a9bc642c7
Ivan Lee <ivan@zynesis.com>
2021-08-23 16:41:57 +0800

    use persistent data for privacy timeout implementation

---
+++ app/api/app_last_active.ts
@@ -0,0 +1,30 @@
+import { EnvironmentName, getEnvironment } from '../environment'
+import { StorageAPI } from './storage'
+
+const KEY = 'APP_LAST_ACTIVE.timestamp'
+
+async function set (): Promise<void> {
+  return await StorageAPI.setItem(KEY, `${Date.now()}`)
+}
+
+async function shouldReauthenticate (): Promise<boolean> {
+  const lastActive = await StorageAPI.getItem(KEY)
+  if (lastActive === null) return false
+  return Number(lastActive) + getTimeoutPeriod() < Date.now()
+}
+
+function getTimeoutPeriod (): number {
+  const env = getEnvironment()
+  if (env.name === EnvironmentName.Development) {
+    return 3000
+  }
+  return 60000
+}
+
+/**
+ * Failed passcode input counter persistence layer
+ */
+export const AppLastActiveTimestamp = {
+  set,
+  shouldReauthenticate
+}
--- app/screens/PrivacyLock.tsx
+++ app/screens/PrivacyLock.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react'
 import { AppState, BackHandler } from 'react-native'
+import { Logging } from '../api'
+import { AppLastActiveTimestamp } from '../api/app_last_active'
 import { useLocalAuthContext } from '../contexts/LocalAuthContext'
 
 export function PrivacyLock (): JSX.Element | null {
@@ -7,12 +9,22 @@ export function PrivacyLock (): JSX.Element | null {
 
   useEffect(() => {
     AppState.addEventListener('change', nextState => {
-      if (localAuth.isPrivacyLock === true) {
-        if (nextState === 'active') {
-          const backHandler = BackHandler.addEventListener('hardwareBackPress', () => null)
-          localAuth.privacyLock()
-            .catch(e => BackHandler.exitApp())
-            .finally(() => backHandler.remove())
+      if (nextState === 'background' || nextState === 'inactive') {
+        AppLastActiveTimestamp.set()
+          .then()
+          .catch(error => Logging.error(error))
+      } else {
+        if (nextState === 'active' && localAuth.isPrivacyLock === true) {
+          AppLastActiveTimestamp.shouldReauthenticate()
+            .then(async authReq => {
+              if (authReq) {
+                const backHandler = BackHandler.addEventListener('hardwareBackPress', () => null)
+                localAuth.privacyLock()
+                  .catch(e => BackHandler.exitApp())
+                  .finally(() => backHandler.remove())
+              }
+            })
+            .catch(error => Logging.error(error))
         }
       }
     })
