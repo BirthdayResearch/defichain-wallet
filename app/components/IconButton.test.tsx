9ca09bd367906f4290d9f2394d619f5eb4b4cc78
Keng Ye <kengyeleow@gmail.com>
2021-08-26 12:48:12 +0800

    test: add snapshot testing for IconButton

---
+++ app/components/IconButton.test.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import { render } from '@testing-library/react-native'
+import { IconButton } from './IconButton'
+import { MaterialIcons } from '@expo/vector-icons'
+
+const buttonName: React.ComponentProps<typeof MaterialIcons>['name'][] = ['swap-vert', 'swap-horiz', 'add', 'remove']
+
+describe('icon button', () => {
+	buttonName.forEach(name => {
+		it(`should match snapshot of button with ${name} icon`, async() => {
+			const onPress = jest.fn()
+			const rendered = render(<IconButton testID="test" onPress={onPress} materialIconName={name} size={24} />)
+			expect(rendered.toJSON()).toMatchSnapshot()
+		})
+	})
+})
