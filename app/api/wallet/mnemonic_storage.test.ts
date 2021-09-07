0d12217ebfce0b0ca8e9f8be3108ea4f3b04988e
Keng Ye <kengyeleow@gmail.com>
2021-09-07 11:20:13 +0800

    test: remove unused function

--- app/api/wallet/mnemonic_storage.test.ts
+++ app/api/wallet/mnemonic_storage.test.ts
@@ -1,16 +1,7 @@
 import { MnemonicStorage } from './mnemonic_storage'
 
 describe('EncryptedMnemonicStorage', () => {
-  function generateText (length: number, sequence: string): string {
-    let text = ''
-    for (let i = 0; i < length; i++) {
-      text += sequence
-    }
-    return text
-  }
-
-  it('should throw error when failed to retrieve encrypted value from secure store', async () => {
-    const text = generateText(2047, '0')
-    await expect(MnemonicStorage.get(text)).rejects.toThrowError('NO_MNEMONIC_BACKUP')
+  it('should throw error when failed to retrieve entropy from secure store', async () => {
+    await expect(MnemonicStorage.get('foo')).rejects.toThrowError('NO_MNEMONIC_BACKUP')
   })
 })
