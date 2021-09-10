26f223f886f18d368bffd65b92f13fdaea777fbd
Keng Ye <kengyeleow@gmail.com>
2021-09-10 19:53:35 +0800

    translation: updated german entries

--- app/components/RecoveryWordsFaq.tsx
+++ app/components/RecoveryWordsFaq.tsx
@@ -21,15 +21,15 @@ export function RecoveryWordFaq (): JSX.Element {
       content: translate('components/RecoveryWordFaq', 'Once you lose your recovery words, you will not be able to restore your wallet and any funds within it. We encourage you to be responsible with your recovery words.')
     },
     {
-      title: translate('components/RecoveryWordFaq', 'Can I use the 24-word from xxx that I created from xxx?'),
-      content: translate('components/RecoveryWordFaq', 'No, reusing 24-word from another computer, phone tablet, software, hardware, wallet provider will only be as secure as where you re-used it from.\n\nYou can only trust the 24-word if you can trust the device/app that generated it.\n\nYou must only enter the 24-word into a device that you trust isn\'t tamper with.')
+      title: translate('components/RecoveryWordFaq', 'Can I use the 24-words from xxx that I created from xxx?'),
+      content: translate('components/RecoveryWordFaq', 'No, reusing 24-words from another computer, phone tablet, software, hardware, wallet provider will only be as secure as where you re-used it from.\n\nYou can only trust the 24-word if you can trust the device/app that generated it.\n\nYou must only enter the 24-word into a device that you trust isn\'t tamper with.')
     },
     {
       title: translate('components/RecoveryWordFaq', 'Can someone help me if I lose my recovery words?'),
-      content: translate('components/RecoveryWordFaq', 'No. You are the only one who has the access to your recovery words. No one is responsible and liable for it except you.')
+      content: translate('components/RecoveryWordFaq', 'No. You have sole access to your recovery words. No one is responsible and liable for it except you.')
     },
     {
-      title: translate('components/RecoveryWordFaq', 'Why is it recommended to keep my recovery words offline and in paper?'),
+      title: translate('components/RecoveryWordFaq', 'Why is it recommended to keep my recovery words offline and on paper?'),
       content: translate('components/RecoveryWordFaq', 'Storing it in digital networks could make it accessible to hackers. It is best to write it in paper and to keep it secure and private.')
     }
   ]
