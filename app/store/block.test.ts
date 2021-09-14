a8c422b04d1abc4297dc81c8d84cdc684be5b3b8
Harsh <harshrathi.dev@gmail.com>
2021-09-14 20:12:34 +0530

    Updated test cases

--- app/store/block.test.ts
+++ app/store/block.test.ts
@@ -27,16 +27,16 @@ describe('block reducer', () => {
   it('should handle updateBlock', () => {
     const payload = { count: 99, masterNodeCount: 0, lastSync: date }
     const actual = block.reducer(initialState, block.actions.updateBlockDetails(payload))
-    expect(actual).toStrictEqual({ ...payload, isPolling: false, connected: false })
+    expect(actual).toStrictEqual({ ...initialState, ...payload })
   })
 
   it('should handle setConnected', () => {
     const actual = block.reducer(initialState, block.actions.setConnected(true))
-    expect(actual).toStrictEqual({ count: 77, isPolling: false, connected: true })
+    expect(actual).toStrictEqual({ ...initialState, count: 77, isPolling: false, connected: true })
   })
 
   it('should handle setPolling', () => {
     const actual = block.reducer(initialState, block.actions.setPolling(true))
-    expect(actual).toStrictEqual({ count: 77, isPolling: true, connected: false })
+    expect(actual).toStrictEqual({ ...initialState, count: 77, isPolling: true, connected: false })
   })
 })
