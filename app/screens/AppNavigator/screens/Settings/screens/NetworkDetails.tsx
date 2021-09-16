6455a2640885009121df0492d2acffd6a2819eb0
Harsh <harshrathi.dev@gmail.com>
2021-09-16 19:17:45 +0530

    Removed sectionlist for network details

--- app/screens/AppNavigator/screens/Settings/screens/NetworkDetails.tsx
+++ app/screens/AppNavigator/screens/Settings/screens/NetworkDetails.tsx
@@ -4,114 +4,69 @@ import React from 'react'
 import dayjs from 'dayjs'
 import { useSelector } from 'react-redux'
 import { View } from '../../../../../components'
-import { ThemedText, ThemedSectionTitle, ThemedSectionList, ThemedScrollView, ThemedView } from '../../../../../components/themed'
+import { ThemedText, ThemedSectionTitle, ThemedScrollView, ThemedView } from '../../../../../components/themed'
 import { tailwind } from '../../../../../tailwind'
 import { translate } from '../../../../../translations'
-
-enum NETWORK {
-  ConnectedStatus = 'YOU ARE CURRENTLY CONNECTED TO',
-  NetworkDetails = 'NETWORK DETAILS'
-}
-
-interface StatItemProps {
-  label: string
-  status: string
-  statusIcon?: JSX.Element
-  testID: string
-}
+import { TextRow } from '@components/TextRow'
+import { NumberRow } from '@components/NumberRow'
 
 export function NetworkDetails (): JSX.Element {
   const { network } = useNetworkContext()
   const { connected, count: blockCount, masternodeCount, lastSync } = useSelector((state: RootState) => state.block)
   const syncFormattedDate = (lastSync != null) ? dayjs(lastSync).format('MMM D, h:mm a') : ''
 
-  const statsData = [
-    {
-      key: NETWORK.ConnectedStatus,
-      data: [
-        {
-          label: translate('screens/NetworkDetails', 'Network'),
-          status: network,
-          testID: 'network'
-        }, {
-          label: translate('screens/NetworkDetails', 'Status'),
-          status: translate('screens/NetworkDetails', connected ? 'Connected' : 'Disconnected'),
-          testID: 'status',
-          statusIcon: (
-            <View
-              style={tailwind(`h-2 w-2 rounded-full ${connected ? 'bg-green-500' : 'bg-red-500'} mr-1.5`)}
-              testID='status_icon_network'
-            />)
-        }
-      ]
-    }, {
-      key: NETWORK.NetworkDetails,
-      data: [
-        {
-          label: translate('screens/NetworkDetails', 'Last synced'),
-          status: syncFormattedDate,
-          testID: 'last_sync'
-        }, {
-          label: translate('screens/NetworkDetails', 'Block height'),
-          status: blockCount,
-          testID: 'block_height'
-        }, {
-          label: translate('screens/NetworkDetails', 'Total Masternodes'),
-          status: masternodeCount,
-          testID: 'total_masternodes'
-        }
-      ]
-    }
-  ]
-
   return (
     <ThemedScrollView testID='network_details'>
-      <ThemedSectionList
-        keyExtractor={(item: StatItemProps, index: number) => `${index}`}
-        renderItem={({ item }): JSX.Element => {
-         return <StatItemRow {...item} />
-        }}
-        renderSectionHeader={({ section }) => {
-          switch (section.key) {
-            case NETWORK.ConnectedStatus:
-              return (
-                <ThemedSectionTitle
-                  testID='network_detail_title'
-                  text={translate('screens/NetworkDetails', section.key)}
-                />
-              )
-            case NETWORK.NetworkDetails:
-              return (
-                <ThemedSectionTitle
-                  testID={section.key}
-                  text={translate('screens/NetworkDetails', section.key)}
-                />
-              )
-            default:
-              return <></>
-          }
-        }}
-        sections={statsData}
-        testID='network_detail_screen_list'
+
+      <ThemedSectionTitle
+        testID='current_connection_detail_title'
+        text={translate('screens/NetworkDetails', 'YOU ARE CURRENTLY CONNECTED TO')}
+      />
+
+      <TextRow
+        lhs={translate('screens/NetworkDetails', 'Network')}
+        rhs={{ value: network, testID: 'network' }}
+      />
+
+      <NetworkStatusRow connected={connected} />
+
+      <ThemedSectionTitle
+        testID='network_detail_title'
+        text={translate('screens/NetworkDetails', 'NETWORK DETAILS')}
+      />
+
+      <TextRow
+        lhs={translate('screens/NetworkDetails', 'Last synced')}
+        rhs={{ value: syncFormattedDate, testID: 'last_sync' }}
+      />
+
+      <NumberRow
+        lhs={translate('screens/NetworkDetails', 'Block height')}
+        rightHandElements={[{ value: blockCount ?? '', testID: 'block_height' }]}
+      />
+
+      <NumberRow
+        lhs={translate('screens/NetworkDetails', 'Total Masternodes')}
+        rightHandElements={[{ value: masternodeCount ?? '', testID: 'total_masternodes' }]}
       />
     </ThemedScrollView>
   )
 }
 
-function StatItemRow ({ label, status, testID, statusIcon }: StatItemProps): JSX.Element {
+function NetworkStatusRow ({ connected }: {connected: boolean}): JSX.Element {
   return (
     <ThemedView
       dark={tailwind('bg-gray-800 border-b border-gray-700')}
       light={tailwind('bg-white border-b border-gray-200')}
-      style={tailwind('flex flex-row p-4 pr-2 items-center justify-between')}
+      style={tailwind('flex flex-row p-4 items-center justify-between w-full')}
     >
       <ThemedText
         dark={tailwind('text-gray-200')}
         light={tailwind('text-black')}
         style={tailwind('font-medium')}
-        testID={`${testID}_label`}
+        testID='status_label'
       >
-        {label}
+        {translate('screens/NetworkDetails', 'Status')}
       </ThemedText>
 
       <ThemedView
@@ -119,14 +74,17 @@ function StatItemRow ({ label, status, testID, statusIcon }: StatItemProps): JSX
         light={tailwind('text-gray-600')}
         style={tailwind('flex-row items-center')}
       >
-        {statusIcon}
+        <View
+          style={tailwind(`h-3 w-3 rounded-full ${connected ? 'bg-green-500' : 'bg-red-500'} mr-1.5`)}
+          testID='status_icon_network'
+        />
         <ThemedText
           dark={tailwind('text-gray-400')}
-          light={tailwind('text-gray-600')}
-          style={tailwind('text-sm font-medium text-gray-600')}
-          testID={`${testID}_value`}
+          light={tailwind('text-gray-500')}
+          style={tailwind('font-medium text-right')}
+          testID='status_value'
         >
-          {status}
+          {translate('screens/NetworkDetails', connected ? 'Connected' : 'Disconnected')}
         </ThemedText>
       </ThemedView>
     </ThemedView>
