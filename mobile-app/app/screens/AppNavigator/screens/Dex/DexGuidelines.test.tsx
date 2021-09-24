c497f863c828faabf0c93ce2c5ab040c277802e9
Harsh <harshrathi.dev@gmail.com>
2021-09-24 15:58:08 +0530

    Updated unit test case

--- mobile-app/app/screens/AppNavigator/screens/Dex/DexGuidelines.test.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Dex/DexGuidelines.test.tsx
@@ -3,25 +3,12 @@ import * as React from 'react'
 import { DexGuidelines } from './DexGuidelines'
 
 jest.mock('../../../../contexts/ThemeProvider')
-jest.mock('../../../../api', () => ({
-  DisplayDexGuidelinesPersistence: {
-    get: async () => {
-      return true
-    }
-  }
-}))
 
 describe('Dex guide', () => {
   it('should match snapshot', async () => {
-    const navigation: any = {
-      navigate: jest.fn()
-    }
-    const route: any = {}
+    const onClose = jest.fn
     const rendered = render(
-      <DexGuidelines
-        navigation={navigation}
-        route={route}
-      />
+      <DexGuidelines onClose={onClose} />
     )
     await waitFor(() => rendered.toJSON() !== null)
     expect(rendered.toJSON()).toMatchSnapshot()
