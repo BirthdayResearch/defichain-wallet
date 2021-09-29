260f4912f56ce649a7443f2bd220f2901abf43aa
Keng Ye <kengyeleow@gmail.com>
2021-09-29 10:26:10 +0800

    revert: classNames()

--- mobile-app/app/components/IconButton.tsx
+++ mobile-app/app/components/IconButton.tsx
@@ -3,7 +3,6 @@ import * as React from 'react'
 import { TouchableOpacityProps } from 'react-native'
 import { tailwind } from '@tailwind'
 import { ThemedIcon, ThemedText, ThemedTouchableOpacity } from './themed'
-import classNames from 'classnames'
 
 type IconType = 'MaterialCommunityIcons' | 'MaterialIcons'
 
@@ -21,24 +20,24 @@ export function IconButton (props: IconButtonProps): JSX.Element {
   } = props
   return (
     <ThemedTouchableOpacity
-      light={tailwind(classNames({ 'border-gray-300 bg-white': !disabled, 'border-gray-100 bg-gray-100': disabled }))}
-      dark={tailwind(classNames({ 'border-gray-400 bg-gray-900': !disabled, 'border-gray-800 bg-gray-800': disabled }))}
+      light={tailwind({ 'border-gray-300 bg-white': !disabled, 'border-gray-100 bg-gray-100': disabled })}
+      dark={tailwind({ 'border-gray-400 bg-gray-900': !disabled, 'border-gray-800 bg-gray-800': disabled })}
       onPress={props.onPress}
       style={[tailwind('p-1 flex-row items-center border rounded'), props.style]}
       testID={props.testID}
       disabled={props.disabled}
     >
       <ThemedIcon
-        light={tailwind(classNames({ 'text-primary-500': !disabled, 'text-gray-300': disabled }))}
-        dark={tailwind(classNames({ 'text-darkprimary-500': !disabled, 'text-gray-600': disabled }))}
+        light={tailwind({ 'text-primary-500': !disabled, 'text-gray-300': disabled })}
+        dark={tailwind({ 'text-darkprimary-500': !disabled, 'text-gray-600': disabled })}
         iconType={props.iconType}
         name={props.iconName}
         size={props.iconSize}
       />
       {props.iconLabel !== undefined &&
         <ThemedText
-          light={tailwind(classNames({ 'text-primary-500': !disabled, 'text-gray-300': disabled }))}
-          dark={tailwind(classNames({ 'text-darkprimary-500': !disabled, 'text-gray-600': disabled }))}
+          light={tailwind({ 'text-primary-500': !disabled, 'text-gray-300': disabled })}
+          dark={tailwind({ 'text-darkprimary-500': !disabled, 'text-gray-600': disabled })}
           style={tailwind('px-1 text-sm font-medium')}
         >
           {props.iconLabel}
