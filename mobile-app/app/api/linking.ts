67205595b713a43ca65835c33d891a8c8b66d0a9
thedoublejay <josejaimecareers@gmail.com>
2021-09-30 13:49:47 +0800

    added intent

--- mobile-app/app/api/linking.ts
+++ mobile-app/app/api/linking.ts
@@ -1,9 +1,10 @@
+import { Platform } from 'react-native'
+import * as Linking from 'expo-linking'
 import * as WebBrowser from 'expo-web-browser'
 
 export async function openURL (url: string): Promise<void> {
-  await WebBrowser.openBrowserAsync(url)
-  /* if (Platform.OS === 'android') {
-
+  if (Platform.OS === 'android') {
+    await WebBrowser.openBrowserAsync(url)
   } else {
     const supported = await Linking.canOpenURL(url)
     if (supported) {
@@ -11,5 +12,5 @@ export async function openURL (url: string): Promise<void> {
     } else {
       await WebBrowser.openBrowserAsync(url)
     }
-  } */
+  }
 }
