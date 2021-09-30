6cc01655fe9d309fa1fe45385f713c20adb62ddc
thedoublejay <josejaimecareers@gmail.com>
2021-09-30 14:47:01 +0800

    update comment

--- mobile-app/app/api/linking.ts
+++ mobile-app/app/api/linking.ts
@@ -3,14 +3,12 @@ import * as Linking from 'expo-linking'
 import * as WebBrowser from 'expo-web-browser'
 
 export async function openURL (url: string): Promise<void> {
-  if (Platform.OS === 'android') {
-    await WebBrowser.openBrowserAsync(url)
-  } else {
+  if (Platform.OS !== 'android') {
     const supported = await Linking.canOpenURL(url)
     if (supported) {
       await Linking.openURL(url)
-    } else {
-      await WebBrowser.openBrowserAsync(url)
+      return
     }
   }
+  await WebBrowser.openBrowserAsync(url)
 }
