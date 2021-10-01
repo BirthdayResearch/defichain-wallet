daf9ab5d1ba8803ea219bc2d5c47dc38e96cb5b8
Keng Ye <kengyeleow@gmail.com>
2021-10-01 11:25:32 +0800

    feat: replace height with tailwind class

--- mobile-app/app/components/WalletTextInput.tsx
+++ mobile-app/app/components/WalletTextInput.tsx
@@ -110,10 +110,7 @@ function ClearButton (props: {onPress?: () => void, testID?: string}): JSX.Eleme
       <ThemedView
         light={tailwind('bg-gray-800')}
         dark={tailwind('bg-gray-100')}
-        style={[
-          tailwind('top-2 left-3 rounded-full absolute w-9/12 -z-1', { 'w-5/12': Platform.OS === 'web' }),
-          { height: 16 }
-        ]}
+        style={tailwind('top-2 left-3 rounded-full absolute w-9/12 h-4 -z-1', { 'w-5/12': Platform.OS === 'web' })}
       />
       <ThemedIcon
         iconType='MaterialIcons'
