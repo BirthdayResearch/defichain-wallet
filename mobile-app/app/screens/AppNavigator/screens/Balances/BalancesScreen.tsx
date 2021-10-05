16617460180f8c01cf702dacf0ab3d983e24eeb4
Keng Ye <kengyeleow@gmail.com>
2021-10-05 15:47:50 +0800

    feat: add dfi unified info in balance screen
    Fixes #554

--- mobile-app/app/screens/AppNavigator/screens/Balances/BalancesScreen.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Balances/BalancesScreen.tsx
@@ -1,3 +1,4 @@
+import { IconButton } from '@components/IconButton'
 import { getNativeIcon } from '@components/icons/assets'
 import { View } from '@components/index'
 import {
@@ -14,17 +15,21 @@ import { useWhaleApiClient } from '@contexts/WhaleContext'
 import { fetchTokens, useTokensAPI } from '@hooks/wallet/TokensAPI'
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
 import { StackScreenProps } from '@react-navigation/stack'
+import { RootState } from '@store'
 import { ocean } from '@store/ocean'
-import { WalletToken } from '@store/wallet'
+import { DFITokenSelector, DFIUtxoSelector, WalletToken } from '@store/wallet'
 import { tailwind } from '@tailwind'
 import { translate } from '@translations'
+import DFIBackground from '@assets/images/DFI_balance_background.png'
+import DFIBackgroundDark from '@assets/images/DFI_balance_background_dark.png'
 import BigNumber from 'bignumber.js'
 import * as React from 'react'
 import { useCallback, useEffect, useState } from 'react'
-import { RefreshControl } from 'react-native'
+import { ImageBackground, RefreshControl } from 'react-native'
 import NumberFormat from 'react-number-format'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { BalanceParamList } from './BalancesNavigator'
+import { useThemeContext } from '@contexts/ThemeProvider'
 
 type Props = StackScreenProps<BalanceParamList, 'BalancesScreen'>
 
@@ -35,6 +40,7 @@ export function BalancesScreen ({ navigation }: Props): JSX.Element {
   const [refreshing, setRefreshing] = useState(false)
   const dispatch = useDispatch()
   const { wallets } = useWalletPersistenceContext()
+  const { isLight } = useThemeContext()
 
   useEffect(() => {
     dispatch(ocean.actions.setHeight(height))
@@ -47,6 +53,23 @@ export function BalancesScreen ({ navigation }: Props): JSX.Element {
   }, [address, client, dispatch])
 
   const tokens = useTokensAPI()
+  const DFIToken = useSelector((state: RootState) => DFITokenSelector(state.wallet))
+  const DFIUtxo = useSelector((state: RootState) => DFIUtxoSelector(state.wallet))
+  const onConvertPress = (): void => {
+    navigation.navigate({
+      name: 'Convert',
+      params: { mode: 'utxoToAccount' },
+      merge: true
+    })
+  }
+  const onSendPress = (): void => {
+    navigation.navigate({
+      name: 'Send',
+      params: { token: DFIUtxo },
+      merge: true
+    })
+  }
+
   return (
     <ThemedFlatList
       ItemSeparatorComponent={() => (
@@ -57,10 +80,13 @@ export function BalancesScreen ({ navigation }: Props): JSX.Element {
         />
       )}
       ListHeaderComponent={(
-        <ThemedSectionTitle
-          testID='balances_title'
-          text={translate('screens/BalancesScreen', 'PORTFOLIO')}
-        />
+        <>
+          <DFIBalanceCard utxo={DFIUtxo} token={DFIToken} isLight={isLight} onConvertPress={onConvertPress} onSendPress={onSendPress} />
+          <ThemedSectionTitle
+            testID='balances_title'
+            text={translate('screens/BalancesScreen', 'PORTFOLIO')}
+          />
+        </>
       )}
       data={tokens}
       refreshControl={
@@ -148,5 +174,88 @@ function BalanceItemRow ({
         </View>
       </View>
     </ThemedTouchableOpacity>
+  )
+}
+
+interface DFIBalanceCardProps {
+  utxo: WalletToken
+  token: WalletToken
+  onConvertPress: () => void
+  onSendPress: () => void
+  isLight: boolean
+}
+
+function DFIBalanceCard (props: DFIBalanceCardProps): JSX.Element {
+  const DFIIcon = getNativeIcon('_UTXO')
+  const totalDFI = new BigNumber(props.utxo.amount).plus(new BigNumber(props.token.amount)).toFixed(8)
+
+  return (
+    <ThemedView
+      light={tailwind('bg-white border-gray-100')}
+      dark={tailwind('bg-gray-800')}
+      style={tailwind('mx-2 mt-4 rounded-lg flex-1')}
+    >
+      <ImageBackground source={props.isLight ? DFIBackground : DFIBackgroundDark} style={tailwind('flex-1 rounded-lg overflow-hidden')} resizeMode='cover' resizeMethod='scale'>
+        <View style={tailwind('flex-col flex-1 mx-4 mt-5 mb-4')}>
+          <View style={tailwind('flex-row pb-3 items-center')}>
+            <DFIIcon width={24} height={24} style={tailwind('mr-2')} />
+            <ThemedText style={tailwind('pr-9 text-lg font-bold')}>DFI</ThemedText>
+            <ThemedText>{totalDFI} DFI</ThemedText>
+          </View>
+
+          <View style={tailwind('flex-row pb-1.5')}>
+            <ThemedText
+              light={tailwind('text-gray-500')}
+              dark={tailwind('text-gray-400')}
+              style={tailwind('pr-16 text-sm')}
+            >
+              UTXO
+            </ThemedText>
+            <ThemedText
+              light={tailwind('text-gray-500')}
+              dark={tailwind('text-gray-400')}
+              style={tailwind('text-sm')}
+            >
+              {new BigNumber(props.utxo.amount).toFixed(8)}
+            </ThemedText>
+          </View>
+
+          <View style={tailwind('flex-row items-center flex-1')}>
+            <ThemedText
+              light={tailwind('text-gray-500')}
+              dark={tailwind('text-gray-400')}
+              style={tailwind('pr-14 text-sm')}
+            >
+              Token
+            </ThemedText>
+            <ThemedText
+              light={tailwind('text-gray-500')}
+              dark={tailwind('text-gray-400')}
+              style={tailwind('pl-1.5 text-sm')}
+            >
+              {new BigNumber(props.token.amount).toFixed(8)}
+            </ThemedText>
+
+            <View style={tailwind('flex-row flex-grow justify-end')}>
+              <IconButton
+                iconName='swap-vert'
+                iconSize={24}
+                iconType='MaterialIcons'
+                onPress={props.onConvertPress}
+                testID='swap_button'
+                style={tailwind('mr-2')}
+              />
+              <IconButton
+                iconName='arrow-upward'
+                iconSize={24}
+                iconType='MaterialIcons'
+                onPress={props.onSendPress}
+                testID='send_button'
+              />
+            </View>
+          </View>
+        </View>
+      </ImageBackground>
+    </ThemedView>
   )
 }
