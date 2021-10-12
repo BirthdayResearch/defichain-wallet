46a0e8344b8c047a9687f7037161e81e422a0b1a
Keng Ye <kengyeleow@gmail.com>
2021-10-12 17:45:28 +0800

    translation: add entries

--- mobile-app/app/screens/WalletNavigator/screens/CreateWallet/PasscodeFaq.tsx
+++ mobile-app/app/screens/WalletNavigator/screens/CreateWallet/PasscodeFaq.tsx
@@ -8,15 +8,15 @@ export function PasscodeFaq (): JSX.Element {
   const faqContent: AccordionContent[] = [
     {
       title: translate('components/PasscodeFaq', 'What happens if I forgot my passcode? Can someone help me to retrieve my passcode?'),
-      content: translate('components/PasscodeFaq', 'Your passcode is your own responsibility and no one, aside from you, knows your passcode.\n\nIt is important that you take care of your passcode and to change them from time to time.')
+      content: translate('components/PasscodeFaq', 'Your passcode is your responsibility, hence no one, aside from you, knows it. It is important that you keep your passcode safe and change it from time to time.')
     },
     {
-      title: translate('components/PasscodeFaq', 'Am I allowed to change my passcode?'),
-      content: translate('components/PasscodeFaq', 'Yes, you can change your passcode from your settings in-app. We recommend you to change your passcode regularly for maximum security.')
+      title: translate('components/PasscodeFaq', 'Can I change my passcode?'),
+      content: translate('components/PasscodeFaq', 'Yes, you can change your passcode from your settings in-app. We recommend changing of passcode regularly for maximum security.')
     },
     {
-      title: translate('components/PasscodeFaq', 'Is my passcode the same as with phone pin code?'),
-      content: translate('components/PasscodeFaq', 'Your passcode is only for DeFiChain wallet.')
+      title: translate('components/PasscodeFaq', 'Is passcode the same as phone PIN code?'),
+      content: translate('components/PasscodeFaq', 'Your passcode is valid only for DeFiChain wallet.')
     }
   ]
 
@@ -34,7 +34,7 @@ export function PasscodeFaq (): JSX.Element {
       <ThemedText
         style={tailwind('mt-2 text-sm')}
       >
-        {translate('components/PasscodeFaq', 'Your passcode is used to authorize any transaction happening in your wallet. They are a six-digit numeric code that will be asked from you every time.\n\nKeep your passcode private and secure. You are responsible for your own security.')}
+        {translate('components/PasscodeFaq', 'Your six-digit numeric code passcode, set by you, would be use to authorize all transactions. Keep your passcode private and secure. You are responsible for your own security.')}
       </ThemedText>
 
       <WalletAccordion
