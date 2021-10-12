49549a8f344c7d21260c6b1c2ee385a681a65441
Keng Ye <kengyeleow@gmail.com>
2021-10-12 17:09:44 +0800

    feat: add passcode faq content

--- mobile-app/app/screens/WalletNavigator/screens/CreateWallet/PasscodeFaq.tsx
+++ mobile-app/app/screens/WalletNavigator/screens/CreateWallet/PasscodeFaq.tsx
@@ -5,19 +5,18 @@ import { translate } from '@translations'
 import React from 'react'
 
 export function PasscodeFaq (): JSX.Element {
-  // TODO(kyleleow): update faq content, add translation
   const faqContent: AccordionContent[] = [
     {
-      title: translate('components/PasscodeFaq', 'What happens if I forgot my passcode?'),
-      content: translate('components/PasscodeFaq', '')
+      title: translate('components/PasscodeFaq', 'What happens if I forgot my passcode? Can someone help me to retrieve my passcode?'),
+      content: translate('components/PasscodeFaq', 'Your passcode is your own responsibility and no one, aside from you, knows your passcode.\n\nIt is important that you take care of your passcode and to change them from time to time.')
     },
     {
       title: translate('components/PasscodeFaq', 'Am I allowed to change my passcode?'),
-      content: translate('components/PasscodeFaq', '')
+      content: translate('components/PasscodeFaq', 'Yes, you can change your passcode from your settings in-app. We recommend you to change your passcode regularly for maximum security.')
     },
     {
-      title: translate('components/PasscodeFaq', 'Can I use the 24-word that I created from another wallet?'),
-      content: translate('components/PasscodeFaq', '')
+      title: translate('components/PasscodeFaq', 'Is my passcode the same as with phone pin code?'),
+      content: translate('components/PasscodeFaq', 'Your passcode is only for DeFiChain wallet.')
     }
   ]
 
@@ -35,7 +34,7 @@ export function PasscodeFaq (): JSX.Element {
       <ThemedText
         style={tailwind('mt-2 text-sm')}
       >
-        {translate('components/PasscodeFaq', 'Your passcode is used to authorize any transaction happening in your wallet.')}
+        {translate('components/PasscodeFaq', 'Your passcode is used to authorize any transaction happening in your wallet. They are a six-digit numeric code that will be asked from you every time.\n\nKeep your passcode private and secure. You are responsible for your own security.')}
       </ThemedText>
 
       <WalletAccordion
