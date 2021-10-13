bbe467608add77aba41b62f7fc70c5e296e88a86
Keng Ye <kengyeleow@gmail.com>
2021-10-13 17:35:18 +0800

    feat: add ConversionInfoText component

---
+++ mobile-app/app/components/ConversionInfoText.tsx
@@ -0,0 +1,18 @@
+import { translate } from '@translations'
+import React from 'react'
+import { StyleProp, ViewStyle } from 'react-native'
+import { InfoText } from './InfoText'
+
+interface ConversionInfoTextProps {
+  style?: StyleProp<ViewStyle>
+}
+
+export function ConversionInfoText (props: ConversionInfoTextProps): JSX.Element {
+  return (
+    <InfoText
+      testID='conversion_info_text'
+      text={translate('components/ConversionInfoText', 'Conversion will be required. Your passcode will be asked to authorize both transactions.')}
+      style={props.style}
+    />
+  )
+}
