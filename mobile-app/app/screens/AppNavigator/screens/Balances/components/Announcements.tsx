9362d708e62ff1e990304735c32aff5ce792d3d2
thedoublejay <josejaimecareers@gmail.com>
2021-10-15 17:38:15 +0800

    alert

--- mobile-app/app/screens/AppNavigator/screens/Balances/components/Announcements.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Balances/components/Announcements.tsx
@@ -2,25 +2,54 @@ import React from 'react'
 import { ThemedIcon, ThemedText, ThemedView } from '@components/themed'
 import { tailwind } from '@tailwind'
 import { useGetAnnouncementsQuery } from '@store/website'
+import { AnnouncementData } from '@shared-types/website'
+import { nativeApplicationVersion } from 'expo-application'
+import { satisfies } from 'semver'
+import { useLanguageContext } from '@contexts/LanguageProvider'
 
 export function Announcements (): JSX.Element {
   const {
-    data: announcement,
+    data: announcements,
     isSuccess
   } = useGetAnnouncementsQuery({})
-  if (isSuccess && announcement?.en !== undefined) {
-    return (
-      <ThemedView
-        style={tailwind('px-4 py-3 flex-row items-center')} light={tailwind('bg-warning-50')}
-        dark={tailwind('bg-darkwarning-50')}
-      >
-        <ThemedIcon style={tailwind('mr-2')} iconType='MaterialIcons' name='campaign' size={22} />
-        <ThemedText style={tailwind('text-xs flex-auto')}>
-          aowjdpawoijdpawojpdawojdpoawjdpoawjdpawjdpawjdpawjpdwajpdawjpdawojpdawojpdwajpdajwpdowjapdojwap 6:00am (SGT)
-        </ThemedText>
-      </ThemedView>
-    )
+  const {
+    language
+  } = useLanguageContext()
+  if (isSuccess && announcements !== undefined && announcements?.length > 0) {
+    const announcement = findAnnouncementForVersion(nativeApplicationVersion ?? '0.0.0', announcements, language)
+    if (announcement === undefined || announcement === '') {
+      return <></>
+    } else {
+      return (
+        <ThemedView
+          style={tailwind('px-4 py-3 flex-row items-center')} light={tailwind('bg-warning-50')}
+          dark={tailwind('bg-darkwarning-50')}
+        >
+          <ThemedIcon style={tailwind('mr-2')} iconType='MaterialIcons' name='campaign' size={22} />
+          <ThemedText style={tailwind('text-xs flex-auto')}>
+            {announcement}
+          </ThemedText>
+        </ThemedView>
+      )
+    }
   } else {
     return <></>
   }
+}
+
+function findAnnouncementForVersion (version: string, announcements: AnnouncementData[], language: string): string {
+  let announcementForVersion = ''
+  announcements.forEach((announcement) => {
+    const {
+      min,
+      max
+    } = announcement.version
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    const range = max !== undefined ? `${min} - ${max}` : `>=${min}`
+    if (satisfies(version, range)) {
+      const lang: any = announcement.lang
+      announcementForVersion = lang[language] ?? lang.en
+    }
+  })
+  return announcementForVersion
 }
--- shared/store/website.ts
+++ shared/store/website.ts
@@ -1,12 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { AnnouncementData } from '@shared-types/website'
 
 export const websiteSlice = createApi({
   reducerPath: 'website',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://deploy-preview-1063--wallet-defichain.netlify.app/api/v0'
+    baseUrl: 'https://wallet.defichain.com/api/v0'
   }),
   endpoints: builder => ({
-    getAnnouncements: builder.query({
+    getAnnouncements: builder.query<AnnouncementData[], any>({
       query: () => ({
         url: '/announcements',
         method: 'GET',
