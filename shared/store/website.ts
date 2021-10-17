68eef289ba0557a30a1ccbe0344ee951974449bf
thedoublejay <josejaimecareers@gmail.com>
2021-10-17 15:55:41 +0800

    revert website change

--- shared/store/website.ts
+++ shared/store/website.ts
@@ -4,7 +4,7 @@ import { AnnouncementData } from '@shared-types/website'
 export const websiteSlice = createApi({
   reducerPath: 'website',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://deploy-preview-1063--wallet-defichain.netlify.app/api/v0'
+    baseUrl: 'https://wallet.defichain.com/api/v0'
   }),
   endpoints: builder => ({
     getAnnouncements: builder.query<AnnouncementData[], any>({
