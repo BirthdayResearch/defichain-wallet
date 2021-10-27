ae3a7f87e4bf34ca080aa2c28f7f2fdbf403927c
Keng Ye <kengyeleow@gmail.com>
2021-10-27 17:31:28 +0800

    feat: add EmptyVault screen

--- mobile-app/app/screens/AppNavigator/screens/Loans/EmptyVault.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Loans/EmptyVault.tsx
@@ -1,4 +1,4 @@
-import { NavigationProp } from '@react-navigation/native'
+import { NavigationProp } from '@react-navigation/core'
 import * as React from 'react'
 import { RefreshControl } from 'react-native'
 import { Button } from '@components/Button'
@@ -6,6 +6,8 @@ import { ThemedIcon, ThemedScrollView, ThemedText } from '@components/themed'
 import { tailwind } from '@tailwind'
 import { translate } from '@translations'
 import { LoanParamList } from './LoansNavigator'
+import { InfoTextCTA } from '@components/InfoTextCTA'
+import { View } from '@components'
 
 interface EmptyVaultProps {
   navigation: NavigationProp<LoanParamList>
@@ -14,6 +16,11 @@ interface EmptyVaultProps {
 }
 
 export function EmptyVault (props: EmptyVaultProps): JSX.Element {
+  // const navigation = useNavigation<NavigationProp<LoanParamList>>()
+  const goToVaultsFaq = (): void => {
+    // TODO: add navigation to vault FAQ screen
+    // navigation.navigate('')
+  }
   return (
     <ThemedScrollView
       refreshControl={
@@ -22,14 +29,14 @@ export function EmptyVault (props: EmptyVaultProps): JSX.Element {
           refreshing={props.loadingStatus === 'loading'}
         />
       }
-      style={tailwind('px-8 pt-32 pb-2 text-center')}
+      contentContainerStyle={tailwind('px-8 pt-32 pb-2 text-center')}
       testID='empty_vault'
     >
       <ThemedIcon
         light={tailwind('text-black')}
         dark={tailwind('text-white')}
         iconType='MaterialCommunityIcons'
-        name='shield-off'
+        name='shield-plus'
         size={44}
         style={tailwind('pb-5 text-center')}
       />
@@ -38,16 +45,24 @@ export function EmptyVault (props: EmptyVaultProps): JSX.Element {
         {translate('screens/LoansScreen', 'No vault created')}
       </ThemedText>
 
-      <ThemedText style={tailwind('text-sm pb-16 text-center opacity-60')}>
+      <ThemedText style={tailwind('text-sm pb-4 text-center opacity-60')}>
         {translate('screens/LoansScreen', 'To get started, create a vault and add DFI and other tokens as collaterals')}
       </ThemedText>
 
       <Button
         label={translate('screens/LoansScreen', 'CREATE VAULT')}
-        onPress={() => props.navigation.navigate('CreateVault')}
+        onPress={() => /* props.navigation.navigate('CreateVault') */{}} // TODO: navigate to create vault screen
         testID='button_create_vault'
         title='Create vault'
+        margin='m-0 mb-8'
       />
+      <View style={tailwind('flex items-center')}>
+        <InfoTextCTA
+          onPress={goToVaultsFaq}
+          text='Learn more about vaults'
+          testId='empty_vault_learn_more'
+        />
+      </View>
     </ThemedScrollView>
   )
 }
