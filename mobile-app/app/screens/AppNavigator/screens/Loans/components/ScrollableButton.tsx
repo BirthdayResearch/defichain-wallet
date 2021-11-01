4fa79acf044aac94616c0c34d1ad435c77716cee
Keng Ye <kengyeleow@gmail.com>
2021-11-01 18:43:42 +0800

    feat: add scrollable button component

---
+++ mobile-app/app/screens/AppNavigator/screens/Loans/components/ScrollableButton.tsx
@@ -0,0 +1,48 @@
+import { View } from '@components'
+import { IconButton } from '@components/IconButton'
+import { IconType, ThemedScrollView } from '@components/themed'
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'
+import { tailwind } from '@tailwind'
+import React from 'react'
+
+interface ScrollableButtonProps {
+  buttons: ScrollButton[]
+}
+
+export interface ScrollButton {
+  label: string
+  iconType?: IconType
+  iconName?: React.ComponentProps<typeof MaterialIcons>['name'] | React.ComponentProps<typeof MaterialCommunityIcons>['name']
+  disabled?: boolean
+}
+
+export function ScrollableButton (props: ScrollableButtonProps): JSX.Element | null {
+  if (props.buttons.length === 0) {
+    return null
+  }
+
+  return (
+    <View
+      style={tailwind('h-8')}
+    >
+      <ThemedScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        light={tailwind('bg-white')}
+        dark={tailwind('bg-gray-800')}
+        style={tailwind('flex flex-row flex-1')}
+      >
+        {props.buttons.map(button => (
+          <IconButton
+            key={button.label}
+            iconLabel={button.label}
+            iconType={button.iconType}
+            iconName={button.iconName}
+            style={tailwind('mr-2 p-2')}
+            disabled={button.disabled}
+          />
+        ))}
+      </ThemedScrollView>
+    </View>
+  )
+}
