6bc6735045f19a8b6f7d35c1f3b0309704bcb4a3
Keng Ye <kengyeleow@gmail.com>
2021-11-12 15:23:42 +0800

    feat: integrate loan token api and display loan cards

--- mobile-app/app/screens/AppNavigator/screens/Loans/LoansScreen.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Loans/LoansScreen.tsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { View } from 'react-native'
-import BigNumber from 'bignumber.js'
 import { tailwind } from '@tailwind'
 import { ThemedView } from '@components/themed'
-import { LoanCardOptions, LoanCards } from '@components/LoanCards'
+import { LoanCards } from '@components/LoanCards'
 import { Tabs } from '@components/Tabs'
 import { Vaults } from './components/Vaults'
 import { EmptyVault } from './components/EmptyVault'
 import { SkeletonLoader, SkeletonLoaderScreen } from '@components/SkeletonLoader'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '@store'
-import { fetchVaults } from '@store/loans'
+import { fetchLoanTokens, fetchVaults } from '@store/loans'
 import { useWhaleApiClient } from '@shared-contexts/WhaleContext'
 import { useWalletContext } from '@shared-contexts/WalletContext'
 
@@ -24,6 +23,7 @@ export type LoadingState = 'empty_vault' | 'loading' | 'success'
 export function LoansScreen (): JSX.Element {
   const { address } = useWalletContext()
   const vaults = useSelector((state: RootState) => state.loans.vaults)
+  const loans = useSelector((state: RootState) => state.loans.loanTokens)
   const [activeTab, setActiveTab] = useState<string>(TabKey.BrowseLoans)
   const dispatch = useDispatch()
   const client = useWhaleApiClient()
@@ -33,6 +33,7 @@ export function LoansScreen (): JSX.Element {
 
   useEffect(() => {
     dispatch(fetchVaults({ address, client }))
+    dispatch(fetchLoanTokens({ client }))
   }, [])
 
   const tabsList = [{
@@ -46,108 +47,6 @@ export function LoansScreen (): JSX.Element {
     disabled: false,
     handleOnPress: onPress
   }]
-
-  const loans: LoanCardOptions[] = [
-    {
-      loanName: 'BTC',
-      priceType: 'ACTIVE',
-      price: new BigNumber('123.4567'),
-      isVerified: true,
-      interestRate: new BigNumber('1.2345'),
-      onPress: () => {},
-      testID: 'loan_0'
-    },
-    {
-      loanName: 'BTC',
-      priceType: 'NEXT',
-      price: new BigNumber('123.4567'),
-      isVerified: false,
-      interestRate: new BigNumber('1.2345'),
-      onPress: () => {},
-      testID: 'loan_1'
-    },
-    {
-      loanName: 'BTC',
-      priceType: 'ACTIVE',
-      price: new BigNumber('123.4567'),
-      isVerified: true,
-      interestRate: new BigNumber('1.2345'),
-      onPress: () => {},
-      testID: 'loan_2'
-    },
-    {
-      loanName: 'BTC',
-      priceType: 'NEXT',
-      price: new BigNumber('123.4567'),
-      isVerified: false,
-      interestRate: new BigNumber('1.2345'),
-      onPress: () => {},
-      testID: 'loan_3'
-    },
-    {
-      loanName: 'BTC',
-      priceType: 'ACTIVE',
-      price: new BigNumber('123.4567'),
-      isVerified: true,
-      interestRate: new BigNumber('1.2345'),
-      onPress: () => {},
-      testID: 'loan_4'
-    },
-    {
-      loanName: 'BTC',
-      priceType: 'NEXT',
-      price: new BigNumber('123.4567'),
-      isVerified: false,
-      interestRate: new BigNumber('1.2345'),
-      onPress: () => {},
-      testID: 'loan_5'
-    },
-    {
-      loanName: 'BTC',
-      priceType: 'ACTIVE',
-      price: new BigNumber('123.4567'),
-      isVerified: true,
-      interestRate: new BigNumber('1.2345'),
-      onPress: () => {},
-      testID: 'loan_6'
-    },
-    {
-      loanName: 'BTC',
-      priceType: 'NEXT',
-      price: new BigNumber('123.4567'),
-      isVerified: false,
-      interestRate: new BigNumber('1.2345'),
-      onPress: () => {},
-      testID: 'loan_7'
-    },
-    {
-      loanName: 'BTC',
-      priceType: 'ACTIVE',
-      price: new BigNumber('123.4567'),
-      isVerified: true,
-      interestRate: new BigNumber('1.2345'),
-      onPress: () => {},
-      testID: 'loan_8'
-    },
-    {
-      loanName: 'BTC',
-      priceType: 'NEXT',
-      price: new BigNumber('123.4567'),
-      isVerified: false,
-      interestRate: new BigNumber('1.2345'),
-      onPress: () => {},
-      testID: 'loan_9'
-    },
-    {
-      loanName: 'BTC',
-      priceType: 'ACTIVE',
-      price: new BigNumber('123.4567'),
-      isVerified: true,
-      interestRate: new BigNumber('1.2345'),
-      onPress: () => {},
-      testID: 'loan_10'
-    }
-  ]
 
   if (vaults?.length === 0) {
     return (
@@ -165,16 +64,17 @@ export function LoansScreen (): JSX.Element {
     >
       <Tabs tabSections={tabsList} testID='loans_tabs' activeTabKey={activeTab} />
       {activeTab === TabKey.YourVaults && <Vaults />}
-      {activeTab === TabKey.BrowseLoans
-        ? (
+      {activeTab === TabKey.BrowseLoans && loans.length === 0 &&
+        (
           <View style={tailwind('mt-1')}>
             <SkeletonLoader
               row={6}
               screen={SkeletonLoaderScreen.Loan}
             />
           </View>
-          )
-        : <LoanCards testID='loans_cards' loans={loans} />}
+        )}
+      {activeTab === TabKey.BrowseLoans && loans.length > 0 &&
+        (<LoanCards testID='loans_cards' loans={loans} />)}
     </ThemedView>
   )
 }
