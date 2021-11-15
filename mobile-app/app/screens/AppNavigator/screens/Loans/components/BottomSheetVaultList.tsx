f368cd4a78f967feda98a24bf399b8c0afb238b1
Keng Ye <kengyeleow@gmail.com>
2021-11-15 12:06:17 +0800

    wip: placeholder for BottomSheetVaultList

---
+++ mobile-app/app/screens/AppNavigator/screens/Loans/components/BottomSheetVaultList.tsx
@@ -0,0 +1,26 @@
+// import { ThemedFlatList } from '@components/themed'
+// import { LoanVault } from '@store/loans'
+// import React, { memo } from 'react'
+
+// interface BottomSheetVaultListProps {
+//   headerLabel: string
+//   onCloseButtonPress: () => void
+//   onVaultPress?: (vault: LoanVault) => void
+//   navigateToScreen?: {
+//     screenName: string
+//     onButtonPress: () => void
+//   }
+// }
+
+// export const BottomSheetVaultList = ({
+//   headerLabel,
+//   onCloseButtonPress,
+//   onVaultPress
+// }: BottomSheetVaultListProps): React.MemoExoticComponent<() => JSX.Element> => memo(() => {
+//   const vaultList =
+//   return (
+//     <ThemedFlatList
+
+//     />
+//   )
+// })
