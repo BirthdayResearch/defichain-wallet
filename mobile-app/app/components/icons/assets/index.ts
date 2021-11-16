978b07b9e618471942972f311b49791a65158b09
thedoublejay <josejaimecareers@gmail.com>
2021-11-16 10:58:49 +0800

    added icons

--- mobile-app/app/components/icons/assets/index.ts
+++ mobile-app/app/components/icons/assets/index.ts
@@ -10,6 +10,21 @@ import { DFI } from './DFI'
 import { dLTC } from './dLTC'
 import { dUSDC } from './dUSDC'
 import { dUSDT } from './dUSDT'
+import { dTSLA } from './dTSLA'
+import { dUSD } from './dUSD'
+import { dAAPL } from './dAAPL'
+import { dAMD } from './dAMD'
+import { dGME } from '@components/icons/assets/dGME'
+import { dBABA } from '@components/icons/assets/dBABA'
+import { dGOOG } from '@components/icons/assets/dGOOG'
+import { dPLTR } from '@components/icons/assets/dPLTR'
+import { dARKK } from '@components/icons/dARKK'
+import { dAMZN } from '@components/icons/assets/dAMZN'
+import { dCOIN } from '@components/icons/assets/dCOIN'
+import { dFB } from '@components/icons/assets/dFB'
+import { dTWTR } from '@components/icons/assets/dTWTR'
+import { dNVDA } from '@components/icons/assets/dNVDA'
+import { dMSFT } from '@components/icons/assets/dMSFT'
 
 const mapping: Record<string, (props: SvgProps) => JSX.Element> = {
   _UTXO: DFI,
@@ -24,7 +39,23 @@ const mapping: Record<string, (props: SvgProps) => JSX.Element> = {
   dETH: dETH,
   dLTC: dLTC,
   dUSDT: dUSDT,
-  dUSDC: dUSDC
+  dUSDC: dUSDC,
+  dTSLA: dTSLA,
+  dAAPL: dAAPL,
+  dAMD: dAMD,
+  dGME: dGME,
+  dBABA: dBABA,
+  dGOOG: dGOOG,
+  dDUSD: dUSD,
+  DUSD: dUSD,
+  dPLTR: dPLTR,
+  dARKK: dARKK,
+  dAMZN: dAMZN,
+  dCOIN: dCOIN,
+  dFB: dFB,
+  dTWTR: dTWTR,
+  dNVDA: dNVDA,
+  dMSFT: dMSFT
 }
 
 /**
