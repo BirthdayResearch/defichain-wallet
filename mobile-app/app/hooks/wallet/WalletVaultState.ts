02e68e20037d4d4f802c33599f0d07b01fa1739b
Keng Ye <kengyeleow@gmail.com>
2021-11-17 18:24:04 +0800

    wip: placeholder for hook to get vault status

---
+++ mobile-app/app/hooks/wallet/WalletVaultState.ts
@@ -0,0 +1,11 @@
+// export enum WalletVaultStatus {
+//   Active = 'ACTIVE',
+//   Healthy = 'HEALTHY',
+//   AtRisk = 'AT RISK',
+//   Halted = 'HALTED',
+//   Liquidated = 'LIQUIDATED'
+// }
+
+// export function useWalletVaultState (props): WalletVaultStatus {
+
+// }
