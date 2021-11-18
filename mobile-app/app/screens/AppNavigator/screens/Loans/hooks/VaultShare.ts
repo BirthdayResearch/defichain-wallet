917c16e5dccac3c288abc20734694a43d075862e
thedoublejay <josejaimecareers@gmail.com>
2021-11-18 18:42:55 +0800

    updated

--- mobile-app/app/screens/AppNavigator/screens/Loans/hooks/VaultShare.ts
+++ mobile-app/app/screens/AppNavigator/screens/Loans/hooks/VaultShare.ts
@@ -1,5 +1,6 @@
 import BigNumber from 'bignumber.js'
 
 export function useVaultShare (collateralAmount: BigNumber, factor: BigNumber, price: BigNumber, totalCollateralValue: BigNumber): BigNumber {
-  return new BigNumber(collateralAmount ?? 0).multipliedBy(price ?? 0).multipliedBy(factor ?? 0).dividedBy(totalCollateralValue ?? 1).times(100)
+  const vaultShare = new BigNumber(collateralAmount ?? 0).multipliedBy(price ?? 0).multipliedBy(factor ?? 0).dividedBy(totalCollateralValue ?? 1)
+  return BigNumber.min(1, vaultShare).times(100)
 }
