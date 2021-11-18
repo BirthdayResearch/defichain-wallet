e58a88dd70a393b7454b50da0fe13ebb94d943c7
JJ Adonis <josejaimecareers@gmail.com>
2021-11-18 17:13:10 +0800

    `loans` - add collateral conversion and confirm page (#1321)
    * add collateral conversion and confirm page
    
    * translations

---
+++ mobile-app/app/screens/AppNavigator/screens/Loans/hooks/CollateralPrice.ts
@@ -0,0 +1,23 @@
+import BigNumber from 'bignumber.js'
+import { useVaultShare } from '@screens/AppNavigator/screens/Loans/hooks/VaultShare'
+import { CollateralItem } from '@screens/AppNavigator/screens/Loans/screens/EditCollateralScreen'
+
+interface CollateralPrice {
+  activePrice: BigNumber
+  collateralPrice: BigNumber
+  vaultShare: BigNumber
+  collateralFactor: BigNumber
+}
+
+export function useCollateralPrice (amount: BigNumber, collateralItem: CollateralItem, totalCollateralValue: BigNumber): CollateralPrice {
+  const activePrice = new BigNumber(collateralItem.activePrice?.active?.amount ?? 0)
+  const collateralPrice = new BigNumber(activePrice).multipliedBy(amount)
+  const collateralFactor = new BigNumber(collateralItem?.factor ?? 0)
+  const vaultShare = useVaultShare(new BigNumber(amount), collateralFactor, new BigNumber(activePrice), new BigNumber(totalCollateralValue.isZero() ? collateralPrice : totalCollateralValue))
+  return {
+    activePrice,
+    collateralPrice,
+    vaultShare,
+    collateralFactor
+  }
+}
