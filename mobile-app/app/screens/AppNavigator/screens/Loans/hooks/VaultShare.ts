bca07c791296b5a9ad1257c4b8e37f87d94b80bd
thedoublejay <josejaimecareers@gmail.com>
2021-11-18 19:04:10 +0800

    removed fallbacks

--- mobile-app/app/screens/AppNavigator/screens/Loans/hooks/VaultShare.ts
+++ mobile-app/app/screens/AppNavigator/screens/Loans/hooks/VaultShare.ts
@@ -1,6 +1,6 @@
 import BigNumber from 'bignumber.js'
 
 export function useVaultShare (collateralAmount: BigNumber, factor: BigNumber, price: BigNumber, totalCollateralValue: BigNumber): BigNumber {
-  const vaultShare = new BigNumber(collateralAmount ?? 0).multipliedBy(price ?? 0).multipliedBy(factor ?? 0).dividedBy(totalCollateralValue ?? 1)
+  const vaultShare = new BigNumber(collateralAmount).multipliedBy(price).multipliedBy(factor).dividedBy(totalCollateralValue)
   return BigNumber.min(1, vaultShare).times(100)
 }
