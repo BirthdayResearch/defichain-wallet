66520c9ebf5f83603ba4d43e3d3ff5c7ef7a3344
thedoublejay <josejaimecareers@gmail.com>
2021-11-20 11:31:25 +0800

    updated tests

--- mobile-app/app/screens/AppNavigator/screens/Loans/components/VaultInfo.test.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Loans/components/VaultInfo.test.tsx
@@ -1,4 +1,3 @@
-import BigNumber from 'bignumber.js'
 import { render } from '@testing-library/react-native'
 import React from 'react'
 import { VaultInfo } from './VaultInfo'
@@ -9,24 +8,10 @@ jest.mock('@react-navigation/native', () => ({
 }))
 
 describe('Vault info', () => {
-  it('should render label with numeric value', async () => {
-    const rendered = render(
-      <VaultInfo
-        label='Foo'
-        valueType='NUMBER'
-        value={new BigNumber('123.456789')}
-        suffix='%'
-        prefix='$'
-      />
-    )
-    expect(rendered.toJSON()).toMatchSnapshot()
-  })
-
   it('should render label with token icon group', async () => {
     const rendered = render(
       <VaultInfo
         label='Foo'
-        valueType='TOKEN_ICON_GROUP'
         tokens={['BTC', 'dLTC', 'dDOGE', 'dETH', 'dBCH', 'DFI']}
       />
     )
