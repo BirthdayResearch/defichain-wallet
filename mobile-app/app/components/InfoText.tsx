0b986207920f06bc6807e1dd5650f322e2d1c13d
Pierre Gee <pierre.gabaldon@gmail.com>
2021-11-23 16:13:14 +0800

    Composite swap feature (#1255)
    * Add static ui for composite swap
    
    * Polish UI Dex Screen swap button
    
    * Change price rate value
    
    * Add bottom sheet token list
    
    * Rename Bottom sheet header
    
    * Add composite swapping, estimates, price rates
    
    * Add UTXO conversion
    
    * Display authorization hint on valid form
    
    * Update translation functions
    
    * Move conversion to confirm screen to handle success message
    
    * Merge logic in getting adjacent vertices
    
    * Merge bottom sheet tokens
    
    * Add translations
    
    * Add translations
    
    * Remove temporary modal web view
    
    * Improve path finding to exit on first one
    
    * Change formula for estimated price and price rates
    
    * Revert pool swap change and use id instead of symbol for comparing
    
    * Revert conversion ui
    
    * Add swap  e2e tests
    
    * Fix estimate on multiple hops
    
    * Add e2e test for multiple hops
    
    * Add missing translations
    
    * Fix alignment on bottom sheet
    
    * Use descriptive name for unit test - path find
    
    * Use descriptive name for unit test - path find
    
    * Add props in prep for pool swap migration
    
    * Fix test (unified token was not used)
    
    * Move tolerable slippage to final estimated (not per hop)
    
    * Add code improvements
    
    * Remove unneeded tests
    
    Co-authored-by: thedoublejay <josejaimecareers@gmail.com>

--- mobile-app/app/components/InfoText.tsx
+++ mobile-app/app/components/InfoText.tsx
@@ -8,14 +8,14 @@ interface InfoTextProp extends ThemedProps, TextProps {
   type?: InfoTextType
 }
 
-export type InfoTextType = 'warning' | 'error'
+export type InfoTextType = 'warning' | 'error' | 'success'
 
 export function InfoText (props: InfoTextProp): JSX.Element {
   const {
     type = 'warning',
     style,
-    light = tailwind({ 'bg-warning-50 border-warning-200': type === 'warning', 'bg-error-50 border-error-200': type === 'error' }),
-    dark = tailwind({ 'bg-darkwarning-50 border-darkwarning-200': type === 'warning', 'bg-darkerror-50 border-darkerror-200': type === 'error' }),
+    light = tailwind({ 'bg-warning-50 border-warning-200': type === 'warning', 'bg-error-50 border-error-200': type === 'error', 'bg-success-50 border-success-200': type === 'success' }),
+    dark = tailwind({ 'bg-darkwarning-50 border-darkwarning-200': type === 'warning', 'bg-darkerror-50 border-darkerror-200': type === 'error', 'bg-success-50 border-success-200': type === 'success' }),
     ...otherProps
   } = props
 
@@ -30,10 +30,10 @@ export function InfoText (props: InfoTextProp): JSX.Element {
     >
       <ThemedIcon
         iconType='MaterialIcons'
-        name={type === 'warning' ? 'info' : 'warning'}
+        name={type === 'success' ? 'check-circle-outline' : type === 'warning' ? 'info' : 'warning'}
         size={14}
-        light={tailwind({ 'text-warning-500': type === 'warning', 'text-error-500': type === 'error' })}
-        dark={tailwind({ 'text-darkwarning-500': type === 'warning', 'text-darkerror-500': type === 'error' })}
+        light={tailwind({ 'text-warning-500': type === 'warning', 'text-error-500': type === 'error', 'text-success-500': type === 'success' })}
+        dark={tailwind({ 'text-darkwarning-500': type === 'warning', 'text-darkerror-500': type === 'error', 'text-darksuccess-500': type === 'success' })}
       />
       <ThemedText
         style={tailwind('text-xs pl-2 font-medium flex-1')}
