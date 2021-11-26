800e1b3b0155fab482dcfd6652756121c9551c41
Keng Ye <kengyeleow@gmail.com>
2021-11-26 14:45:47 +0800

    feat: integrate list auction api

--- shared/store/auctions.ts
+++ shared/store/auctions.ts
@@ -1,132 +1,22 @@
 import { WhaleApiClient } from '@defichain/whale-api-client'
-import { LoanVaultLiquidated, LoanVaultState } from '@defichain/whale-api-client/dist/api/loan'
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { LoanVaultLiquidated, LoanVaultLiquidationBatch, LoanVaultTokenAmount } from '@defichain/whale-api-client/dist/api/loan'
+import { createAsyncThunk, createSlice, PayloadAction, createSelector } from '@reduxjs/toolkit'
+import { customDUSDActivePrice } from './loans'
 
 interface AuctionsState {
   auctions: LoanVaultLiquidated[]
+  hasFetchAuctionsData: boolean
 }
 
 const initialState: AuctionsState = {
-  auctions: []
+  auctions: [],
+  hasFetchAuctionsData: false
 }
 
 export const fetchAuctions = createAsyncThunk(
   'wallet/fetchAuctions',
   async ({ size = 50, client }: { size?: number, client: WhaleApiClient }) => {
-    const auctions: LoanVaultLiquidated[] = [{
-      vaultId: '92dcef48f0109d007f6c02a263fdb9d30e618739a8d749584e0b732c5b968f54',
-      loanScheme: {
-        id: 'MIN200',
-        minColRatio: '200',
-        interestRate: '2'
-      },
-      ownerAddress: '8ZxMPyEeyTDcvmxgWWRDfrq8kwQEm5sbXq',
-      state: LoanVaultState.IN_LIQUIDATION,
-      batchCount: 2,
-      liquidationHeight: 9870,
-      liquidationPenalty: 5,
-      batches: [
-        {
-          index: 0,
-          collaterals: [
-            {
-              id: '0',
-              amount: '3360.60854727',
-              symbol: 'DFI',
-              symbolKey: 'DFI',
-              name: 'Default Defi token',
-              displaySymbol: 'DFI',
-              activePrice: {
-                id: 'DFI-USD-1386480',
-                key: 'DFI-USD',
-                isLive: true,
-                block: {
-                  hash: 'af18460c64945121d96fd126bcc22dd48229ada245b0bc33129364b49168346c',
-                  height: 1386480,
-                  medianTime: 1637562729,
-                  time: 1637562731
-                },
-                active: {
-                  amount: '2.97565149',
-                  weightage: 30,
-                  oracles: {
-                    active: 3,
-                    total: 3
-                  }
-                },
-                next: {
-                  amount: '2.98680778',
-                  weightage: 30,
-                  oracles: {
-                    active: 3,
-                    total: 3
-                  }
-                },
-                sort: '001527f0'
-              }
-            }
-          ],
-          loan: {
-            id: '15',
-            amount: '5015.07942533',
-            symbol: 'DUSD',
-            symbolKey: 'DUSD',
-            name: 'Decentralized USD',
-            displaySymbol: 'DUSD'
-          }
-        },
-        {
-          index: 1,
-          collaterals: [
-            {
-              id: '0',
-              amount: '2341.07877576',
-              symbol: 'DFI',
-              symbolKey: 'DFI',
-              name: 'Default Defi token',
-              displaySymbol: 'DFI',
-              activePrice: {
-                id: 'DFI-USD-1386480',
-                key: 'DFI-USD',
-                isLive: true,
-                block: {
-                  hash: 'af18460c64945121d96fd126bcc22dd48229ada245b0bc33129364b49168346c',
-                  height: 1386480,
-                  medianTime: 1637562729,
-                  time: 1637562731
-                },
-                active: {
-                  amount: '2.97565149',
-                  weightage: 30,
-                  oracles: {
-                    active: 3,
-                    total: 3
-                  }
-                },
-                next: {
-                  amount: '2.98680778',
-                  weightage: 30,
-                  oracles: {
-                    active: 3,
-                    total: 3
-                  }
-                },
-                sort: '001527f0'
-              }
-            }
-          ],
-          loan: {
-            id: '15',
-            amount: '3493.62201408',
-            symbol: 'DUSD',
-            symbolKey: 'DUSD',
-            name: 'Decentralized USD',
-            displaySymbol: 'DUSD'
-          }
-        }
-      ]
-    }]
-    return auctions // await client.loan.listAuction(size)
+    return await client.loan.listAuction(size)
   }
 )
 
@@ -137,6 +27,33 @@ export const auctions = createSlice({
   extraReducers: (builder) => {
     builder.addCase(fetchAuctions.fulfilled, (state, action: PayloadAction<LoanVaultLiquidated[]>) => {
       state.auctions = action.payload
+      state.hasFetchAuctionsData = true
     })
   }
+})
+
+export const auctionsSelector = createSelector((state: AuctionsState) => state.auctions, auctions => {
+  return auctions.map(auction => {
+    const modifiedBatches: LoanVaultLiquidationBatch[] = auction.batches.map(batch => {
+      const modifiedCollaterals: LoanVaultTokenAmount[] = batch.collaterals.map(collateral => {
+        if (collateral.symbol === 'DUSD') {
+          const modifiedCollateral: LoanVaultTokenAmount = {
+            ...collateral,
+            activePrice: customDUSDActivePrice
+          }
+          return modifiedCollateral
+        } else {
+          return { ...collateral }
+        }
+      })
+      return {
+        ...batch,
+        collaterals: modifiedCollaterals
+      }
+    })
+    return {
+      ...auction,
+      batches: modifiedBatches
+    }
+  }) as LoanVaultLiquidated []
 })
