cd395c07ba426c07a0d7979e16827f1f0d2889d7
thedoublejay <josejaimecareers@gmail.com>
2021-11-30 12:04:40 +0800

    style

--- mobile-app/app/components/BottomSheetModal.web.tsx
+++ mobile-app/app/components/BottomSheetModal.web.tsx
@@ -19,22 +19,24 @@ export const BottomSheetModal = React.forwardRef((props: Props, ref: React.Ref<a
     <Modal
       container={ref as React.RefObject<any>}
       show={props.isModalDisplayed}
-      renderBackdrop={() => <View style={{
-        position: 'absolute',
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-        backgroundColor: 'black',
-        opacity: 0.3
-      }}
-                            />}
+      renderBackdrop={() => (
+        <View style={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          backgroundColor: 'black',
+          opacity: 0.3
+        }}
+        />
+      )}
       style={{
         position: 'fixed',
         height: '240px',
         width: '375px',
         zIndex: 50,
-        top: '50%'
+        top: '55%'
       }}
     >
       {children}
