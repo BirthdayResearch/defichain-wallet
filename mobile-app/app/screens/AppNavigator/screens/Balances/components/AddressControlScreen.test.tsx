8720c9f8a71f5b59d125998714fb41fc685910b6
Keng Ye <kengyeleow@gmail.com>
2021-12-08 18:09:55 +0800

    test: add store to snapshot test

--- mobile-app/app/screens/AppNavigator/screens/Balances/components/AddressControlScreen.test.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Balances/components/AddressControlScreen.test.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { render } from '@testing-library/react-native'
 import { AddressControlCard, AddressControlModal, AddressControlScreen, AddressItemRow } from './AddressControlScreen'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import { RootState } from '@store'
+import { transactionQueue } from '@store/transaction_queue'
+import { ocean } from '@store/ocean'
 
 jest.mock('@shared-contexts/ThemeProvider')
 jest.mock('@shared-contexts/WalletContext')
@@ -28,13 +33,33 @@ describe('DFI address control', () => {
 
   it('should match snapshot for Address Item Row', async () => {
     const onPress = jest.fn()
+    const initialState: Partial<RootState> = {
+      transactionQueue: {
+        transactions: []
+      },
+      ocean: {
+        transactions: [],
+        height: 0,
+        err: undefined
+      }
+    }
+    const store = configureStore({
+      preloadedState: initialState,
+      reducer: {
+        transactionQueue: transactionQueue.reducer,
+        ocean: ocean.reducer
+      }
+    })
     const rendered = render(
-      <AddressItemRow
-        address='bcrt1q8rfsfny80jx78cmk4rsa069e2ckp6rn83u6ut9'
-        isActive
-        index={0}
-        onPress={onPress}
-      />)
+      <Provider store={store}>
+        <AddressItemRow
+          address='bcrt1q8rfsfny80jx78cmk4rsa069e2ckp6rn83u6ut9'
+          isActive
+          index={0}
+          onPress={onPress}
+        />
+      </Provider>
+    )
     expect(rendered.toJSON()).toMatchSnapshot()
   })
 })
