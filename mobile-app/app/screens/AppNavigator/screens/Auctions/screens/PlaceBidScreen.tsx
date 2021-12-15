85062c0ced9d6ad37908f096e7acfbfaf52d81f1
Keng Ye <kengyeleow@gmail.com>
2021-12-15 16:06:06 +0800

    feat: replace BidCardRow with VaultSectionTextRow
    To avoid re-introducing local component

--- mobile-app/app/screens/AppNavigator/screens/Auctions/screens/PlaceBidScreen.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Auctions/screens/PlaceBidScreen.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Platform, View, NativeSyntheticEvent, TextInputChangeEventData } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
-import NumberFormat from 'react-number-format'
 import { StackScreenProps } from '@react-navigation/stack'
 import { NavigationProp, useNavigation } from '@react-navigation/native'
 import BigNumber from 'bignumber.js'
@@ -32,6 +31,7 @@ import { InfoText } from '@components/InfoText'
 import { getActivePrice } from '../helpers/ActivePrice'
 import { useWalletContext } from '@shared-contexts/WalletContext'
 import { fetchTokens, tokensSelector } from '@store/wallet'
+import { VaultSectionTextRow } from '../../Loans/components/VaultSectionTextRow'
 
 type Props = StackScreenProps<AuctionsParamList, 'PlaceBidScreen'>
 
@@ -261,93 +261,37 @@ function BidSummaryCard (props: {
         </View>
       </View>
 
-      <View>
-        <BidCardRow
-          lhs={translate('screens/PlaceBidScreen', 'Total auction value (USD)')}
-          rhs={{
-            suffixType: 'component',
-            value: new BigNumber(props.totalAuctionValue),
-            prefix: '$'
-          }}
-        >
-          <TouchableOpacity onPress={props.onPressFullDetails}>
-            <ThemedText
-              dark={tailwind('text-darkprimary-500')}
-              light={tailwind('text-primary-500')}
-              style={tailwind('text-xs')}
-            >{` ${translate('screens/PlaceBidScreen', '(Full details)')}`}
-            </ThemedText>
-          </TouchableOpacity>
-        </BidCardRow>
-        <BidCardRow
-          lhs={translate('screens/PlaceBidScreen', 'Min. next bid')}
-          rhs={{
-            suffixType: 'text',
-            value: props.minNextBid,
-            suffix: props.displaySymbol
-          }}
-        />
+      <VaultSectionTextRow
+        value={new BigNumber(props.totalAuctionValue).toFixed(2)}
+        lhs={translate('screens/PlaceBidScreen', 'Total auction value (USD)')}
+        testID='text_total_auction_value'
+        suffixType='component'
+        prefix='$'
+      >
+        <TouchableOpacity onPress={props.onPressFullDetails}>
+          <ThemedText
+            dark={tailwind('text-darkprimary-500')}
+            light={tailwind('text-primary-500')}
+            style={tailwind('text-xs')}
+          >{` ${translate('screens/PlaceBidScreen', '(Full details)')}`}
+          </ThemedText>
+        </TouchableOpacity>
+      </VaultSectionTextRow>
+      <VaultSectionTextRow
+        value={props.minNextBid}
+        lhs={translate('screens/PlaceBidScreen', 'Min. next bid')}
+        testID='text_min_next_bid'
+        suffixType='text'
+        suffix={props.displaySymbol}
+      />
+      <View style={tailwind('mt-1')}>
         <AuctionTimeProgress
           liquidationHeight={props.liquidationHeight}
           blockCount={props.blockCount}
           label='Auction time left'
           auctionTextStyle={tailwind('text-xs')}
         />
-
       </View>
     </ThemedView>
-  )
-}
-
-interface BidCardRowProps {
-  lhs: string
-  rhs: {
-    suffixType: 'text' | 'component'
-    value: BigNumber | string
-    suffix?: string
-    prefix?: string
-  }
-}
-
-function BidCardRow (props: React.PropsWithChildren<BidCardRowProps>): JSX.Element {
-  const rhsStyle = {
-    style: tailwind('text-xs text-right')
-  }
-
-  return (
-    <View style={tailwind('flex flex-row mb-1')}>
-      <View style={tailwind('w-6/12')}>
-        <ThemedText
-          light={tailwind('text-gray-500')}
-          dark={tailwind('text-gray-400')}
-          style={tailwind('text-xs')}
-        >
-          {props.lhs}
-        </ThemedText>
-      </View>
-      <View style={tailwind('flex flex-row flex-1 flex-wrap w-6/12 justify-end')}>
-        <ThemedText {...rhsStyle}>
-          {props.rhs.prefix !== undefined && <ThemedText {...rhsStyle}>{props.rhs.prefix}</ThemedText>}
-          {BigNumber.isBigNumber(props.rhs.value)
-            ? (
-              <NumberFormat
-                decimalScale={8}
-                displayType='text'
-                thousandSeparator
-                value={props.rhs.value.toFixed(2)}
-                renderText={(val: string) => (
-                  <ThemedText {...rhsStyle}>{val}</ThemedText>
-                )}
-              />
-            )
-            : (
-              <ThemedText {...rhsStyle}>{props.rhs.value}</ThemedText>
-            )}
-          {props.rhs.suffix !== undefined && props.rhs.suffixType === 'text' &&
-            <ThemedText {...rhsStyle}>{` ${props.rhs.suffix}`}</ThemedText>}
-        </ThemedText>
-        {props.rhs.suffixType === 'component' && props.children}
-      </View>
-    </View>
   )
 }
