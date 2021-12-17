89fe0dbc0cfb10be4ebf986c0d813fb6a0282718
Keng Ye <kengyeleow@gmail.com>
2021-12-17 19:07:57 +0800

    feat: update UI to match latest design

--- mobile-app/app/screens/AppNavigator/screens/Balances/components/Announcements.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Balances/components/Announcements.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { ThemedIcon, ThemedText, ThemedTouchableOpacity } from '@components/themed'
+import { ThemedView } from '@components/themed'
 import { tailwind } from '@tailwind'
 import { useGetAnnouncementsQuery } from '@store/website'
 import { AnnouncementData } from '@shared-types/website'
 import { satisfies } from 'semver'
 import { useLanguageContext } from '@shared-contexts/LanguageProvider'
 import { openURL } from '@api/linking'
-import { Platform } from 'react-native'
+import { Platform, TouchableOpacity } from 'react-native'
 import { nativeApplicationVersion } from 'expo-application'
+import { translate } from '@translations'
+import { Text } from '@components'
+import { MaterialIcons } from '@expo/vector-icons'
 
 export function Announcements (): JSX.Element {
   const {
@@ -25,28 +28,36 @@ export function Announcements (): JSX.Element {
   }
 
   return (
-    <ThemedTouchableOpacity
+    <ThemedView
       testID='announcements_banner'
-      style={tailwind('px-4 py-3 flex-row items-center')} light={tailwind('bg-warning-50')}
-      dark={tailwind('bg-darkwarning-50')}
-      onPress={async () => await openURL(announcement.url)}
-      disabled={announcement.url === undefined || announcement.url.length === 0}
+      style={tailwind('px-4 py-3 flex-row items-center')}
+      light={tailwind('bg-primary-700')}
+      dark={tailwind('bg-darkprimary-700')}
     >
-      <ThemedIcon style={tailwind('mr-2')} iconType='MaterialIcons' name='campaign' size={22} />
-      <ThemedText style={tailwind('text-xs flex-auto')} testID='announcements_text'>
+      <MaterialIcons
+        style={tailwind('mr-2.5 text-white')}
+        iconType='MaterialIcons'
+        name='campaign'
+        size={22}
+      />
+      <Text
+        style={tailwind('text-xs flex-auto text-white')}
+        testID='announcements_text'
+      >
         {`${announcement.content} `}
-      </ThemedText>
+      </Text>
       {announcement.url !== undefined && announcement.url.length !== 0 &&
-          (
-            <ThemedIcon
-              dark={tailwind('text-darkprimary-500')}
-              iconType='MaterialIcons'
-              light={tailwind('text-primary-500')}
-              name='chevron-right'
-              size={24}
-            />
-          )}
-    </ThemedTouchableOpacity>
+        (
+          <TouchableOpacity
+            onPress={async () => await openURL(announcement.url)}
+            style={tailwind('py-1 px-2 rounded border border-white')}
+          >
+            <Text style={tailwind('text-xs font-medium text-white')}>
+              {translate('components/Announcements', 'VIEW')}
+            </Text>
+          </TouchableOpacity>
+        )}
+    </ThemedView>
   )
 }
 
