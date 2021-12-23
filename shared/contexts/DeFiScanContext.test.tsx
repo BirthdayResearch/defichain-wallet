339d506010e2dd9dff23c1d7afac754a21bc73c9
Harsh <harshrathi.dev@gmail.com>
2021-12-23 16:40:53 +0530

    Added unit test case for DeFiScanContext

---
+++ shared/contexts/DeFiScanContext.test.tsx
@@ -0,0 +1,56 @@
+import { EnvironmentNetwork } from '@environment'
+import { getTxURLByNetwork, getURLByNetwork, DeFiScanProvider, useDeFiScanContext } from './DeFiScanContext'
+import * as React from 'react'
+import { Text, View } from 'react-native'
+import { render } from '@testing-library/react-native'
+
+jest.mock('@shared-contexts/NetworkContext')
+describe('DeFiScanContext test', () => {
+  it('should match the expected txn redirect url', () => {
+    const txnId = 'dummyTransactionIdForTesting'
+    const rawId = 'dummyRawTransactionIdForTesting'
+    const expectedTxnUrl = `https://defiscan.live/transactions/${txnId}`
+    const txnUrl = getTxURLByNetwork(EnvironmentNetwork.MainNet, txnId, '')
+    const rawTxnUrl = getTxURLByNetwork(EnvironmentNetwork.MainNet, txnId, rawId)
+    expect(txnUrl).toBe(expectedTxnUrl)
+    expect(rawTxnUrl).toBe(`${expectedTxnUrl}?rawtx=${rawId}`)
+  })
+
+  it('should match the expected URL By Network', () => {
+    const expectedUrl = 'https://defiscan.live/blocks/1'
+    const url = getURLByNetwork('blocks', EnvironmentNetwork.MainNet, '1')
+    expect(url).toBe(expectedUrl)
+  })
+})
+
+describe('DeFi Scan Context test', () => {
+  it('should match snapchot', () => {
+    function DeFiScanProviderComponent (): JSX.Element {
+      const { getTransactionUrl, getBlocksUrl, getTokenUrl, getAddressUrl, getVaultsUrl, getAuctionsUrl } = useDeFiScanContext()
+      const transactionUrl = getTransactionUrl('txnId')
+      const blocksUrl = getBlocksUrl(1)
+      const tokenUrl = getTokenUrl('0')
+      const addressUrl = getAddressUrl('address')
+      const vaultsUrl = getVaultsUrl('vaultId')
+      const auctionsUrl = getAuctionsUrl('vaultId', 2)
+      return (
+        <View>
+          <Text>{transactionUrl}</Text>
+          <Text>{blocksUrl}</Text>
+          <Text>{tokenUrl}</Text>
+          <Text>{addressUrl}</Text>
+          <Text>{vaultsUrl}</Text>
+          <Text>{auctionsUrl}</Text>
+        </View>
+      )
+    }
+
+    const rendered = render(
+      <DeFiScanProvider>
+        <DeFiScanProviderComponent />
+      </DeFiScanProvider>
+    )
+
+    expect(rendered.toJSON()).toMatchSnapshot()
+  })
+})
