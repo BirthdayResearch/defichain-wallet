2aaf54a5ab7725c6d542aeee210dfe9dc2568842
chloezxyy <44501120+chloezxyy@users.noreply.github.com>
2022-01-11 13:25:37 +0800

    Update mobile-app/cypress/integration/functional/wallet/settings/knowledgeBase.spec.ts
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>

--- mobile-app/cypress/integration/functional/wallet/settings/knowledgeBase.spec.ts
+++ mobile-app/cypress/integration/functional/wallet/settings/knowledgeBase.spec.ts
@@ -62,6 +62,7 @@ context('Wallet - Settings', () => {
     cy.go('back')
     cy.url().should('include', 'app/Settings/KnowledgeBaseScreen')
   })
+
   it('should navigate to Liquidity Mining faq from knowledge base page', function () {
     cy.getByTestID('liquidity_mining_faq').should('exist').click()
     cy.url().should('include', 'app/Settings/LiquidityMiningFaq')
