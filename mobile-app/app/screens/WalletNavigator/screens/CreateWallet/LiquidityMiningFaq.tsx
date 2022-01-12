f12d47ef6d6530c00d1bf4d6f397461e85f5d2a0
chloezxyy <chloezhengxy@gmail.com>
2022-01-12 15:18:59 +0800

    code clean up

--- mobile-app/app/screens/WalletNavigator/screens/CreateWallet/LiquidityMiningFaq.tsx
+++ mobile-app/app/screens/WalletNavigator/screens/CreateWallet/LiquidityMiningFaq.tsx
@@ -8,21 +8,21 @@ export function LiquidityMiningFaq (): JSX.Element {
     {
       title: translate('components/LiquidityMiningFaq', 'How do I supply liquidity?'),
       content: [{
-        text: translate('components/LiquidityMiningFaq', 'After deciding which pool pair you would like to supply liquidity to, click ADD LIQUIDITY and enter the input amount for either of the tokens in the pair. \n\nOnce an amount is entered on one side of the pair, the app automatically balances the amount of the other side, based on the ratio of current liquidity in the pool.'),
+        text: translate('components/LiquidityMiningFaq', 'After deciding which pool pair you would like to supply liquidity to, click ADD LIQUIDITY and enter the input amount for either of the tokens in the pair.\n\nOnce an amount is entered on one side of the pair, the app automatically balances the amount of the other side, based on the ratio of current liquidity in the pool.'),
         type: 'paragraph'
       }]
     },
     {
       title: translate('components/LiquidityMiningFaq', 'How do I send tokens from my DFI wallet?'),
       content: [{
-        text: translate('components/LiquidityMiningFaq', 'Tokens on DeFiChain can only be sent to DFI addresses only. This means that you should not send directly to an exchange or a third party platform that does not support DeFiChain. \n\nSimilarly, liquidity tokens can only be sent to another DFI wallet address.'),
+        text: translate('components/LiquidityMiningFaq', 'Tokens on DeFiChain can only be sent to DFI addresses only. This means that you should not send directly to an exchange or a third party platform that does not support DeFiChain.\n\nSimilarly, liquidity tokens can only be sent to another DFI wallet address.'),
         type: 'paragraph'
       }]
     },
     {
       title: translate('components/LiquidityMiningFaq', 'What are some exchanges and platforms I can send to?'),
       content: [{
-        text: translate('components/LiquidityMiningFaq', '1. Kucoin\n2. Bittrex\n3. Transak\n4. Cake DeFi \n\nView more at DeFiChain.com'),
+        text: translate('components/LiquidityMiningFaq', '1. Kucoin\n2. Bittrex\n3. Transak\n4. Cake DeFi\n\nView more at DeFiChain.com'),
         type: 'paragraph'
       }]
     },
@@ -36,7 +36,7 @@ export function LiquidityMiningFaq (): JSX.Element {
     {
       title: translate('components/LiquidityMiningFaq', 'How do I receive tokens on my DFI wallet?'),
       content: [{
-        text: translate('components/LiquidityMiningFaq', 'You can receive DFI tokens into your wallet by sharing your address found in the Receive page, or by getting the sender to scan the QR code. \n\nNote that you will not be able to receive native tokens (e.g. BTC, ETH) directly into your wallet.'),
+        text: translate('components/LiquidityMiningFaq', 'You can receive DFI tokens into your wallet by sharing your address found in the Receive page, or by getting the sender to scan the QR code.\n\nNote that you will not be able to receive native tokens (e.g. BTC, ETH) directly into your wallet.'),
         type: 'paragraph'
       }]
     }
@@ -56,7 +56,7 @@ export function LiquidityMiningFaq (): JSX.Element {
       <ThemedText
         style={tailwind('mt-2 text-sm')}
       >
-        {translate('components/LiquidityMiningFaq', 'Liquidity mining is a DeFi mechanism in which participants supply cryptocurrencies into liquidity pools, and in turn be rewarded with fees and tokens based on their share of the total pool liquidity. \n\nLiquidity pools in DeFiChain consist of liquidity in pairs of coins, used by the DeFiChain DEX (Decentralized Exchange).')}
+        {translate('components/LiquidityMiningFaq', 'Liquidity mining is a DeFi mechanism in which participants supply cryptocurrencies into liquidity pools, and in turn be rewarded with fees and tokens based on their share of the total pool liquidity.\n\nLiquidity pools in DeFiChain consist of liquidity in pairs of coins, used by the DeFiChain DEX (Decentralized Exchange).')}
       </ThemedText>
 
       <WalletAccordion
