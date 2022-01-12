d4b4f2bdfbce95c809ec86eb64c866d431fbabb1
pierregee <pierre.gabaldon@gmail.com>
2022-01-12 12:50:26 +0800

    Refactor time ago translations

--- mobile-app/app/screens/AppNavigator/screens/Auctions/helpers/SecondstoHm.ts
+++ mobile-app/app/screens/AppNavigator/screens/Auctions/helpers/SecondstoHm.ts
@@ -17,7 +17,12 @@ export function secondsToHmDisplay (d: number): string {
 
 export function secondsToTimeAgo (d: number): string {
   const { h, m } = secondsToHm(d)
-  const hDisplay = h > 0 ? `${translate('components/BatchCard', '{{h}}h', { h })} ` : ''
-  const mDisplay = m >= 0 ? translate('components/BatchCard', '{{m}}m', { m: h > 0 ? padStart(m.toString(), 2, '0') : m }) : ''
-  return translate('components/BidHistory', '{{h}}{{m}} ago', { h: hDisplay, m: mDisplay })
+  let display = ''
+  if (h > 0 && m >= 0) {
+    display = translate('components/BatchHistory', '{{h}}h {{m}}m ago', { h, m })
+  } else {
+    display = translate('components/BatchHistory', '{{m}}m ago', { m })
+  }
+
+  return display
 }
