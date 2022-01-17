8643ee9317a678dae0dd939c960e63a5fa01c0d8
pierregee <pierre.gabaldon@gmail.com>
2022-01-17 10:24:55 +0800

    Remove comments

--- mobile-app/cypress/support/index.ts
+++ mobile-app/cypress/support/index.ts
@@ -23,12 +23,8 @@ Cypress.Server.defaults({
 })
 
 Cypress.on('uncaught:exception', (err) => {
-  // we expect a 3rd party library error with message 'list not defined'
-  // and don't want to fail the test so we return false
   if (err.message.includes("Cannot read properties of undefined (reading 'undefined')")) {
     return false
   }
-  // we still want to ensure there are no other unexpected
-  // errors, so we let them fail the test
 })
 
