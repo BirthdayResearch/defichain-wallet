46d558ed0149c4f8197e30d7be46c9e4a6dfb0d4
Harsh R <53080940+fullstackninja864@users.noreply.github.com>
2022-01-21 18:01:14 +0530

    chore: removed auctions announcement (#1967)

--- website/src/pages/api/v0/announcements.ts
+++ website/src/pages/api/v0/announcements.ts
@@ -9,23 +9,5 @@ export const cors = Cors({
 
 export default async function handle (req: NextApiRequest, res: NextApiResponse<AnnouncementData[]>): Promise<void> {
   await runMiddleware(req, res, cors)
-  res.json([{
-    lang: {
-      en: 'Update to the latest app version to access Auctions.',
-      de: 'Aktualisiere auf die neueste App-Version, um Zugriff auf Auktionen zu bekommen.',
-      'zh-Hans': '请即更新至最新应用程式版本以体验拍卖功能',
-      'zh-Hant': '請即更新至最新應用程式版本以體驗拍賣功能'
-    },
-    version: '<=0.22.0',
-    id: '0'
-  }, {
-    lang: {
-      en: 'Auctions are now accessible from the bottom menu bar.',
-      de: 'Auktionen lassen jetzt über die untere Menüleiste aufrufen.',
-      'zh-Hans': '现在可以从页面下方目录栏点击拍卖',
-      'zh-Hant': '現在可以從頁面下方目錄欄點擊拍賣'
-    },
-    version: '>=0.23.0',
-    id: '1'
-  }])
+  res.json([])
 }
