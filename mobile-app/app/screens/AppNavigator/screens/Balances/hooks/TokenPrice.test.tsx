c7b1ef9fd5e3cee91f8734585c59d6cb7a2f5064
pierregee <pierre.gabaldon@gmail.com>
2022-01-24 10:36:34 +0800

    Add unit test for token price hook

---
+++ mobile-app/app/screens/AppNavigator/screens/Balances/hooks/TokenPrice.test.tsx
@@ -0,0 +1,263 @@
+import { ReactElement } from 'react'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import { renderHook } from '@testing-library/react-hooks'
+import { BigNumber } from '@defichain/jellyfish-json'
+import { useTokenPrice } from '@screens/AppNavigator/screens/Balances/hooks/TokenPrice'
+import { DexItem, wallet } from '@store/wallet'
+import { block } from '@store/block'
+
+describe('Token Price - Get Token Price (DEX)', () => {
+  const getChangingPoolPairReserve = ({
+    pair1ReserveA, // BTC (BTC-DFI)
+    pair1ReserveB, // DFI (BTC-DFI)
+    pair2ReserveA, // DUSD (DUSD-DFI)
+    pair2ReserveB // DFI (DUSD-DFI)
+  }: {
+    pair1ReserveA: string
+    pair1ReserveB: string
+    pair2ReserveA: string
+    pair2ReserveB: string
+  }): DexItem[] => ([{
+    type: 'available',
+    data: {
+      id: '15',
+      symbol: 'BTC-DFI',
+      displaySymbol: 'dBTC-DFI',
+      name: 'Playground BTC-Default Defi token',
+      status: true,
+      tokenA: {
+        symbol: 'BTC',
+        displaySymbol: 'dBTC',
+        id: '1',
+        reserve: pair1ReserveA,
+        blockCommission: '0'
+      },
+      tokenB: {
+        symbol: 'DFI',
+        displaySymbol: 'DFI',
+        id: '0',
+        reserve: pair1ReserveB,
+        blockCommission: '0'
+      },
+      priceRatio: {
+        ab: '1',
+        ba: '1'
+      },
+      commission: '0',
+      totalLiquidity: {
+        token: '2500',
+        usd: '20000000'
+      },
+      tradeEnabled: true,
+      ownerAddress: 'mswsMVsyGMj1FzDMbbxw2QW3KvQAv2FKiy',
+      rewardPct: '0.1',
+      creation: {
+        tx: '79b5f7853f55f762c7550dd7c734dff0a473898bfb5639658875833accc6d461',
+        height: 132
+      },
+      apr: {
+        reward: 66.8826,
+        total: 66.8826
+      }
+    }
+  },
+  {
+    type: 'available',
+    data: {
+      id: '20',
+      symbol: 'DUSD-DFI',
+      displaySymbol: 'DUSD-DFI',
+      name: 'Decentralized USD-Default Defi token',
+      status: true,
+      tokenA: {
+        symbol: 'DUSD',
+        displaySymbol: 'DUSD',
+        id: '14',
+        reserve: pair2ReserveA,
+        blockCommission: '0'
+      },
+      tokenB: {
+        symbol: 'DFI',
+        displaySymbol: 'DFI',
+        id: '0',
+        reserve: pair2ReserveB,
+        blockCommission: '0'
+      },
+      priceRatio: {
+        ab: '10',
+        ba: '0.1'
+      },
+      commission: '0.02',
+      totalLiquidity: {
+        token: '2500',
+        usd: '16660'
+      },
+      tradeEnabled: true,
+      ownerAddress: 'mswsMVsyGMj1FzDMbbxw2QW3KvQAv2FKiy',
+      rewardPct: '0.1',
+      creation: {
+        tx: '4b8d5ec122052cdb8e8ffad63865444a10edc396d44e52957758ef7a39b228fa',
+        height: 147
+      },
+      apr: {
+        reward: 80291.23649459783,
+        total: 80291.23649459783
+      }
+    }
+  },
+  {
+    type: 'available',
+    data: {
+      id: '16',
+      symbol: 'ETH-DFI',
+      displaySymbol: 'dETH-DFI',
+      name: 'Playground ETH-Default Defi token',
+      status: true,
+      tokenA: {
+        symbol: 'ETH',
+        displaySymbol: 'dETH',
+        id: '2',
+        reserve: '100000',
+        blockCommission: '0'
+      },
+      tokenB: {
+        symbol: 'DFI',
+        displaySymbol: 'DFI',
+        id: '0',
+        reserve: '1000',
+        blockCommission: '0'
+      },
+      priceRatio: {
+        ab: '100',
+        ba: '0.01'
+      },
+      commission: '0',
+      totalLiquidity: {
+        token: '10000',
+        usd: '20000000'
+      },
+      tradeEnabled: true,
+      ownerAddress: 'mswsMVsyGMj1FzDMbbxw2QW3KvQAv2FKiy',
+      rewardPct: '0.1',
+      creation: {
+        tx: 'ac61a7ee391c2beb02043e76df4cde2baa2a7778ee6a1d4ec616a0b112462231',
+        height: 147
+      },
+      apr: {
+        reward: 66.8826,
+        total: 66.8826
+      }
+    }
+  }
+  ])
+
+  const initialState = {
+    wallet: {
+      utxoBalance: '77',
+      tokens: [],
+      allTokens: {
+        dTSLA: {
+          id: '17',
+          symbol: 'TSLA',
+          symbolKey: 'TSLA',
+          name: '',
+          decimal: 8,
+          limit: '0',
+          mintable: true,
+          tradeable: true,
+          isDAT: true,
+          isLPS: false,
+          isLoanToken: true,
+          finalized: false,
+          minted: '0',
+          creation: {
+            tx: '3a7e97db4b913fd249da2a59f2edd84f34e111fe1c775a01addfb3b96c147d40',
+            height: 151
+          },
+          destruction: {
+            tx: '0000000000000000000000000000000000000000000000000000000000000000',
+            height: -1
+          },
+          collateralAddress: 'bcrt1qyrfrpadwgw7p5eh3e9h3jmu4kwlz4prx73cqny',
+          displaySymbol: 'dTSLA'
+        }
+      },
+      poolpairs: getChangingPoolPairReserve({
+        pair1ReserveA: '5',
+        pair1ReserveB: '1000',
+        pair2ReserveA: '8300',
+        pair2ReserveB: '100'
+      }),
+      hasFetchedPoolpairData: false
+    }
+  }
+
+  const wrapper = ({ children }: {children: ReactElement}): JSX.Element => {
+    const store = configureStore({
+      preloadedState: initialState,
+      reducer: { wallet: wallet.reducer, block: block.reducer }
+    })
+
+    return (
+      <Provider store={store}>{children}</Provider>
+    )
+  }
+
+  it('should be able to get selected poolpairs', () => {
+    const { result } = renderHook(() => useTokenPrice(), { wrapper })
+
+    expect(result.current.getSelectedPoolPairs('BTC', 'DUSD')).toStrictEqual(
+      initialState.wallet.poolpairs
+        .filter(pair => ['BTC-DFI', 'DUSD-DFI'].includes(pair.data.symbol))
+        .map(pair => pair.data))
+
+    expect(result.current.getSelectedPoolPairs('BTC', 'DUSD')
+      .some(pair => pair.symbol === 'ETH-DFI')).toBe(false)
+  })
+
+  it('should be able to get pair from LP', () => {
+    const { result } = renderHook(() => useTokenPrice(), { wrapper })
+
+    expect(result.current.getPairAmountFromLP('BTC-DFI', '1')).toStrictEqual({
+      tokenAAmount: new BigNumber(0.002), // BTC = 0.0004 * 5
+      tokenBAmount: new BigNumber(0.4), // DFI = 0.0004 * 1000
+      tokenASymbol: 'BTC',
+      tokenBSymbol: 'DFI'
+    })
+  })
+
+  it('should be able to calculate price rates', () => {
+    const { result } = renderHook(() => useTokenPrice(), { wrapper })
+
+    expect(result.current.calculatePriceRates(
+      'BTC',
+      result.current.getSelectedPoolPairs('BTC', 'DUSD'), '2'))
+      .toStrictEqual({
+        aToBPrice: new BigNumber('16600'), // (1000 / 5) * (8300 / 100)
+        bToAPrice: new BigNumber('0.00006024096385542168675'), // (5 / 1000) * (100 / 8300)
+        estimated: new BigNumber('33200') // 2 * (1000 / 5) * (8300 / 100)
+      })
+
+    expect(result.current.calculatePriceRates(
+      'BTC',
+      result.current.getSelectedPoolPairs('BTC', 'DUSD'), '1'))
+      .toStrictEqual({
+        aToBPrice: new BigNumber('16600'), // (1000 / 5) * (8300 / 100)
+        bToAPrice: new BigNumber('0.00006024096385542168675'), // (5 / 1000) * (100 / 8300)
+        estimated: new BigNumber('16600') // 1 * (1000 / 5) * (8300 / 100)
+      })
+  })
+
+  it('should be able to get the token price', () => {
+    const { result } = renderHook(() => useTokenPrice(), { wrapper })
+
+    // DFI / BTC * DUSD / DFI
+    // (1000 / 5) * (8300 / 100)
+    expect(result.current.getTokenPrice('BTC')).toStrictEqual(new BigNumber('16600'))
+
+    // DFI / ETH * DUSD / DFI
+    // (1000 / 100000) * (8300 / 100)
+    expect(result.current.getTokenPrice('ETH')).toStrictEqual(new BigNumber('0.83'))
+  })
+})
