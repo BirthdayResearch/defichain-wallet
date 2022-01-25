74a2fcb10f4ee69c936b379b93c5500521be0556
JJ Adonis <josejaimecareers@gmail.com>
2022-01-25 15:34:57 +0800

    chore(website): make french announcement optional (#1992)
    * chore(website): make french announcement optional
    
    * chore(website): added french entry

--- website/src/pages/api/v0/announcements.ts
+++ website/src/pages/api/v0/announcements.ts
@@ -14,7 +14,8 @@ export default async function handle (req: NextApiRequest, res: NextApiResponse<
       en: 'Update to the latest app version to access Auctions.',
       de: 'Aktualisiere auf die neueste App-Version, um Zugriff auf Auktionen zu bekommen.',
       'zh-Hans': '请即更新至最新应用程式版本以体验拍卖功能',
-      'zh-Hant': '請即更新至最新應用程式版本以體驗拍賣功能'
+      'zh-Hant': '請即更新至最新應用程式版本以體驗拍賣功能',
+      fr: 'Update to the latest app version to access Auctions.'
     },
     version: '<=0.22.0',
     id: '0'
@@ -23,7 +24,8 @@ export default async function handle (req: NextApiRequest, res: NextApiResponse<
       en: 'Auctions are now accessible from the bottom menu bar.',
       de: 'Auktionen lassen jetzt über die untere Menüleiste aufrufen.',
       'zh-Hans': '现在可以从页面下方目录栏点击拍卖',
-      'zh-Hant': '現在可以從頁面下方目錄欄點擊拍賣'
+      'zh-Hant': '現在可以從頁面下方目錄欄點擊拍賣',
+      fr: 'Auctions are now accessible from the bottom menu bar.'
     },
     version: '>=0.23.0',
     id: '1'
