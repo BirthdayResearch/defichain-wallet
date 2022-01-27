bf2e99b1f3351af8a3c1550c62ce235945c7895b
chloezxyy <chloezhengxy@gmail.com>
2022-01-27 11:40:15 +0800

    use dayjs library instead of Date.parse()

--- mobile-app/app/hooks/useBlockchainStatus.ts
+++ mobile-app/app/hooks/useBlockchainStatus.ts
@@ -3,9 +3,10 @@ import { useSelector } from 'react-redux'
 import { RootState } from '@store'
 import { getReleaseChannel } from '@api/releaseChannel'
 import { getEnvironment } from '@environment'
+import dayjs from 'dayjs'
 
 // MAX_TIME_DIFF set to 45 mins to display warning message (in AnnouncementBanner) when blockchain is down only in Production mode, else 5 seconds for local runs
-const MAX_TIME_DIFF = getEnvironment(getReleaseChannel()).debug ? 5 * 1000 : 45 * 60 * 1000
+const MAX_TIME_DIFF = getEnvironment(getReleaseChannel()).debug ? 5 * 1000 : 45 * 60 * 1000 // 45 mins in milliseconds
 
 export function useBlockchainStatus (): boolean {
     const { lastSync, lastSuccessfulSync } = useSelector((state: RootState) => state.block)
@@ -14,8 +15,8 @@ export function useBlockchainStatus (): boolean {
     useEffect(() => {
         function getBlockchainStatus (): boolean {
             if (lastSync !== undefined && lastSuccessfulSync !== undefined) {
-                const lastSyncTime = Date.parse(lastSync)
-                const lastSuccessfulSyncTime = Date.parse(lastSuccessfulSync)
+                const lastSyncTime = dayjs(lastSync).valueOf() // returns in milliseconds
+                const lastSuccessfulSyncTime = dayjs(lastSuccessfulSync).valueOf()
                 const timeDifference = lastSyncTime - lastSuccessfulSyncTime
                 return timeDifference > MAX_TIME_DIFF
             }
