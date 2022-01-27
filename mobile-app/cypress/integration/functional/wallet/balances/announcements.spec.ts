dd1bcf173fc6cea7df78cb280f065b74ce320fd2
chloezxyy <chloezhengxy@gmail.com>
2022-01-27 17:59:15 +0800

    resolve comments and update test cases desciption

--- mobile-app/cypress/integration/functional/wallet/balances/announcements.spec.ts
+++ mobile-app/cypress/integration/functional/wallet/balances/announcements.spec.ts
@@ -107,7 +107,63 @@ context('Wallet - Balances - Announcements', () => {
     cy.reload()
     cy.getByTestID('announcements_banner').should('not.exist')
   })
+})
 
+context('Wallet - Balances - Announcements - Blockchain warning messages', () => {
+  const sampleAnnouncements = [{
+    lang: {
+      en: 'Guidelines',
+      de: 'Richtlinien',
+      'zh-Hans': '指导方针',
+      'zh-Hant': '指導方針'
+    },
+    version: '0.0.0 - 0.12.0',
+    url: {
+      ios: '',
+      android: '',
+      macos: '',
+      windows: '',
+      web: ''
+    }
+  }, {
+    lang: {
+      en: 'Refresh',
+      de: 'Erneuern',
+      'zh-Hans': '刷新',
+      'zh-Hant': '刷新'
+    },
+    version: '>=0.12.1',
+    url: {
+      ios: 'https://foo.ios',
+      android: 'https://foo.android',
+      macos: 'https://foo.macos',
+      windows: 'https://foo.windows',
+      web: 'https://foo.web'
+    }
+  }]
+
+  const sampleAnnouncementsWithID = [{
+    lang: {
+      en: 'Guidelines',
+      de: 'Richtlinien',
+      'zh-Hans': '指导方针',
+      'zh-Hant': '指導方針'
+    },
+    version: '0.0.0 - 0.12.0',
+    url: {
+      ios: '',
+      android: '',
+      macos: '',
+      windows: '',
+      web: ''
+    },
+    id: '1'
+  }]
+
+  beforeEach(function () {
+    cy.clearLocalStorage()
+    cy.createEmptyWallet(true)
+  })
   it('should be able to display announcement with ID not within hidden list', function () {
     cy.intercept('**/announcements', {
       statusCode: 200,
@@ -161,7 +217,7 @@ context('Wallet - Balances - Announcements', () => {
       body: sampleAnnouncementsWithID
     })
     localStorage.setItem('WALLET.HIDDEN_ANNOUNCEMENTS', '[]')
-    cy.url().should('include', 'app/balances', () => {
+    cy.getByTestID('close_announcement').click().should(() => {
       expect(localStorage.getItem('WALLET.HIDDEN_ANNOUNCEMENTS')).to.eq('["1"]')
     })
     cy.getByTestID('announcements_banner').should('not.exist')
