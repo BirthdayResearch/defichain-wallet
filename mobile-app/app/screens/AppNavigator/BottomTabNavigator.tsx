1500996decbacb78d29b434bbc4724a403d5a091
Harsh R <53080940+fullstackninja864@users.noreply.github.com>
2022-02-09 14:33:09 +0530

    chore(ui-ux): removed feature gate for loans (#2095)
    * chore(ui-ux): removed featuregate for loans
    
    * added feature gate test for auctions
    
    * updated test case for auction feature gate
    
    * Updated loan token specs
    
    Co-authored-by: thedoublejay <josejaimecareers@gmail.com>

--- mobile-app/app/screens/AppNavigator/BottomTabNavigator.tsx
+++ mobile-app/app/screens/AppNavigator/BottomTabNavigator.tsx
@@ -65,22 +65,20 @@ export function BottomTabNavigator (): JSX.Element {
           }}
         />
 
-        {isFeatureAvailable('loan') && (
-          <BottomTab.Screen
-            component={LoansNavigator}
-            name={translate('BottomTabNavigator', 'Loans')}
-            options={{
-              tabBarTestID: 'bottom_tab_loans',
-              tabBarIcon: ({ color }) => (
-                <MaterialIcons
-                  color={color}
-                  name='credit-card'
-                  size={24}
-                />
-              )
-            }}
-          />
-        )}
+        <BottomTab.Screen
+          component={LoansNavigator}
+          name={translate('BottomTabNavigator', 'Loans')}
+          options={{
+            tabBarTestID: 'bottom_tab_loans',
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons
+                color={color}
+                name='credit-card'
+                size={24}
+              />
+            )
+          }}
+        />
 
         {isFeatureAvailable('auction') && (
           <BottomTab.Screen
