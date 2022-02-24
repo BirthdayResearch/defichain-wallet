d34df791f069f9000f1e731a281abe80b1013692
JJ Adonis <josejaimecareers@gmail.com>
2022-02-24 18:30:29 +0800

    fix(ui-ux): remove animation causing issues (#2265)
    * fix(ui-ux): remove animation causing issues
    
    * fix(ui-ux): increase size of caret

--- mobile-app/app/screens/AppNavigator/screens/Dex/components/PoolPairCards/PoolPairCards.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Dex/components/PoolPairCards/PoolPairCards.tsx
@@ -1,5 +1,4 @@
 import BigNumber from 'bignumber.js'
-import Animated, { FadeInUp } from 'react-native-reanimated'
 import { View } from '@components'
 import {
   ThemedFlatList,
@@ -121,9 +120,9 @@ export function PoolPairCards ({
           .toLowerCase()
           .includes(debouncedSearchTerm.trim().toLowerCase())
       ).sort((a, b) =>
-          availablePairs.findIndex(x => x.data.id === a.data.id) -
-          availablePairs.findIndex(x => x.data.id === b.data.id
-          ))
+        availablePairs.findIndex(x => x.data.id === a.data.id) -
+        availablePairs.findIndex(x => x.data.id === b.data.id
+        ))
     )
   }, [yourPairs, debouncedSearchTerm])
 
@@ -296,15 +295,13 @@ export function PoolPairCards ({
               dark={tailwind('text-darkprimary-500')}
               iconType='MaterialIcons'
               name={!isExpanded ? 'expand-more' : 'expand-less'}
-              size={20}
+              size={32}
             />
           </TouchableOpacity>
         </View>
         {
           isExpanded &&
-            <Animated.View
-              entering={FadeInUp}
-            >
+            <View>
               <ThemedView
                 style={tailwind('border-b h-px mt-4')}
                 light={tailwind('border-gray-100')}
@@ -325,7 +322,7 @@ export function PoolPairCards ({
               }
                 testID={type}
               />
-            </Animated.View>
+            </View>
         }
       </ThemedView>
     )
