c0b6e07e8edf852d1b4fd8dfda3ea483961be364
Keng Ye <kengyeleow@gmail.com>
2022-03-07 11:38:29 +0800

    wip: add BottomSheetAddressList placeholder file

---
+++ mobile-app/app/screens/AppNavigator/screens/Balances/components/BottomSheetAddressList.tsx
@@ -0,0 +1,25 @@
+// import { View } from '@components'
+// import { Button } from '@components/Button'
+// import { ThemedFlatList, ThemedIcon, ThemedScrollView, ThemedText, ThemedTouchableOpacity } from '@components/themed'
+// import { translate } from '@translations'
+// import React, { memo } from 'react'
+// import { Platform, TouchableOpacity } from 'react-native'
+// import { tailwind } from '@tailwind'
+// import { RandomAvatar } from './RandomAvatar'
+// import { openURL } from '@api/linking'
+// import { useDeFiScanContext } from '@shared-contexts/DeFiScanContext'
+// import { BottomSheetFlatList } from '@gorhom/bottom-sheet'
+
+// interface BottomSheetAddressListProps {
+// }
+
+// export const BottomSheetAddressList = (props: BottomSheetAddressListProps): React.MemoExoticComponent<() => JSX.Element> => memo(() => {
+//   const flatListComponents = {
+//     mobile: BottomSheetFlatList,
+//     web: ThemedFlatList
+//   }
+//   const FlatList = Platform.OS === 'web' ? flatListComponents.web : flatListComponents.mobile
+//   return (
+//     <></>
+//   )
+// })
