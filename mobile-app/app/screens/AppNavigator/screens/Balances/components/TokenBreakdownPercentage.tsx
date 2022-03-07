cfe9f2cb1a23d58b32b1e81a5658d38985522730
Keng Ye <kengyeleow@gmail.com>
2022-03-07 19:59:09 +0800

    feat: add toggle button to breakdown component
    Change to token agnostic component

--- mobile-app/app/screens/AppNavigator/screens/Balances/components/TokenBreakdownPercentage.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Balances/components/TokenBreakdownPercentage.tsx
@@ -4,31 +4,50 @@ import { ThemedIcon, ThemedText, ThemedView } from '@components/themed'
 import { tailwind } from '@tailwind'
 import { translate } from '@translations'
 import BigNumber from 'bignumber.js'
+import { TouchableOpacity } from 'react-native'
 import NumberFormat from 'react-number-format'
-import { useTokenBreakdownPercentage, useTokenLockedBalance } from '../hooks/TokenLockedBalance'
+import { useTokenBreakdownPercentage } from '../hooks/TokenLockedBalance'
 
 interface TokenBreakdownPercentageProps {
-  availableDFI: BigNumber
+  lockedAmount: BigNumber
+  availableAmount: BigNumber
+  symbol: string
+  onBreakdownPress: () => void
+  isBreakdownExpanded: boolean
 }
 
 export function TokenBreakdownPercentage (props: TokenBreakdownPercentageProps): JSX.Element {
-  const lockedDfi = useTokenLockedBalance({ symbol: 'DFI' })
   const {
     availablePercentage,
     lockedPercentage
-  } = useTokenBreakdownPercentage({ available: props.availableDFI, locked: lockedDfi })
+  } = useTokenBreakdownPercentage({ available: props.availableAmount, locked: props.lockedAmount })
 
   return (
-    <View style={tailwind('flex flex-row items-center bg-transparent')}>
-      <BreakdownPercentageItem
-        type='available'
-        isDfi
-        percentage={availablePercentage}
-      />
-      <BreakdownPercentageItem
-        type='locked'
-        percentage={lockedPercentage}
-      />
+    <View style={tailwind('flex flex-row items-center bg-transparent justify-between mb-1')}>
+      <View style={tailwind('flex flex-row items-center')}>
+        <BreakdownPercentageItem
+          type='available'
+          isDfi={props.symbol === 'DFI'}
+          percentage={availablePercentage}
+        />
+        <BreakdownPercentageItem
+          type='locked'
+          percentage={lockedPercentage}
+        />
+      </View>
+      <TouchableOpacity
+        onPress={props.onBreakdownPress}
+        style={tailwind('flex flex-row pb-2 pt-1.5')}
+        testID={`details_${props.symbol}`}
+      >
+        <ThemedIcon
+          light={tailwind('text-primary-500')}
+          dark={tailwind('text-darkprimary-500')}
+          iconType='MaterialIcons'
+          name={!props.isBreakdownExpanded ? 'expand-more' : 'expand-less'}
+          size={28}
+        />
+      </TouchableOpacity>
     </View>
   )
 }
@@ -51,14 +70,14 @@ function BreakdownPercentageItem (props: BreakdownPercentageItemProps): JSX.Elem
         ? (props.isDfi === true && (
           <SymbolIcon symbol='DFI' styleProps={tailwind('w-4 h-4')} />
         ))
-: (
-  <ThemedIcon
-    light={tailwind('text-gray-600')}
-    dark={tailwind('text-gray-500')}
-    iconType='MaterialIcons'
-    name='lock'
-    size={16}
-  />
+        : (
+          <ThemedIcon
+            light={tailwind('text-gray-600')}
+            dark={tailwind('text-gray-500')}
+            iconType='MaterialIcons'
+            name='lock'
+            size={16}
+          />
         )}
       <ThemedText
         light={tailwind('text-gray-500')}
