785bd7aec29364f3fe3ecbcde8913390a88f5e3b
Keng Ye <kengyeleow@gmail.com>
2022-03-11 18:50:10 +0800

    test: add snapshot test

---
+++ mobile-app/app/screens/AppNavigator/screens/Balances/components/TokenBreakdownDetails.test.tsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react-native'
+import { TokenBreakdownDetails } from './TokenBreakdownDetails'
+import BigNumber from 'bignumber.js'
+
+describe('Token Breakdown Details', () => {
+  it('should match snapshot', async () => {
+    const component = (
+      <TokenBreakdownDetails
+        hasFetchedToken
+        lockedAmount={new BigNumber('1')}
+        lockedValue={new BigNumber('123')}
+        availableAmount={new BigNumber('99')}
+        availableValue={new BigNumber('999999.12345678')}
+        testID='foo'
+      />
+    )
+    const rendered = render(component)
+    expect(rendered.toJSON()).toMatchSnapshot()
+  })
+})
