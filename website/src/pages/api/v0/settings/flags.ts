289c4a9a10549ce58e5e5dd0963c1fb60114304b
JJ Adonis <josejaimecareers@gmail.com>
2022-03-16 11:38:23 +0800

    feature(website-api): updated announcements and flags

--- website/src/pages/api/v0/settings/flags.ts
+++ website/src/pages/api/v0/settings/flags.ts
@@ -10,37 +10,31 @@ export const cors = Cors({
 
 export default async function handle (req: NextApiRequest, res: NextApiResponse<FeatureFlag[]>): Promise<void> {
   await runMiddleware(req, res, cors)
-  res.json([{
-    id: 'loan',
-    name: 'Loans',
-    stage: 'public',
-    version: '>=0.17.0',
-    description: 'Browse loan tokens provided by DeFiChain',
-    networks: [EnvironmentNetwork.MainNet, EnvironmentNetwork.TestNet, EnvironmentNetwork.RemotePlayground, EnvironmentNetwork.LocalPlayground],
-    platforms: ['ios', 'android', 'web']
-  }, {
-    id: 'auction',
-    name: 'Auction',
-    stage: 'beta',
-    version: '<0.23.0',
-    description: 'Browse auctions provided by DeFiChain',
-    networks: [EnvironmentNetwork.MainNet, EnvironmentNetwork.TestNet, EnvironmentNetwork.RemotePlayground, EnvironmentNetwork.LocalPlayground],
-    platforms: ['ios', 'android', 'web']
-  }, {
-    id: 'auction',
-    name: 'Auction',
-    stage: 'public',
-    version: '>=0.23.0',
-    description: 'Browse auctions provided by DeFiChain',
-    networks: [EnvironmentNetwork.MainNet, EnvironmentNetwork.TestNet, EnvironmentNetwork.RemotePlayground, EnvironmentNetwork.LocalPlayground],
-    platforms: ['ios', 'android', 'web']
-  }, {
-    id: 'dfi_loan_payment',
-    name: 'DFI Loan Payment',
-    stage: 'public',
-    version: '>=1.1.1',
-    description: 'DFI Loan Payment',
-    networks: [EnvironmentNetwork.MainNet, EnvironmentNetwork.TestNet, EnvironmentNetwork.RemotePlayground, EnvironmentNetwork.LocalPlayground],
-    platforms: ['ios', 'android', 'web']
-  }])
+  res.json([
+    {
+      id: 'loan',
+      name: 'Loans',
+      stage: 'public',
+      version: '>=0.17.0',
+      description: 'Browse loan tokens provided by DeFiChain',
+      networks: [EnvironmentNetwork.MainNet, EnvironmentNetwork.TestNet, EnvironmentNetwork.RemotePlayground, EnvironmentNetwork.LocalPlayground],
+      platforms: ['ios', 'android', 'web']
+    },
+    {
+      id: 'auction',
+      name: 'Auction',
+      stage: 'public',
+      version: '>=0.23.0',
+      description: 'Browse auctions provided by DeFiChain',
+      networks: [EnvironmentNetwork.MainNet, EnvironmentNetwork.TestNet, EnvironmentNetwork.RemotePlayground, EnvironmentNetwork.LocalPlayground],
+      platforms: ['ios', 'android', 'web']
+    }, {
+      id: 'dfi_loan_payment',
+      name: 'DFI Loan Payment',
+      stage: 'public',
+      version: '>=1.1.1',
+      description: 'DFI Loan Payment',
+      networks: [EnvironmentNetwork.MainNet, EnvironmentNetwork.TestNet, EnvironmentNetwork.RemotePlayground, EnvironmentNetwork.LocalPlayground],
+      platforms: ['ios', 'android', 'web']
+    }])
 }
