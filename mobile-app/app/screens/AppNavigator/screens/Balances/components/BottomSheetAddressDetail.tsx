dfba46423921a61ce89528fa371091245055c5ac
Keng Ye <kengyeleow@gmail.com>
2022-03-16 18:40:04 +0800

    feat: add toast to active address onPress
    Copy address to clipboard
    
    Update UI to separate view on defiscan button

--- mobile-app/app/screens/AppNavigator/screens/Balances/components/BottomSheetAddressDetail.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Balances/components/BottomSheetAddressDetail.tsx
@@ -1,13 +1,16 @@
 import { View } from '@components'
-import { ThemedIcon, ThemedScrollView, ThemedText, ThemedView } from '@components/themed'
+import { ThemedIcon, ThemedScrollView, ThemedText, ThemedTouchableOpacity } from '@components/themed'
 import { translate } from '@translations'
-import React, { memo } from 'react'
+import React, { memo, useCallback, useEffect, useState } from 'react'
 import { TouchableOpacity } from 'react-native'
 import { tailwind } from '@tailwind'
 import { RandomAvatar } from './RandomAvatar'
 import { openURL } from '@api/linking'
 import { useDeFiScanContext } from '@shared-contexts/DeFiScanContext'
 import { IconButton } from '@components/IconButton'
+import { useToast } from 'react-native-toast-notifications'
+import { debounce } from 'lodash'
+import * as Clipboard from 'expo-clipboard'
 
 interface BottomSheetAddressDetailProps {
   address: string
@@ -17,6 +20,31 @@ interface BottomSheetAddressDetailProps {
 }
 
 export const BottomSheetAddressDetail = (props: BottomSheetAddressDetailProps): React.MemoExoticComponent<() => JSX.Element> => memo(() => {
+  const toast = useToast()
+  const [showToast, setShowToast] = useState(false)
+  const TOAST_DURATION = 3000
+
+  const onActiveAddressPress = useCallback(debounce(() => {
+    if (showToast) {
+      return
+    }
+    setShowToast(true)
+    setTimeout(() => setShowToast(false), TOAST_DURATION)
+  }, 500), [showToast])
+
+  useEffect(() => {
+    if (showToast) {
+      Clipboard.setString(props.address)
+      toast.show('Copied to clipboard', {
+        type: 'wallet_success',
+        placement: 'top',
+        duration: TOAST_DURATION
+      })
+    } else {
+      toast.hideAll()
+    }
+  }, [showToast, props.address])
+
   return (
     <ThemedScrollView
       style={tailwind('flex flex-col')}
@@ -37,46 +65,54 @@ export const BottomSheetAddressDetail = (props: BottomSheetAddressDetailProps): 
           </TouchableOpacity>
         </View>
         <RandomAvatar name={props.address} size={64} />
-        <DefiscanButton address={props.address} />
-        <IconButton
-          iconLabel={translate('components/BottomSheetAddressDetail', 'RECEIVE IN WALLET')}
-          iconName='arrow-downward'
-          iconSize={18}
-          iconType='MaterialIcons'
-          style={tailwind('p-2')}
-          onPress={props.onReceiveButtonPress}
-        />
+        <ActiveAddress address={props.address} onPress={onActiveAddressPress} />
+        <AddressDetailAction address={props.address} onReceivePress={props.onReceiveButtonPress} />
       </View>
     </ThemedScrollView>
   )
 })
 
-function DefiscanButton ({ address }: { address: string }): JSX.Element {
-  const { getAddressUrl } = useDeFiScanContext()
-
+function ActiveAddress ({ address, onPress }: { address: string, onPress: () => void }): JSX.Element {
   return (
-    <ThemedView
-      style={tailwind('mt-4 rounded-2xl py-1 pl-4 pr-3 flex flex-row items-center mb-2')}
+    <ThemedTouchableOpacity
+      style={tailwind('my-4 rounded-2xl py-1 px-2')}
       light={tailwind('bg-gray-50')}
       dark={tailwind('bg-gray-900')}
+      onPress={onPress}
     >
       <ThemedText
-        style={tailwind('font-medium text-sm')}
+        style={tailwind('text-sm')}
         light={tailwind('text-black')}
         dark={tailwind('text-white')}
         selectable
       >
         {address}
       </ThemedText>
-      <ThemedIcon
-        iconType='MaterialCommunityIcons'
-        name='open-in-new'
-        size={18}
-        style={tailwind('ml-2')}
-        light={tailwind('text-primary-500')}
-        dark={tailwind('text-darkprimary-500')}
+    </ThemedTouchableOpacity>
+  )
+}
+
+function AddressDetailAction ({ address, onReceivePress }: { address: string, onReceivePress: () => void }): JSX.Element {
+  const { getAddressUrl } = useDeFiScanContext()
+
+  return (
+    <View style={tailwind('flex flex-row')}>
+      <IconButton
+        iconLabel={translate('components/BottomSheetAddressDetail', 'RECEIVE')}
+        iconName='arrow-downward'
+        iconSize={18}
+        iconType='MaterialIcons'
+        style={tailwind('py-2 px-3 mr-1 w-6/12 flex-row justify-center')}
+        onPress={onReceivePress}
+      />
+      <IconButton
+        iconLabel={translate('components/BottomSheetAddressDetail', 'VIEW ON SCAN')}
+        iconName='open-in-new'
+        iconSize={18}
+        iconType='MaterialIcons'
+        style={tailwind('py-2 px-3 ml-1 w-6/12 flex-row justify-center')}
         onPress={async () => await openURL(getAddressUrl(address))}
       />
-    </ThemedView>
+    </View>
   )
 }
