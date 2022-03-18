d60dabcef16a379251d5ef2d39280cc706014dcb
Keng Ye <kengyeleow@gmail.com>
2022-03-18 16:12:18 +0800

    feat: update dark mode styling of toast

--- mobile-app/app/components/WalletToast.tsx
+++ mobile-app/app/components/WalletToast.tsx
@@ -10,15 +10,15 @@ interface WalletToastProps {
 export function WalletToast (props: WalletToastProps): JSX.Element {
   return (
     <ThemedView
-      style={tailwind('py-1.5 px-4 mt-8 rounded-lg')}
+      style={tailwind('pb-1.5 pt-1 px-4 mt-8 rounded-lg')}
       light={tailwind('bg-gray-600')}
-      dark={tailwind('bg-gray-300')}
+      dark={tailwind('bg-gray-800')}
       testID='wallet_toast'
     >
       <ThemedText
         style={tailwind('text-lg')}
         light={tailwind('text-white')}
-        dark={tailwind('text-black')}
+        dark={tailwind('text-white')}
       >
         {translate('components/WalletToast', props.toast.message as string)}
       </ThemedText>
