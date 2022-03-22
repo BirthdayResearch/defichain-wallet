ebeb176c97c247644eb9d3db03bdb40440e44f3f
pierregee <pierre.gabaldon@gmail.com>
2022-03-22 11:33:03 +0800

    performance(core): optimize to cache and only run on screen focus

--- mobile-app/app/screens/AppNavigator/screens/Dex/hook/SwappableTokens.ts
+++ mobile-app/app/screens/AppNavigator/screens/Dex/hook/SwappableTokens.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import BigNumber from 'bignumber.js'
 import { RootState } from '@store'
@@ -6,90 +6,106 @@ import { DexItem, fetchSwappableTokens, tokensSelector } from '@store/wallet'
 import { useWhaleApiClient } from '@shared-contexts/WhaleContext'
 import { BottomSheetToken } from '@components/BottomSheetTokenList'
 import { TokenState } from '../CompositeSwap/CompositeSwapScreen'
+import { CacheApi } from '@api/cache'
+import { useNetworkContext } from '@shared-contexts/NetworkContext'
+import { useFocusEffect } from '@react-navigation/core'
+import { AllSwappableTokensResult } from '@defichain/whale-api-client/dist/api/poolpairs'
 
 interface TokenPrice {
-    toTokens: BottomSheetToken[]
-    fromTokens: BottomSheetToken[]
+  toTokens: BottomSheetToken[]
+  fromTokens: BottomSheetToken[]
 }
 export function useSwappableTokens (fromTokenId: string | undefined): TokenPrice {
-    const client = useWhaleApiClient()
-    const dispatch = useDispatch()
-    const blockCount = useSelector((state: RootState) => state.block.count)
-    const { swappableTokens, poolpairs } = useSelector((state: RootState) => state.wallet)
-    const tokens = useSelector((state: RootState) => tokensSelector(state.wallet))
+  const client = useWhaleApiClient()
+  const { network } = useNetworkContext()
+  const dispatch = useDispatch()
+  const blockCount = useSelector((state: RootState) => state.block.count)
+  const { swappableTokens, poolpairs } = useSelector((state: RootState) => state.wallet)
+  const tokens = useSelector((state: RootState) => tokensSelector(state.wallet))
 
-    const _allTokens = useMemo(() => {
-        return poolpairs.reduce((tokensInPair: TokenState[], pair: DexItem): TokenState[] => {
-            const hasTokenA = tokensInPair.some(token => pair.data.tokenA.id === token.id)
-            const hasTokenB = tokensInPair.some(token => pair.data.tokenB.id === token.id)
-            const tokensToAdd: TokenState[] = []
-            if (!hasTokenA) {
-                tokensToAdd.push(pair.data.tokenA)
-            }
-            if (!hasTokenB) {
-                tokensToAdd.push(pair.data.tokenB)
-            }
+  const [fromTokens, setFromTokens] = useState<BottomSheetToken[]>([])
+  const [allTokens, setAllTokens] = useState<TokenState[]>()
 
-            return [...tokensInPair, ...tokensToAdd]
-        }, [])
-    }, [poolpairs])
+  const cacheKey = `WALLET.${network}.${blockCount ?? 0}.SWAP_FROM_${fromTokenId ?? 0}`
+  const cachedData = CacheApi.get(cacheKey)
 
-    const toTokens = useMemo(() => {
-        const swappableToTokens = swappableTokens[fromTokenId === '0_unified' ? '0' : fromTokenId ?? '']
-        if (swappableToTokens === undefined) {
-            return []
+  /* Opted out of using useMemo to ensure it'll only run when screen is focused */
+  useFocusEffect(useCallback(() => {
+    const _allTokens = poolpairs.reduce((tokensInPair: TokenState[], pair: DexItem): TokenState[] => {
+      const hasTokenA = tokensInPair.some(token => pair.data.tokenA.id === token.id)
+      const hasTokenB = tokensInPair.some(token => pair.data.tokenB.id === token.id)
+      const tokensToAdd: TokenState[] = []
+      if (!hasTokenA) {
+        tokensToAdd.push(pair.data.tokenA)
+      }
+      if (!hasTokenB) {
+        tokensToAdd.push(pair.data.tokenB)
+      }
+
+      return [...tokensInPair, ...tokensToAdd]
+    }, [])
+
+    const swappableFromTokens: BottomSheetToken[] = _allTokens
+      .map((token) => {
+        const tokenId = token.id === '0' ? '0_unified' : token.id
+        const ownedToken = tokens.find(t => t.id === tokenId)
+        return {
+          tokenId: tokenId,
+          available: new BigNumber(ownedToken === undefined ? 0 : ownedToken.amount),
+          token: {
+            displaySymbol: token.displaySymbol,
+            name: '', // not available in API,
+            symbol: token.symbol
+          },
+          reserve: token.reserve
         }
+      }).sort((a, b) => b.available.minus(a.available).toNumber())
 
-        const toTokens: BottomSheetToken[] = swappableToTokens.swappableTokens
-            .map((token) => {
-                const tokenId = token.id === '0' ? '0_unified' : token.id
-                const tokenData = _allTokens.find(t => t.id === token.id)
+    setAllTokens(_allTokens)
+    setFromTokens(swappableFromTokens)
+  }, [poolpairs]))
 
-                return {
-                    tokenId: tokenId,
-                    available: new BigNumber(tokenData?.reserve ?? NaN),
-                    token: {
-                        displaySymbol: token.displaySymbol,
-                        name: '', // not available in API,
-                        symbol: token.symbol
-                    },
-                    reserve: tokenData?.reserve ?? '' // TODO(PIERRE): Ask whale to add reserve on response
-                }
-            })
-
-        return toTokens
-    }, [swappableTokens, fromTokenId])
-
-    const fromTokens = useMemo(() => {
-        const swappableFromTokens: BottomSheetToken[] = _allTokens
-            .map((token) => {
-                const tokenId = token.id === '0' ? '0_unified' : token.id
-                const ownedToken = tokens.find(t => t.id === tokenId)
-                return {
-                    tokenId: tokenId,
-                    available: new BigNumber(ownedToken === undefined ? 0 : ownedToken.amount),
-                    token: {
-                        displaySymbol: token.displaySymbol,
-                        name: '', // not available in API,
-                        symbol: token.symbol
-                    },
-                    reserve: token.reserve
-                }
-            }).sort((a, b) => b.available.minus(a.available).toNumber())
-
-        return swappableFromTokens
-    }, [poolpairs])
-
-    useEffect(() => {
-        if (fromTokenId !== undefined) {
-            dispatch(fetchSwappableTokens({
-                client, fromTokenId: fromTokenId === '0_unified' ? '0' : fromTokenId
-            }))
-        }
-    }, [blockCount, fromTokenId])
-
-    return {
-        toTokens,
-        fromTokens
+  const toTokens = useMemo(() => {
+    const swappableToTokens = swappableTokens[fromTokenId === '0_unified' ? '0' : fromTokenId ?? '']
+    const cachedSwappableToTokens = cachedData as AllSwappableTokensResult ?? swappableToTokens
+    if (fromTokenId !== undefined && swappableToTokens !== undefined) {
+      CacheApi.set(cacheKey, swappableTokens[fromTokenId === '0_unified' ? '0' : fromTokenId ?? ''])
     }
+
+    if (cachedSwappableToTokens === undefined || cachedSwappableToTokens.swappableTokens.length === 0 || allTokens === undefined) {
+      return []
+    }
+
+    const toTokens: BottomSheetToken[] = cachedSwappableToTokens.swappableTokens
+      .map((token) => {
+        const tokenId = token.id === '0' ? '0_unified' : token.id
+        const tokenData = allTokens.find(t => t.id === token.id)
+
+        return {
+          tokenId: tokenId,
+          available: new BigNumber(tokenData?.reserve ?? NaN),
+          token: {
+            displaySymbol: token.displaySymbol,
+            name: '', // not available in API,
+            symbol: token.symbol
+          },
+          reserve: tokenData?.reserve ?? '' // TODO(PIERRE): Ask whale to add reserve on response
+        }
+      })
+
+    return toTokens
+  }, [swappableTokens, fromTokenId])
+
+  useFocusEffect(useCallback(() => {
+    if (fromTokenId !== undefined && cachedData === undefined) {
+      dispatch(fetchSwappableTokens({
+        client, fromTokenId: fromTokenId === '0_unified' ? '0' : fromTokenId
+      }))
+    }
+  }, [fromTokenId]))
+
+  return {
+    toTokens,
+    fromTokens
+  }
 }
