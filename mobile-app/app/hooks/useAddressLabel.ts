84f7f705c2dde485a48dbe9806842b0840ffb65f
JJ Adonis <josejaimecareers@gmail.com>
2022-03-24 16:33:26 +0800

    feature(core): display address

--- mobile-app/app/hooks/useAddressLabel.ts
+++ mobile-app/app/hooks/useAddressLabel.ts
@@ -4,7 +4,7 @@ import { RootState } from '@store'
 export function useAddressLabel (address: string): string | null {
   const addresses = useSelector((state: RootState) => state.userPreferences?.addresses)
   let label = null
-  if (addresses[address] != null) {
+  if (addresses?.[address] != null) {
     label = addresses[address].label
   }
   return label
