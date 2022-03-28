d266b68d778a10f5757538782c9edfea753a2c7a
Keng Ye <40191153+kyleleow@users.noreply.github.com>
2022-03-28 15:39:44 +0800

    chore(e2e): add tests for edit address label (#2492)
    * fix(ui-ux): set min & max width for AddressSelectionButton
    
    Trim label input before save and during validation
    
    * fix: button label alignment
    
    Increase web modal height to fix failing e2e
    
    * fix: replace with absolute styling
    
    For web bottom sheet modal to not scale with browser view port
    
    * test: add e2e for edit address label flow
    
    * test: increase timeout for utxo to top up
    
    * test: move send dfi command to the same it() test
    
    To ensure sendDFItoWallet command is triggerred on different addresses
    
    To remove spaces in between input string during assertion of `contains()`
    
    Co-authored-by: JJ Adonis <josejaimecareers@gmail.com>

--- mobile-app/app/screens/AppNavigator/screens/Balances/BalancesScreen.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Balances/BalancesScreen.tsx
@@ -296,11 +296,11 @@ export function BalancesScreen ({ navigation }: Props): JSX.Element {
               screenList={bottomSheetScreen}
               isModalDisplayed={isModalDisplayed}
               modalStyle={{
-                position: 'fixed',
+                position: 'absolute',
+                bottom: '0',
                 height: '505px',
                 width: '375px',
-                zIndex: 50,
-                top: '33%'
+                zIndex: 50
               }}
             />
           )
