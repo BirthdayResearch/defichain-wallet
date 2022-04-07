cad8d1ff64457a81da6d5e4908bc68a48e8dd608
Chloezxyy <chloe@cakedefi.com>
2022-04-07 17:02:27 +0800

    create async storage to store portfolio currency

---
+++ mobile-app/app/api/persistence/portfolio_currency_storage.ts
@@ -0,0 +1,18 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { PortfolioButtonGroupTabKey } from '@screens/AppNavigator/screens/Balances/components/TotalPortfolio'
+
+const KEY = 'WALLET.PORTFOLIO_CURRENCY'
+
+async function set (portfolioCurrency: PortfolioButtonGroupTabKey): Promise<void> {
+    await AsyncStorage.setItem(KEY, JSON.stringify(portfolioCurrency))
+}
+
+async function get (): Promise<PortfolioButtonGroupTabKey> {
+    const val = await AsyncStorage.getItem(KEY) ?? PortfolioButtonGroupTabKey.USD
+    return JSON.parse(val)
+}
+
+export const PortfolioCurrencyPersistence = {
+    set,
+    get
+}
