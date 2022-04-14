2527ecae15cc463521dfa2354d2f90aac1b8e514
thedoublejay <josejaimecareers@gmail.com>
2022-04-14 13:34:39 +0800

    added a fallback on identifier

--- app.config.js
+++ app.config.js
@@ -1,6 +1,7 @@
 // TODO(thedoublejay) - Remove when migration process has been completed
 export default ({ config }) => {
   const bundleIdentifier = process.env.EXPO_BUNDLE_IDENTIFIER != null ? process.env.EXPO_BUNDLE_IDENTIFIER : config.ios.bundleIdentifier
+  console.log(bundleIdentifier)
   config.ios = {
     ...config.ios,
     bundleIdentifier
