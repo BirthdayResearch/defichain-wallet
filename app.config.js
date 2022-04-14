54222e1fc0f48a9b14e04fc0909432d543b0ed9f
thedoublejay <josejaimecareers@gmail.com>
2022-04-14 14:18:29 +0800

    check if empty string

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 // TODO(thedoublejay) - Remove when migration process has been completed
 export default ({ config }) => {
-  const bundleIdentifier = process.env.EXPO_BUNDLE_IDENTIFIER != null ? process.env.EXPO_BUNDLE_IDENTIFIER : config.ios.bundleIdentifier
+  const bundleIdentifier = process.env.EXPO_BUNDLE_IDENTIFIER !== '' ? process.env.EXPO_BUNDLE_IDENTIFIER : config.ios.bundleIdentifier
   console.log(bundleIdentifier)
   config.ios = {
     ...config.ios,
