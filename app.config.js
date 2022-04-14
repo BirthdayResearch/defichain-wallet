8ebba4a844a8f3c776d6edfdb10452d9562732ef
thedoublejay <josejaimecareers@gmail.com>
2022-04-14 15:23:26 +0800

    add env

--- app.config.js
+++ app.config.js
@@ -1,9 +1,8 @@
 // TODO(thedoublejay) - Remove when migration process has been completed
 export default ({ config }) => {
-  const bundleIdentifier = process.env.EXPO_BUNDLE_IDENTIFIER !== '' ? process.env.EXPO_BUNDLE_IDENTIFIER : config.ios.bundleIdentifier
   config.ios = {
     ...config.ios,
-    bundleIdentifier: bundleIdentifier
+    bundleIdentifier: process.env.EXPO_BUNDLE_IDENTIFIER
   }
   return {
     ...config,
