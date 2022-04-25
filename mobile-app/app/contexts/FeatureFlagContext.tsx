985ad9a000fc198cd045cbd266efc6cfde0d669d
pierregee <pierre.gabaldon@gmail.com>
2022-04-25 17:35:34 +0800

    refactor(core): change retry interval

--- mobile-app/app/contexts/FeatureFlagContext.tsx
+++ mobile-app/app/contexts/FeatureFlagContext.tsx
@@ -45,7 +45,7 @@ export function FeatureFlagProvider (props: React.PropsWithChildren<any>): JSX.E
     setTimeout(() => {
       prefetchPage({})
       setRetries(retries + 1)
-    }, 10000)
+    }, 5000)
   } else if (!isError) {
     prefetchPage({})
   }
