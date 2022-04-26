22f789c80b16d3378c5806ea178f3ce20782b6ce
Pierre Gee <pierre.gabaldon@gmail.com>
2022-04-26 15:03:54 +0800

    feature(core): move the fetch tokens down to app nav (#2718)

--- shared/contexts/WalletDataProvider.tsx
+++ shared/contexts/WalletDataProvider.tsx
@@ -2,9 +2,8 @@ import React, { useEffect, PropsWithChildren } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '@store'
 import { useNetworkContext } from '@shared-contexts/NetworkContext'
-import { useWalletContext } from '@shared-contexts/WalletContext'
 import { useWhaleApiClient } from './WhaleContext'
-import { fetchDexPrice, fetchPoolPairs, fetchTokens } from '@store/wallet'
+import { fetchDexPrice, fetchPoolPairs } from '@store/wallet'
 import { fetchUserPreferences } from '@store/userPreferences'
 import { useWalletPersistenceContext } from '@shared-contexts/WalletPersistenceContext'
 import { useFeatureFlagContext } from '@contexts/FeatureFlagContext'
@@ -13,7 +12,6 @@ export function WalletDataProvider (props: PropsWithChildren<any>): JSX.Element 
   const blockCount = useSelector((state: RootState) => state.block.count)
   const client = useWhaleApiClient()
   const { network } = useNetworkContext()
-  const { address } = useWalletContext()
   const dispatch = useDispatch()
   const { wallets } = useWalletPersistenceContext()
   const { isFeatureAvailable } = useFeatureFlagContext()
@@ -23,10 +21,6 @@ export function WalletDataProvider (props: PropsWithChildren<any>): JSX.Element 
     dispatch(fetchPoolPairs({ client }))
     dispatch(fetchDexPrice({ client, denomination: 'USDT' }))
   }, [blockCount, network])
-  
-  useEffect(() => {
-    dispatch(fetchTokens({ client, address }))
-  }, [blockCount, network, address])
 
   // Fetch user data on start up
   // Will only refetch on network or wallet change
