6b3ad37260120fec699dadc41d042b55c9dee787
rox1338 <roland.hackl@gmail.com>
2022-04-28 08:55:08 +0200

    welcome screen - display app version #2703

--- mobile-app/app/screens/AppNavigator/screens/Settings/screens/AboutScreen.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Settings/screens/AboutScreen.tsx
@@ -121,8 +121,12 @@ export function AboutScreen (): JSX.Element {
           {translate('screens/AboutScreen', 'DeFiChain Wallet')}
         </ThemedText>
 
-        <ThemedText style={tailwind('text-base font-light text-black')}>
-          {`v${nativeApplicationVersion ?? '0.0.0'}`}
+        <ThemedText
+          dark={tailwind('text-gray-400 border-gray-400')}
+          light={tailwind('text-gray-500 border-gray-500')}
+          style={tailwind('text-sm font-medium mt-1 border rounded py-0.5 px-1.5')}
+        >
+          {`Version ${nativeApplicationVersion ?? '0.0.0'}`}
         </ThemedText>
 
         {hasBetaFeatures && (
--- mobile-app/app/screens/WalletNavigator/screens/components/OnboardingCarousel.tsx
+++ mobile-app/app/screens/WalletNavigator/screens/components/OnboardingCarousel.tsx
@@ -65,9 +65,9 @@ export function InitialSlide (): JSX.Element {
       </ThemedText>
 
       <ThemedText
-        dark={tailwind('text-gray-400')}
-        light={tailwind('text-gray-500')}
-        style={tailwind('text-sm font-medium mt-1')}
+        dark={tailwind('text-gray-400 border-gray-400')}
+        light={tailwind('text-gray-500 border-gray-500')}
+        style={tailwind('text-sm font-medium mt-1 border rounded py-0.5 px-1.5')}
       >
         {`Version ${nativeApplicationVersion ?? '0.0.0'}`}
       </ThemedText>
