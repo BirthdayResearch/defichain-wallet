21f6242372f40ae3b2f2a9cb537e9d7a58dc5515
Keng Ye <kengyeleow@gmail.com>
2022-05-25 15:58:03 +0800

    feat: update error message when removing col
    Change condition to compare resulting col ratio against min col ratio

--- mobile-app/app/screens/AppNavigator/screens/Loans/components/AddOrRemoveCollateralForm.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Loans/components/AddOrRemoveCollateralForm.tsx
@@ -134,7 +134,7 @@ export const AddOrRemoveCollateralForm = memo(({ route }: Props): JSX.Element =>
     token.id,
     totalAmount
   )
-  const isValidCollateralRatio = requiredTokensShare?.gte(minRequiredTokensShare)
+  const isValidCollateralRatio = resultingColRatio.isGreaterThanOrEqualTo(vault.loanScheme.minColRatio)
   const removeMaxCollateralAmount = !isAdd && new BigNumber(collateralValue).isEqualTo(new BigNumber(available)) && prices.vaultShare.isNaN() && collateralItem !== undefined
   const displayNA = new BigNumber(collateralValue).isZero() || collateralValue === '' || removeMaxCollateralAmount
 
@@ -401,14 +401,14 @@ export const AddOrRemoveCollateralForm = memo(({ route }: Props): JSX.Element =>
         margin='mt-6 mb-2'
         testID='add_collateral_button_submit'
       />
-      {(isFeatureAvailable('dusd_vault_share') && !isAdd && !isValidCollateralRatio && requiredVaultShareTokens.includes(token.symbol)) && hasLoan && (
+      {isFeatureAvailable('dusd_vault_share') && !isAdd && !isValidCollateralRatio && hasLoan && (
         <ThemedText
           dark={tailwind('text-error-500')}
           light={tailwind('text-error-500')}
           style={tailwind('text-sm text-center')}
           testID='vault_min_share_warning'
         >
-          {translate('screens/BorrowLoanTokenScreen', 'Your vault needs at least 50% of DFI and/or DUSD as collateral')}
+          {translate('screens/BorrowLoanTokenScreen', 'Removing collateral will liquidate your vault')}
         </ThemedText>
       )}
       <ThemedText
