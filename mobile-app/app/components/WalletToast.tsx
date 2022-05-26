c3a20b14bd8732c25171930f834c248a1b086e87
Harsh <harshrathi.dev@gmail.com>
2022-05-26 12:15:46 +0530

    added box shadow for toaster

--- mobile-app/app/components/WalletToast.tsx
+++ mobile-app/app/components/WalletToast.tsx
@@ -2,6 +2,7 @@ import { tailwind } from '@tailwind'
 import { translate } from '@translations'
 import { ToastProps } from 'react-native-toast-notifications/lib/typescript/toast'
 import { ThemedText, ThemedView } from './themed'
+import { StyleSheet } from 'react-native'
 
 interface WalletToastProps {
   toast: ToastProps
@@ -10,18 +11,27 @@ interface WalletToastProps {
 export function WalletToast (props: WalletToastProps): JSX.Element {
   return (
     <ThemedView
-      style={tailwind('pb-1.5 pt-1 px-4 mt-8 rounded-lg')}
-      light={tailwind('bg-gray-600')}
-      dark={tailwind('bg-gray-800')}
+      style={tailwind('pb-1.5 pt-1 px-4 mt-16 rounded-3xl')}
+      dark={[styles.darkShadow, tailwind('bg-gray-700')]}
+      light={[styles.lightShadow, tailwind('bg-white')]}
       testID='wallet_toast'
     >
       <ThemedText
         style={tailwind('text-lg')}
-        light={tailwind('text-white')}
-        dark={tailwind('text-white')}
+        dark={tailwind('text-gray-50')}
+        light={tailwind('text-gray-900')}
       >
         {translate('components/WalletToast', props.toast.message as string)}
       </ThemedText>
     </ThemedView>
   )
 }
+
+const styles = StyleSheet.create({
+  darkShadow: {
+    boxShadow: '0px 0px 0px 1px rgba(0, 0, 0, 0.1), 0px 40px 32px -16px rgba(0, 0, 0, 0.05), 0px 6px 4px -4px rgba(0, 0, 0, 0.12), 0px 16px 16px -8px rgba(0, 0, 0, 0.12)'
+  },
+  lightShadow: {
+    boxShadow: '0px 0px 0px 1px rgba(0, 0, 0, 0.05), 0px 40px 35px -16px rgba(0, 0, 0, 0.08), 0px 6px 4px -4px rgba(0, 0, 0, 0.06), 0px 16px 16px -8px rgba(0, 0, 0, 0.12)'
+  }
+})
