d532f53a0b9b03726a04ec3001356c09d83a563c
thedoublejay <josejaimecareers@gmail.com>
2022-05-31 11:01:17 +0800

    chore(api): update polling

--- mobile-app/app/screens/RootNavigator.tsx
+++ mobile-app/app/screens/RootNavigator.tsx
@@ -1,4 +1,3 @@
-
 import { WalletContextProvider } from '@shared-contexts/WalletContext'
 import { WalletNodeProvider } from '@shared-contexts/WalletNodeProvider'
 import { useWalletPersistenceContext } from '@shared-contexts/WalletPersistenceContext'
