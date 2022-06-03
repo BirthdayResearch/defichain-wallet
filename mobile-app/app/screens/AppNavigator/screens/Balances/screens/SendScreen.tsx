e5550be2b3d4f81ad3639401845306427cd23f44
Keng Ye <kengyeleow@gmail.com>
2022-06-03 18:51:02 +0800

    revert: valid address checking when find matched address

--- mobile-app/app/screens/AppNavigator/screens/Balances/screens/SendScreen.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Balances/screens/SendScreen.tsx
@@ -1,6 +1,6 @@
 import { InputHelperText } from '@components/InputHelperText'
 import { WalletTextInput } from '@components/WalletTextInput'
-import { DeFiAddress, fromAddress } from '@defichain/jellyfish-address'
+import { DeFiAddress } from '@defichain/jellyfish-address'
 import { NetworkName } from '@defichain/jellyfish-network'
 import { StackScreenProps } from '@react-navigation/stack'
 import { DFITokenSelector, DFIUtxoSelector, tokensSelector, WalletToken } from '@store/wallet'
@@ -148,11 +148,6 @@ export function SendScreen ({
   }, [JSON.stringify(tokens)])
 
   useEffect(() => {
-    const decodedAddress = fromAddress(address, networkName)
-    if (decodedAddress === undefined) {
-      setMatchedAddress(undefined)
-      return
-    }
     debounceMatchAddress()
   }, [address, addressBook])
 
