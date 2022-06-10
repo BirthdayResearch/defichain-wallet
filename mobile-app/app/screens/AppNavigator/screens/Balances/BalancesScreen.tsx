720b3e8660aef10fb7c3ec8819b8887d43583534
Chloe <44501120+chloezxyy@users.noreply.github.com>
2022-06-10 10:07:27 +0800

    feature(ui-ux): reduce sorting modal popup length (#2964)
    * feature(ui-ux): reduce sorting modal popup length
    
    * reduce modal sheet size to 55%

--- mobile-app/app/screens/AppNavigator/screens/Balances/BalancesScreen.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Balances/BalancesScreen.tsx
@@ -395,6 +395,7 @@ export function BalancesScreen ({ navigation }: Props): JSX.Element {
   const containerRef = useRef(null)
   const [isModalDisplayed, setIsModalDisplayed] = useState(false)
   const modalSnapPoints = { ios: ['75%'], android: ['75%'] }
+  const modalSortingSnapPoints = { ios: ['55%'], android: ['55%'] }
   const expandModal = useCallback((isSortBottomSheet: boolean) => {
     if (Platform.OS === 'web') {
       setIsModalDisplayed(true)
@@ -530,7 +531,7 @@ export function BalancesScreen ({ navigation }: Props): JSX.Element {
               <BottomSheetWithNav
                 modalRef={bottomSheetSortRef}
                 screenList={assetSortBottomSheetScreen}
-                snapPoints={modalSnapPoints}
+                snapPoints={modalSortingSnapPoints}
               />
               <BottomSheetWithNav
                 modalRef={bottomSheetRef}
