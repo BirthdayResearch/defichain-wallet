7cf817065df401790e122111e21b056bf9208acc
Chloezxyy <chloe@cakedefi.com>
2022-06-13 17:23:28 +0800

    fix indentation spacing

--- mobile-app/app/components/NumberRow.tsx
+++ mobile-app/app/components/NumberRow.tsx
@@ -76,18 +76,18 @@ export function NumberRow (props: INumberRowProps): JSX.Element {
                   </ThemedText>
                   {
                     props.rhs.suffixType === 'text' &&
-                    <>
-                      <Text>{' '}</Text>
-                      <ThemedText
-                        light={tailwind('text-gray-500')}
-                        dark={tailwind('text-gray-400')}
-                        style={[tailwind('text-sm ml-1'), props.textStyle, props.rhs.style]}
-                        testID={`${props.rhs.testID}_suffix`}
-                        {...props.rhsThemedProps}
-                      >
-                        {props.rhs.suffix}
-                      </ThemedText>
-                    </>
+                      <>
+                        <Text>{' '}</Text>
+                        <ThemedText
+                          light={tailwind('text-gray-500')}
+                          dark={tailwind('text-gray-400')}
+                          style={[tailwind('text-sm ml-1'), props.textStyle, props.rhs.style]}
+                          testID={`${props.rhs.testID}_suffix`}
+                          {...props.rhsThemedProps}
+                        >
+                          {props.rhs.suffix}
+                        </ThemedText>
+                      </>
                   }
                 </Text>
               )}
