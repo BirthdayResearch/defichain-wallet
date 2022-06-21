ee300e5140b76054ad8e5f77979fb1299c0e76f4
pierregee <pierre.gabaldon@gmail.com>
2022-06-21 17:20:33 +0800

    fix(core): stop reinitializing on url change

--- mobile-app/app/contexts/StoreProvider.tsx
+++ mobile-app/app/contexts/StoreProvider.tsx
@@ -2,18 +2,16 @@ import { PropsWithChildren, useMemo } from 'react'
 import { Provider } from 'react-redux'
 import { initializeStore } from '@store'
 import { useWalletPersistenceContext } from '../../../shared/contexts/WalletPersistenceContext'
-import { useServiceProviderContext } from './StoreServiceProvider'
 
 /**
  * Store that is memoized to network & wallets setting.
  */
 export function StoreProvider (props: PropsWithChildren<any>): JSX.Element {
   const { wallets } = useWalletPersistenceContext()
-  const { url } = useServiceProviderContext()
 
   const store = useMemo(() => {
     return initializeStore()
-  }, [wallets, url])
+  }, [wallets])
 
   return (
     <Provider store={store}>
