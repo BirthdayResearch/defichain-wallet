21e76a87f4b60ae7092087a8f7f3a29da2fa69ec
Chloezxyy <chloe@cakedefi.com>
2022-07-04 14:25:56 +0800

    add DFIP announcement

--- website/src/pages/api/v0/announcements.ts
+++ website/src/pages/api/v0/announcements.ts
@@ -9,5 +9,20 @@ export const cors = Cors({
 
 export default async function handle (req: NextApiRequest, res: NextApiResponse<AnnouncementData[]>): Promise<void> {
   await runMiddleware(req, res, cors)
-  res.json([])
+  res.json([
+    {
+      lang: {
+        en: 'There is currently a high Dex-Stabilization fee imposed on DUSD-DFI swaps due to DFIP 2206-D.',
+        de: 'Derzeit wird eine hohe Dex-Stabilisierungsgebühr bei Tausch von DUSD-DFI aufgrund von DFIP 2206-D erhoben.',
+        'zh-Hans': '由于社区提案 DFIP 2206-D，目前对 DUSD - DFI 兑换征收高额的 Dex 稳定费。',
+        'zh-Hant': '由於社區提案 DFIP 2206-D，目前對 DUSD - DFI 兌換徵收高額的 Dex 穩定費。',
+        fr: 'Il existe actuellement une taxe de stabilisation du DEX élevée imposée sur les échanges DUSD-DFI en raison du DFIP 2206-D.',
+        es: '',
+        it: ''
+      },
+      version: '>=1.0.0',
+      type: 'OTHER_ANNOUNCEMENT',
+      id: '10'
+    }
+  ])
 }
