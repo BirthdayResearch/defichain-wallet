125f18dcdbec3d4b07ce8ba4b0e9bdf330789953
Keng Ye <kengyeleow@gmail.com>
2022-07-13 16:58:00 +0800

    fix: missing redux initial state in render test

--- mobile-app/app/screens/AppNavigator/screens/Settings/screens/NetworkSelectionScreenV2.test.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Settings/screens/NetworkSelectionScreenV2.test.tsx
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { RootState } from '@store'
 import { block } from '@store/block'
+import { ocean } from '@store/ocean'
+import { transactionQueue } from '@store/transaction_queue'
 import { render } from '@testing-library/react-native'
 import { Provider } from 'react-redux'
 import { NetworkSelectionScreenV2 } from './NetworkSelectionScreenV2'
@@ -25,12 +27,24 @@ describe('onboarding network selection screen', () => {
         lastSuccessfulSync: 'Tue, 14 Sep 2021 15:37:10 GMT',
         connected: true,
         isPolling: true
+      },
+      transactionQueue: {
+        transactions: []
+      },
+      ocean: {
+        transactions: [],
+        height: 0,
+        err: undefined
       }
     }
 
     const store = configureStore({
       preloadedState: initialState,
-      reducer: { block: block.reducer }
+      reducer: {
+        block: block.reducer,
+        transactionQueue: transactionQueue.reducer,
+        ocean: ocean.reducer
+      }
     })
 
     const rendered = render(
