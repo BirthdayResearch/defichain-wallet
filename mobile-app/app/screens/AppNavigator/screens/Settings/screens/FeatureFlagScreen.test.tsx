990a22fb3fc519cd0bf8a75266269226b26f69f0
JJ Adonis <josejaimecareers@gmail.com>
2022-07-14 09:42:24 +0800

    feature(ui-ux): display DFIP fees (#3151)
    * feature(ui-ux): display DFIP fees
    
    * chore(ui-ux): cleanup flags (#3149)
    
    * feature(ui-ux): display DFIP fees

--- mobile-app/app/screens/AppNavigator/screens/Settings/screens/FeatureFlagScreen.test.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Settings/screens/FeatureFlagScreen.test.tsx
@@ -8,14 +8,14 @@ jest.mock('@contexts/FeatureFlagContext')
 describe('feature flag screen', () => {
   it('should render FeatureFlagItem', async () => {
     const feature: BetaFeaturesI = {
-      id: 'loan',
-      name: 'Decentralized Loans',
+      id: 'future_swap',
+      name: 'Future swap',
       stage: 'beta',
       version: '>=0.12.0',
       description: 'Browse loan tokens provided by DeFiChain',
-      value: true,
       networks: [EnvironmentNetwork.LocalPlayground, EnvironmentNetwork.RemotePlayground],
-      platforms: ['ios', 'android', 'web']
+      platforms: ['ios', 'android', 'web'],
+      value: true
     }
     const rendered = render(<FeatureFlagItem item={feature} onChange={() => {}} />)
     expect(rendered.toJSON()).toMatchSnapshot()
