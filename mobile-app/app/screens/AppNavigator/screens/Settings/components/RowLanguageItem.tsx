415b43bbeabfbb11b885911e0cecb5e4904f75eb
G9000 <leo.caesar@live.com>
2022-07-15 15:35:33 +0800

    remove border from ThemedTouchableOpacityV2 on lang screen

--- mobile-app/app/screens/AppNavigator/screens/Settings/components/RowLanguageItem.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Settings/components/RowLanguageItem.tsx
@@ -1,4 +1,4 @@
-import { ThemedIcon, ThemedTextV2, ThemedTouchableOpacityV2 } from '@components/themed'
+import { ThemedIcon, ThemedTextV2, ThemedTouchableOpacityV2, ThemedViewV2 } from '@components/themed'
 import { WalletAlert } from '@components/WalletAlert'
 import { useLanguageContext } from '@shared-contexts/LanguageProvider'
 import { NavigationProp, useNavigation } from '@react-navigation/native'
@@ -7,7 +7,7 @@ import { AppLanguageItem, translate } from '@translations'
 import { View, Text } from 'react-native'
 import { SettingsParamList } from '../SettingsNavigator'
 
-export function RowLanguageItem ({ languageItem, border }: { languageItem: AppLanguageItem, border: boolean}): JSX.Element {
+export function RowLanguageItem ({ languageItem, isLast }: { languageItem: AppLanguageItem, isLast: boolean}): JSX.Element {
   const navigation = useNavigation<NavigationProp<SettingsParamList>>()
   const {
     language,
@@ -40,35 +40,41 @@ export function RowLanguageItem ({ languageItem, border }: { languageItem: AppLa
     })
   }
   return (
-    <ThemedTouchableOpacityV2
-      onPress={onPress}
-      style={tailwind('flex flex-row items-center justify-between py-4.5 mx-5', { 'border-b-0.5': border })}
-      testID={`button_language_${languageItem.language}`}
+    <ThemedViewV2
+      style={tailwind('border-b-0.5', { 'border-0': isLast })}
+      light={tailwind('border-mono-light-v2-300')}
+      dark={tailwind('border-mono-dark-v2-300')}
     >
-      <View style={tailwind('flex flex-row items-center')}>
-        <ThemedTextV2 testID='language_option' style={tailwind('font-normal-v2 text-sm pr-1')}>
-          {languageItem.displayName}
-        </ThemedTextV2>
-        {!language.startsWith(languageItem.locale) &&
-          <ThemedTextV2
-            testID='language_option_description'
-            dark={tailwind('text-mono-dark-v2-900')}
-            light={tailwind('text-mono-light-v2-900')}
-            style={tailwind('font-normal-v2 text-sm')}
-          >
-            <Text>
-              ({translate('screens/Settings', languageItem.language)})
-            </Text>
-          </ThemedTextV2>}
-      </View>
-      <ThemedIcon
-        iconType='MaterialIcons'
-        light={tailwind(`${language.startsWith(languageItem.locale) ? 'text-green-v2' : 'text-mono-light-v2-700 opacity-30'}`)}
-        dark={tailwind(`${language.startsWith(languageItem.locale) ? 'text-green-v2' : 'text-mono-dark-v2-700 opacity-30'}`)}
-        name='check-circle'
-        size={20}
-        testID={`button_network_${languageItem.language}_check`}
-      />
-    </ThemedTouchableOpacityV2>
+      <ThemedTouchableOpacityV2
+        onPress={onPress}
+        style={tailwind('flex flex-row items-center justify-between py-4.5 border-0')}
+        testID={`button_language_${languageItem.language}`}
+      >
+        <View style={tailwind('flex flex-row items-center')}>
+          <ThemedTextV2 testID='language_option' style={tailwind('font-normal-v2 text-sm pr-1')}>
+            {languageItem.displayName}
+          </ThemedTextV2>
+          {!language.startsWith(languageItem.locale) &&
+            <ThemedTextV2
+              testID='language_option_description'
+              dark={tailwind('text-mono-dark-v2-900')}
+              light={tailwind('text-mono-light-v2-900')}
+              style={tailwind('font-normal-v2 text-sm')}
+            >
+              <Text>
+                ({translate('screens/Settings', languageItem.language)})
+              </Text>
+            </ThemedTextV2>}
+        </View>
+        <ThemedIcon
+          iconType='MaterialIcons'
+          light={tailwind(`${language.startsWith(languageItem.locale) ? 'text-green-v2' : 'text-mono-light-v2-700 opacity-30'}`)}
+          dark={tailwind(`${language.startsWith(languageItem.locale) ? 'text-green-v2' : 'text-mono-dark-v2-700 opacity-30'}`)}
+          name='check-circle'
+          size={20}
+          testID={`button_network_${languageItem.language}_check`}
+        />
+      </ThemedTouchableOpacityV2>
+    </ThemedViewV2>
   )
 }
