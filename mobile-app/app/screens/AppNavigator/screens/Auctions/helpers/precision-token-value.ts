40fc1c9e4e6bf82c03e7a29565016dbb29d5a264
Keng Ye <kengyeleow@gmail.com>
2022-07-20 15:24:21 +0800

    chore: add another helper function getPrecisedCurrencyValue
    To get 2 dp when value is 0

--- mobile-app/app/screens/AppNavigator/screens/Auctions/helpers/precision-token-value.ts
+++ mobile-app/app/screens/AppNavigator/screens/Auctions/helpers/precision-token-value.ts
@@ -1,6 +1,15 @@
 import BigNumber from 'bignumber.js'
 
+// display 0.00000000 when value is 0
 export function getPrecisedTokenValue (value: string | number | BigNumber): string {
   const usdValue = new BigNumber(value)
   return usdValue.isLessThanOrEqualTo(0.1) ? usdValue.toFixed(8) : usdValue.toFixed(2)
+}
+
+// display 0.00 when value is 0
+export function getPrecisedCurrencyValue (value: string | number | BigNumber): string {
+  const currencyValue = new BigNumber(value)
+  return currencyValue.isLessThanOrEqualTo(0.1) && currencyValue.isGreaterThan(0)
+    ? currencyValue.toFixed(8)
+: currencyValue.toFixed(2)
 }
