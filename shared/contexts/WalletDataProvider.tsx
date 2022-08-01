416f848acd650e8a16cf01952f476c56bff85011
thedoublejay <josejaimecareers@gmail.com>
2022-08-01 15:29:03 +0800

    feature(ui-ux): add huobi

--- shared/contexts/WalletDataProvider.tsx
+++ shared/contexts/WalletDataProvider.tsx
@@ -9,6 +9,7 @@ import { useWalletPersistenceContext } from '@shared-contexts/WalletPersistenceC
 import { useWalletContext } from '@shared-contexts/WalletContext'
 import { fetchVaults } from '@store/loans'
 import { useAppDispatch } from '@hooks/useAppDispatch'
+import { useFeatureFlagContext } from '@contexts/FeatureFlagContext'
 
 export function WalletDataProvider (props: PropsWithChildren<any>): JSX.Element | null {
   const blockCount = useSelector((state: RootState) => state.block.count)
@@ -17,6 +18,7 @@ export function WalletDataProvider (props: PropsWithChildren<any>): JSX.Element 
   const dispatch = useAppDispatch()
   const { address } = useWalletContext()
   const { wallets } = useWalletPersistenceContext()
+  const { isFeatureAvailable } = useFeatureFlagContext()
 
   // Global polling based on blockCount and network, so no need to fetch per page
   useEffect(() => {
@@ -32,7 +34,9 @@ export function WalletDataProvider (props: PropsWithChildren<any>): JSX.Element 
   // Fetch user data on start up
   // Will only refetch on network or wallet change
   useEffect(() => {
-    dispatch(fetchUserPreferences(network))
+    if (isFeatureAvailable('local_storage')) {
+      dispatch(fetchUserPreferences(network))
+    }
   }, [network, wallets])
 
   /* Global polling based on blockCount, network and address */
