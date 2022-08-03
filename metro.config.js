7010b24596a29463cc1c202e0886ea029978605d
ThaBrad <18183189+ThaBrad@users.noreply.github.com>
2022-08-03 16:42:24 +0200

    eas

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,5 @@
-import { getDefaultConfig } from '@expo/metro-config'
-// const { getDefaultConfig } = require('expo/metro-config')
+// import { getDefaultConfig } from '@expo/metro-config'
+const { getDefaultConfig } = require('@expo/metro-config')
 const defaultConfig = getDefaultConfig(__dirname)
 
 defaultConfig.resolver.extraNodeModules.stream = require.resolve('stream-browserify')
