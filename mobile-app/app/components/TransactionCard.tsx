28b7c73ef9b63e3008f1cf3405a451d804abba11
G9000 <leo.caesar@live.com>
2022-08-11 00:37:50 +0800

    fix merge issue

--- mobile-app/app/components/TransactionCard.tsx
+++ mobile-app/app/components/TransactionCard.tsx
@@ -8,7 +8,7 @@ interface TransactionCardProps {
   maxValue: BigNumber
   onChange: (amount: string) => void
   onPercentageChange: (amount: string, type: AmountButtonTypes) => void
-  status?: string
+  status?: TransactionCardStatus
   containerStyle?: StyleProp<ViewStyle>
 }
 
@@ -17,6 +17,12 @@ export enum AmountButtonTypes {
   Half = '50%',
   SeventyFive = '75%',
   Max = 'MAX'
+}
+
+export enum TransactionCardStatus {
+  Default,
+  Active,
+  Error
 }
 
 export function TransactionCard ({
@@ -30,13 +36,13 @@ export function TransactionCard ({
   return (
     <ThemedViewV2
       light={tailwind('bg-mono-light-v2-00', {
-        'border-0.5 border-mono-light-v2-800': status === 'active'
+        'border-0.5 border-mono-light-v2-800': status === TransactionCardStatus.Active
       })}
       dark={tailwind('bg-mono-dark-v2-00', {
-        'border-0.5 border-mono-dark-v2-800': status === 'active'
+        'border-0.5 border-mono-dark-v2-800': status === TransactionCardStatus.Active
       })}
       style={tailwind('rounded-lg-v2', {
-        'border-0.5 border-red-v2': status === 'error'
+        'border-0.5 border-red-v2': status === TransactionCardStatus.Error
       })}
     >
       <ThemedViewV2
