c1f67719d4fa9085f7550456e7e13005fe48bd88
Suraj Auwal <siradjiawoual@gmail.com>
2022-08-11 13:05:06 +0100

    updated test file

--- mobile-app/app/screens/AppNavigator/screens/Dex/components/FavoriteButton.test.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Dex/components/FavoriteButton.test.tsx
@@ -2,9 +2,6 @@ import { render } from '@testing-library/react-native'
 import { FavoriteButton } from './FavoriteButton'
 
 jest.mock('@shared-contexts/ThemeProvider')
-jest.mock('@react-navigation/native', () => ({
-  useNavigation: jest.fn()
-}))
 
 describe('Favorite Button', () => {
   it('should match snapshot', () => {
