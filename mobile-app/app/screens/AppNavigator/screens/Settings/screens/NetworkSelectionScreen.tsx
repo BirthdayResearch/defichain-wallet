32e962428c3c0973cbba78516c295a7eff390cf8
chloe <chloezhengxy@gmail.com>
2022-08-16 23:18:39 +0800

    feature(ui-ux): network selection ui fix

--- mobile-app/app/screens/AppNavigator/screens/Settings/screens/NetworkSelectionScreen.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Settings/screens/NetworkSelectionScreen.tsx
@@ -81,7 +81,9 @@ export function NetworkSelectionScreen (): JSX.Element {
           }}
           rhs={{
             value: masternodeCount ?? '',
-            testID: 'network_details_total_masternodes'
+            testID: 'network_details_total_masternodes',
+            lightTextStyle: tailwind('text-mono-light-v2-700'),
+            darkTextStyle: tailwind('text-mono-dark-v2-700')
           }}
           containerStyle={{
             style: tailwind('pt-4.5 flex-row items-start w-full bg-transparent'),
