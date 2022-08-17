28171c357906f6631abb1dfec724818dcdc9b62e
ThaBrad <18183189+ThaBrad@users.noreply.github.com>
2022-08-17 16:45:47 +0200

    fix typo

--- mobile-app/app/components/DfxDexFeeInfo.tsx
+++ mobile-app/app/components/DfxDexFeeInfo.tsx
@@ -10,7 +10,7 @@ export function DfxDexFeeInfo (props: DfxDexFeeInfoProps): JSX.Element {
   return (
     <InfoText
       testID='dfx_kyc_info'
-      text={translate('components/DfxDexFeeInfo', 'Please note that all sales of dTokens(e.g.dUSD, dCOIN, dTSLA, dSPY..) are subject to a DEX stabilization fee.The crypto tokens such as DFI, dBTC, dETH and stablecoins such as dUSDT / dUSDC are not affected.Further information')}
+      text={translate('components/DfxDexFeeInfo', 'Please note that all sales of dTokens(e.g.dUSD, dCOIN, dTSLA, dSPY..) are subject to a DEX stabilization fee.The crypto tokens such as DFI, dBTC, dETH and stablecoins such as dUSDT / dUSDC are not affected. Further information')}
       style={props.style}
     />
   )
