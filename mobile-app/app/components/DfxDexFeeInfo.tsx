b62281fd80ceaeb38e15bf0af229afa5f5bd4e01
ThaBrad <18183189+ThaBrad@users.noreply.github.com>
2022-08-17 16:38:41 +0200

    cleanup

--- mobile-app/app/components/DfxDexFeeInfo.tsx
+++ mobile-app/app/components/DfxDexFeeInfo.tsx
@@ -1,8 +1,5 @@
-import { useDFXAPIContext } from '@shared-contexts/DFXAPIContextProvider'
 import { translate } from '@translations'
-
 import { StyleProp, ViewStyle } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
 import { InfoText } from './InfoText'
 
 interface DfxDexFeeInfoProps {
@@ -10,16 +7,11 @@ interface DfxDexFeeInfoProps {
 }
 
 export function DfxDexFeeInfo (props: DfxDexFeeInfoProps): JSX.Element {
-const { openDfxServices } = useDFXAPIContext()
   return (
-    <TouchableOpacity
-      onPress={async () => await openDfxServices()}
-    >
-      <InfoText
-        testID='dfx_kyc_info'
-        text={translate('components/DfxDexFeeInfo', 'Please note that all sales of dTokens(e.g.dUSD, dCOIN, dTSLA, dSPY..) are subject to a DEX stabilization fee.The crypto tokens such as DFI, dBTC, dETH and stablecoins such as dUSDT / dUSDC are not affected.Further information')}
-        style={props.style}
-      />
-    </TouchableOpacity>
+    <InfoText
+      testID='dfx_kyc_info'
+      text={translate('components/DfxDexFeeInfo', 'Please note that all sales of dTokens(e.g.dUSD, dCOIN, dTSLA, dSPY..) are subject to a DEX stabilization fee.The crypto tokens such as DFI, dBTC, dETH and stablecoins such as dUSDT / dUSDC are not affected.Further information')}
+      style={props.style}
+    />
   )
 }
