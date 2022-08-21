7cc8eff8b1fcc4b34e5c428225d1e85d0bfe7c62
Suraj Auwal <siradjiawoual@gmail.com>
2022-08-21 23:50:13 +0100

    Updated Code

--- mobile-app/app/screens/AppNavigator/screens/Dex/components/DexActionButton.test.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Dex/components/DexActionButton.test.tsx
@@ -8,48 +8,9 @@ jest.mock('@react-navigation/native', () => ({
 
 describe('Dex Action Button', () => {
   it('should match snapshot', () => {
-    const pair = {
-      id: '15',
-      symbol: 'BTC-DFI',
-      displaySymbol: 'dBTC-DFI',
-      name: 'Playground BTC-Default Defi token',
-      status: true,
-      tokenA: {
-        symbol: 'BTC',
-        displaySymbol: 'dBTC',
-        id: '1',
-        reserve: '1000',
-        blockCommission: '0'
-      },
-      tokenB: {
-        symbol: 'DFI',
-        displaySymbol: 'DFI',
-        id: '0',
-        reserve: '1000',
-        blockCommission: '0'
-      },
-      priceRatio: {
-        ab: '1',
-        ba: '1'
-      },
-      commission: '0',
-      totalLiquidity: {
-        token: '1000',
-        usd: '20000000'
-      },
-      tradeEnabled: true,
-      ownerAddress: 'mswsMVsyGMj1FzDMbbxw2QW3KvQAv2FKiy',
-      rewardPct: '0.1',
-      rewardLoanPct: '0.1',
-      creation: {
-        tx: '79b5f7853f55f762c7550dd7c734dff0a473898bfb5639658875833accc6d461',
-        height: 132
-      }
-    }
-
     const onPress = jest.fn()
 
-    const rendered = render(<DexActionButton testID='Composite_Swap_2' onPress={() => onPress()} label='Swap' pair={pair} />)
+    const rendered = render(<DexActionButton testID='Composite_Swap_2' onPress={() => onPress()} label='Swap' />)
     expect(rendered.toJSON()).toMatchSnapshot()
   })
 })
