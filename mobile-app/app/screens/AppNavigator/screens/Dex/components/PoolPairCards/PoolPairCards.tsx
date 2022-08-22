bee02f0867c6b5bbfdbe0965f8e446d7c7319fd0
Harsh <harshrathi.dev@gmail.com>
2022-08-22 23:15:05 +0530

    fix e2e

--- mobile-app/app/screens/AppNavigator/screens/Dex/components/PoolPairCards/PoolPairCards.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Dex/components/PoolPairCards/PoolPairCards.tsx
@@ -99,28 +99,6 @@ export function PoolPairCards ({
     availablePairs.findIndex(x => x.data.id === pairB.data.id)
   )
 
-  const buttonGroup = [
-    {
-      id: ButtonGroupTabKey.AllPairs,
-      label: translate('screens/DexScreen', 'All pairs'),
-      handleOnPress: () => onButtonGroupChange(ButtonGroupTabKey.AllPairs)
-    },
-    {
-      id: ButtonGroupTabKey.DFIPairs,
-      label: translate('screens/DexScreen', 'DFI pairs'),
-      handleOnPress: () => onButtonGroupChange(ButtonGroupTabKey.DFIPairs)
-    },
-    {
-      id: ButtonGroupTabKey.DUSDPairs,
-      label: translate('screens/DexScreen', 'DUSD pairs'),
-      handleOnPress: () => onButtonGroupChange(ButtonGroupTabKey.DUSDPairs)
-    },
-    {
-      id: ButtonGroupTabKey.FavouritePairs,
-      label: translate('screens/DexScreen', 'Favourites'),
-      handleOnPress: () => onButtonGroupChange(ButtonGroupTabKey.FavouritePairs)
-    }
-  ]
   useEffect(() => {
     setIsSearching(false)
     if (showSearchInput === false) {
@@ -167,7 +145,7 @@ export function PoolPairCards ({
     <ThemedFlatListV2
       light={tailwind('bg-mono-light-v2-100')}
       dark={tailwind('bg-mono-dark-v2-100')}
-      contentContainerStyle={tailwind('pb-4 pb-2')}
+      contentContainerStyle={tailwind('pb-4 pb-2', { 'pt-8': type === 'your' })}
       ref={ref}
       data={type === 'your' ? filteredYourPairs : sortedPairs}
       numColumns={1}
@@ -177,58 +155,92 @@ export function PoolPairCards ({
       testID={
         type === 'your' ? 'your_liquidity_tab' : 'available_liquidity_tab'
       }
+      renderItem={renderItem}
       ListHeaderComponent={
         <>
-          {showSearchInput === false && (
-            <View style={tailwind('my-4')}>
-              <ThemedViewV2 testID='dex_button_group'>
-                <ScrollView
-                  horizontal
-                  showsHorizontalScrollIndicator={false}
-                  contentContainerStyle={tailwind('flex justify-between items-center flex-row px-5')}
-                >
-                  <ThemedTouchableOpacityV2
-                    onPress={onSearchBtnPress}
-                    style={tailwind('text-center pr-4')}
-                    testID='dex_search_icon'
-                  >
-                    <ThemedIcon
-                      iconType='Feather'
-                      name='search'
-                      size={24}
-                      light={tailwind('text-mono-light-v2-700')}
-                      dark={tailwind('text-mono-dark-v2-700')}
-                    />
-                  </ThemedTouchableOpacityV2>
-                  {buttonGroup.map((button, index) => (
-                    <AssetsFilterItem
-                      key={button.id}
-                      label={button.label}
-                      onPress={button.handleOnPress}
-                      isActive={activeButtonGroup === button.id}
-                      testID={`dex_button_group_${button.id}`}
-                      additionalStyles={!(buttonGroup.length === index) ? tailwind('mr-3') : undefined}
-                    />
-                  )
-                )}
-                </ScrollView>
-              </ThemedViewV2>
-            </View>
-          )}
-          {type === 'available' &&
-            showSearchInput === false && (
+          {(type === 'available' && showSearchInput === false)
+            ? (
               <>
+                <DexBtnGroup
+                  onSearchBtnPress={onSearchBtnPress}
+                  onButtonGroupChange={onButtonGroupChange}
+                  activeButtonGroup={activeButtonGroup}
+                />
                 <TotalValueLocked tvl={tvl ?? 0} />
                 <TopLiquiditySection onPress={onSwap} pairs={topLiquidityPairs} />
                 <NewPoolsSection onPress={onAdd} pairs={newPoolsPairs} />
-              </>
-          )}
+              </>)
+            : <></>}
         </>
-      }
-      renderItem={renderItem}
+        }
     />
   )
 }
+
+const DexBtnGroup = React.memo((props: {
+  onSearchBtnPress: () => void
+  onButtonGroupChange: (buttonGroupTabKey: ButtonGroupTabKey) => void
+  activeButtonGroup: ButtonGroupTabKey
+}) => {
+  const buttonGroup = [
+    {
+      id: ButtonGroupTabKey.AllPairs,
+      label: translate('screens/DexScreen', 'All pairs'),
+      handleOnPress: () => props.onButtonGroupChange(ButtonGroupTabKey.AllPairs)
+    },
+    {
+      id: ButtonGroupTabKey.DFIPairs,
+      label: translate('screens/DexScreen', 'DFI pairs'),
+      handleOnPress: () => props.onButtonGroupChange(ButtonGroupTabKey.DFIPairs)
+    },
+    {
+      id: ButtonGroupTabKey.DUSDPairs,
+      label: translate('screens/DexScreen', 'DUSD pairs'),
+      handleOnPress: () => props.onButtonGroupChange(ButtonGroupTabKey.DUSDPairs)
+    },
+    {
+      id: ButtonGroupTabKey.FavouritePairs,
+      label: translate('screens/DexScreen', 'Favourites'),
+      handleOnPress: () => props.onButtonGroupChange(ButtonGroupTabKey.FavouritePairs)
+    }
+  ]
+  return (
+    <View style={tailwind('my-4')}>
+      <ThemedViewV2 testID='dex_button_group'>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={tailwind('flex justify-between items-center flex-row px-5')}
+        >
+          <ThemedTouchableOpacityV2
+            onPress={props.onSearchBtnPress}
+            style={tailwind('text-center pr-4')}
+            testID='dex_search_icon'
+          >
+            <ThemedIcon
+              iconType='Feather'
+              name='search'
+              size={24}
+              light={tailwind('text-mono-light-v2-700')}
+              dark={tailwind('text-mono-dark-v2-700')}
+            />
+          </ThemedTouchableOpacityV2>
+          {buttonGroup.map((button, index) => (
+            <AssetsFilterItem
+              key={button.id}
+              label={button.label}
+              onPress={button.handleOnPress}
+              isActive={props.activeButtonGroup === button.id}
+              testID={`dex_button_group_${button.id}`}
+              additionalStyles={!(buttonGroup.length === index) ? tailwind('mr-3') : undefined}
+            />
+          )
+        )}
+        </ScrollView>
+      </ThemedViewV2>
+    </View>
+  )
+})
 
 interface PoolCardProps {
   item: DexItem<WalletToken | PoolPairData>
