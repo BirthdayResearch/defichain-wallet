1414254dd927f2e7e1f4125ea60231ad1c195645
G9000 <leo.caesar@live.com>
2022-08-23 15:52:15 +0800

    fix(ui-ux): disable onPress on playground status info

--- mobile-app/app/screens/PlaygroundNavigator/components/PlaygroundAction.tsx
+++ mobile-app/app/screens/PlaygroundNavigator/components/PlaygroundAction.tsx
@@ -8,11 +8,12 @@ interface PlaygroundActionProps {
   testID?: string
   textStyle?: StyleProp<TextStyle>
   onPress?: () => void
+  disabled?: boolean
 }
 
-export function PlaygroundAction ({ title, isLast, testID, rhsChildren, textStyle, onPress }: PlaygroundActionProps): JSX.Element {
+export function PlaygroundAction ({ title, isLast, testID, rhsChildren, textStyle, onPress, disabled }: PlaygroundActionProps): JSX.Element {
   return (
-    <ThemedTouchableListItem isLast={isLast} testId={testID} onPress={onPress}>
+    <ThemedTouchableListItem isLast={isLast} testId={testID} onPress={onPress} disabled={disabled}>
       <ThemedTextV2 style={[tailwind('text-sm font-normal-v2'), textStyle]}>
         {title}
       </ThemedTextV2>
