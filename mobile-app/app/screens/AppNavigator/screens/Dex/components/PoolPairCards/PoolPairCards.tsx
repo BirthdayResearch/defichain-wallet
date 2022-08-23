6d58fa4a3f3f36a08ad59d5c4a2b3598a3d19a1e
Harsh <harshrathi.dev@gmail.com>
2022-08-23 17:47:08 +0530

    fix(e2e): fixed e2e test cases

--- mobile-app/app/screens/AppNavigator/screens/Dex/components/PoolPairCards/PoolPairCards.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Dex/components/PoolPairCards/PoolPairCards.tsx
@@ -153,7 +153,7 @@ export function PoolPairCards ({
       renderItem={renderItem}
       ListEmptyComponent={
         <>
-          {activeButtonGroup === ButtonGroupTabKey.FavouritePairs && (
+          {showSearchInput === false && activeButtonGroup === ButtonGroupTabKey.FavouritePairs && (
             <EmptyTokensScreen
               icon={EmptyCryptoIcon}
               containerStyle={tailwind('pt-14')}
