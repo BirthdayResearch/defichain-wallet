97af2aee1117ad41574194a0408443a73749a945
Joshua <josh_817@hotmail.com>
2022-08-29 15:19:59 +0800

    fix: pr review comment - disable continue button, adjust bottom sheet

--- mobile-app/app/screens/AppNavigator/screens/Portfolio/screens/WithdrawFutureSwapScreenV2.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Portfolio/screens/WithdrawFutureSwapScreenV2.tsx
@@ -3,7 +3,6 @@ import BigNumber from 'bignumber.js'
 import { StackScreenProps } from '@react-navigation/stack'
 import { tailwind } from '@tailwind'
 import { translate } from '@translations'
-import { View } from '@components'
 import { getNativeIcon } from '@components/icons/assets'
 import {
   ThemedIcon,
@@ -33,7 +32,7 @@ import { ButtonV2 } from '@components/ButtonV2'
 import { useBottomSheet } from '@hooks/useBottomSheet'
 import { useThemeContext } from '@shared-contexts/ThemeProvider'
 import { ViewSwapInfo } from '@screens/AppNavigator/screens/Portfolio/screens/ViewSwapInfo'
-import { Platform } from 'react-native'
+import { Platform, View } from 'react-native'
 import { BottomSheetWebWithNavV2, BottomSheetWithNavV2 } from '@components/BottomSheetWithNavV2'
 
 type Props = StackScreenProps<PortfolioParamList, 'WithdrawFutureSwapScreen'>
@@ -67,8 +66,8 @@ export function WithdrawFutureSwapScreenV2 (props: Props): JSX.Element {
   const [isInputFocus, setIsInputFocus] = useState<boolean>(false)
   const [hasError, setHasError] = useState<boolean>(false)
   const modalSortingSnapPoints = {
-    ios: ['50%'],
-    android: ['50%']
+    ios: ['45%'],
+    android: ['45%']
   }
   const IconSource = getNativeIcon(source.displaySymbol)
 
@@ -192,141 +191,143 @@ export function WithdrawFutureSwapScreenV2 (props: Props): JSX.Element {
   }
 
   return (
-    <ThemedScrollViewV2 ref={containerRef} contentContainerStyle={tailwind('flex-grow justify-between pb-12 pt-8')}>
-      <View>
-        <ViewPoolHeader
-          tokenASymbol={source.displaySymbol}
-          tokenBSymbol={destination.displaySymbol}
-          headerLabel={translate('screens/WithdrawFutureSwapScreen', 'View swap info')}
-          onPress={expandModal}
-          testID='view_swap_info'
-        />
-        <View style={tailwind('flex-col mt-2 px-5')}>
-          <ThemedSectionTitleV2
-            testID='withdraw_title'
-            text={translate('screens/WithdrawFutureSwapScreen', 'I WANT TO WITHDRAW')}
+    <View ref={containerRef} style={tailwind('flex-col flex-1')}>
+      <ThemedScrollViewV2 contentContainerStyle={tailwind('flex-grow justify-between pb-12 pt-8')}>
+        <View>
+          <ViewPoolHeader
+            tokenASymbol={source.displaySymbol}
+            tokenBSymbol={destination.displaySymbol}
+            headerLabel={translate('screens/WithdrawFutureSwapScreen', 'View swap info')}
+            onPress={expandModal}
+            testID='view_swap_info'
           />
-          <TransactionCard
-            maxValue={new BigNumber(source.amount)}
-            onChange={onPercentagePress}
-            amountButtonsStyle={tailwind('border-t-0.5')}
-            containerStyle={tailwind('px-5 rounded-t-lg-v2')}
-            status={transactionCardStatus}
-          >
-            <ThemedViewV2
-              light={tailwind('border-mono-light-v2-300')}
-              dark={tailwind('border-mono-dark-v2-300')}
-              style={tailwind('flex flex-row items-center py-4.5')}
+          <View style={tailwind('flex-col mt-2 px-5')}>
+            <ThemedSectionTitleV2
+              testID='withdraw_title'
+              text={translate('screens/WithdrawFutureSwapScreen', 'I WANT TO WITHDRAW')}
+            />
+            <TransactionCard
+              maxValue={new BigNumber(source.amount)}
+              onChange={onPercentagePress}
+              amountButtonsStyle={tailwind('border-t-0.5')}
+              containerStyle={tailwind('px-5 rounded-t-lg-v2')}
+              status={transactionCardStatus}
             >
-              <IconSource height={20} width={20} />
-              <WalletTransactionCardTextInput
-                onFocus={() => setIsInputFocus(true)}
-                onBlur={() => setIsInputFocus(false)}
-                onChangeText={setAmountToWithdraw}
-                placeholder='0.00'
-                value={amountToWithdraw}
-                inputType='numeric'
-                displayClearButton={amountToWithdraw !== ''}
-                onClearButtonPress={() => setAmountToWithdraw('')}
-                testID='text_input_percentage'
-                containerStyle='flex-1 flex-col'
-                inputContainerStyle={tailwind('pl-2 pr-0 py-0')}
-              />
-            </ThemedViewV2>
-          </TransactionCard>
-          {hasError
-            ? (
-              <ThemedTextV2
-                style={tailwind('font-normal-v2 text-xs px-5 pt-2 text-red-v2')}
-                light={tailwind('text-red-v2')} dark={tailwind('text-red-v2')}
-                testID='text_inline_error'
+              <ThemedViewV2
+                light={tailwind('border-mono-light-v2-300')}
+                dark={tailwind('border-mono-dark-v2-300')}
+                style={tailwind('flex flex-row items-center py-4.5')}
               >
-                {translate('screens/WithdrawFutureSwapScreen', 'Insufficient Balance')}
-              </ThemedTextV2>
-            )
-            : (
-              <View style={tailwind('pt-1 px-1')}>
-                <InputHelperTextV2
-                  testID='text_inline'
-                  label={`${translate('screens/WithdrawFutureSwapScreen', 'Withdraw from')}: `}
-                  content={source.amount}
-                  suffix={` ${source.displaySymbol}`}
+                <IconSource height={20} width={20} />
+                <WalletTransactionCardTextInput
+                  onFocus={() => setIsInputFocus(true)}
+                  onBlur={() => setIsInputFocus(false)}
+                  onChangeText={setAmountToWithdraw}
+                  placeholder='0.00'
+                  value={amountToWithdraw}
+                  inputType='numeric'
+                  displayClearButton={amountToWithdraw !== ''}
+                  onClearButtonPress={() => setAmountToWithdraw('')}
+                  testID='text_input_percentage'
+                  containerStyle='flex-1 flex-col'
+                  inputContainerStyle={tailwind('pl-2 pr-0 py-0')}
                 />
-              </View>
-            )}
+              </ThemedViewV2>
+            </TransactionCard>
+            {hasError
+              ? (
+                <ThemedTextV2
+                  style={tailwind('font-normal-v2 text-xs px-5 pt-2 text-red-v2')}
+                  light={tailwind('text-red-v2')} dark={tailwind('text-red-v2')}
+                  testID='text_inline_error'
+                >
+                  {translate('screens/WithdrawFutureSwapScreen', 'Insufficient Balance')}
+                </ThemedTextV2>
+              )
+              : (
+                <View style={tailwind('pt-1 px-1')}>
+                  <InputHelperTextV2
+                    testID='text_inline'
+                    label={`${translate('screens/WithdrawFutureSwapScreen', 'Withdraw from')}: `}
+                    content={source.amount}
+                    suffix={` ${source.displaySymbol}`}
+                  />
+                </View>
+              )}
+          </View>
+
+          {amountToWithdraw.length > 0 && (
+            <View style={tailwind('flex-col')}>
+              <ThemedViewV2
+                style={tailwind('rounded-lg-v2 border-0.5 mt-8 mx-5 p-5 pb-0')}
+                light={tailwind('border-mono-light-v2-300')}
+                dark={tailwind('border-mono-dark-v2-300')}
+              >
+                <NumberRowV2
+                  lhs={{
+                    value: translate('screens/WithdrawFutureSwapScreen', 'Transaction fee'),
+                    testID: 'transaction_fee_label',
+                    themedProps: {
+                      light: tailwind('text-mono-light-v2-500'),
+                      dark: tailwind('text-mono-dark-v2-500')
+                    }
+                  }} rhs={{
+                  value: fee.toFixed(8),
+                  testID: 'text_fee',
+                  suffix: ' DFI',
+                  usdAmount: getTokenPrice('DFI', new BigNumber(fee)),
+                  usdTextStyle: tailwind('text-sm'),
+                  usdContainerStyle: tailwind('pt-1')
+                }}
+                />
+              </ThemedViewV2>
+              <ThemedTextV2
+                style={tailwind('font-normal-v2 text-xs text-center pt-12 pb-5')}
+                light={tailwind('text-mono-light-v2-500')} dark={tailwind('text-mono-dark-v2-500')}
+              >
+                {translate('screens/WithdrawFutureSwapScreen', 'Review full details in the next screen')}
+              </ThemedTextV2>
+            </View>
+          )}
         </View>
 
-        {amountToWithdraw.length > 0 && (
-          <View style={tailwind('flex-col')}>
-            <ThemedViewV2
-              style={tailwind('rounded-lg-v2 border-0.5 mt-8 mx-5 p-5 pb-0')}
-              light={tailwind('border-mono-light-v2-300')}
-              dark={tailwind('border-mono-dark-v2-300')}
-            >
-              <NumberRowV2
-                lhs={{
-                  value: translate('screens/WithdrawFutureSwapScreen', 'Transaction fee'),
-                  testID: 'transaction_fee_label',
-                  themedProps: {
-                    light: tailwind('text-mono-light-v2-500'),
-                    dark: tailwind('text-mono-dark-v2-500')
-                  }
-                }} rhs={{
-                value: fee.toFixed(8),
-                testID: 'text_fee',
-                suffix: ' DFI',
-                usdAmount: getTokenPrice('DFI', new BigNumber(fee)),
-                usdTextStyle: tailwind('text-sm'),
-                usdContainerStyle: tailwind('pt-1')
+        <View style={tailwind('w-full px-12')}>
+          <ButtonV2
+            fillType='fill' label={translate('components/Button', 'Continue')}
+            disabled={hasPendingJob || hasPendingBroadcastJob || new BigNumber(amountToWithdraw).isNaN() || new BigNumber(amountToWithdraw).isZero() || isEnded || hasError}
+            styleProps='w-full'
+            onPress={onSubmit}
+            testID='button_continue_withdraw'
+          />
+        </View>
+
+        {Platform.OS === 'web'
+          ? (
+            <BottomSheetWebWithNavV2
+              modalRef={containerRef}
+              screenList={viewSwapInfo}
+              isModalDisplayed={isModalDisplayed}
+              // eslint-disable-next-line react-native/no-inline-styles
+              modalStyle={{
+                position: 'absolute',
+                bottom: '0',
+                height: '404px',
+                width: '375px',
+                zIndex: 50,
+                borderTopLeftRadius: 15,
+                borderTopRightRadius: 15,
+                overflow: 'hidden'
               }}
-              />
-            </ThemedViewV2>
-            <ThemedTextV2
-              style={tailwind('font-normal-v2 text-xs text-center pt-12 pb-5')}
-              light={tailwind('text-mono-light-v2-500')} dark={tailwind('text-mono-dark-v2-500')}
-            >
-              {translate('screens/WithdrawFutureSwapScreen', 'Review full details in the next screen')}
-            </ThemedTextV2>
-          </View>
-        )}
-      </View>
-
-      <View style={tailwind('w-full px-12')}>
-        <ButtonV2
-          fillType='fill' label={translate('components/Button', 'Continue')}
-          disabled={hasPendingJob || hasPendingBroadcastJob || new BigNumber(amountToWithdraw).isNaN() || new BigNumber(amountToWithdraw).isZero() || isEnded}
-          styleProps='w-full'
-          onPress={onSubmit}
-          testID='button_continue_withdraw'
-        />
-      </View>
-
-      {Platform.OS === 'web'
-        ? (
-          <BottomSheetWebWithNavV2
-            modalRef={containerRef}
-            screenList={viewSwapInfo}
-            isModalDisplayed={isModalDisplayed}
-            // eslint-disable-next-line react-native/no-inline-styles
-            modalStyle={{
-              position: 'absolute',
-              bottom: '0',
-              height: '404px',
-              width: '375px',
-              zIndex: 50,
-              borderTopLeftRadius: 15,
-              borderTopRightRadius: 15,
-              overflow: 'hidden'
-            }}
-          />
-        )
-        : (
-          <BottomSheetWithNavV2
-            modalRef={bottomSheetRef}
-            screenList={viewSwapInfo}
-            snapPoints={modalSortingSnapPoints}
-          />
-        )}
-    </ThemedScrollViewV2>
+            />
+          )
+          : (
+            <BottomSheetWithNavV2
+              modalRef={bottomSheetRef}
+              screenList={viewSwapInfo}
+              snapPoints={modalSortingSnapPoints}
+            />
+          )}
+      </ThemedScrollViewV2>
+    </View>
   )
 }
