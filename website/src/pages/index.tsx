379f1a2d7d1dcf03996017df2b819f91a09a60d0
thedoublejay <work@josejai.me>
2022-09-02 13:55:56 +0800

    chore(ui-ux): redirect to defichain.com

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,9 +1,13 @@
-import { Container } from '@components/commons/Container'
+import { Container } from "@components/commons/Container";
+import { useEffect } from "react";
 
-export default function Index (): JSX.Element {
+export default function Index(): JSX.Element {
+  useEffect(() => {
+    window.location.href = "https://defichain.com";
+  }, []);
   return (
-    <Container className='py-12'>
-      <h1 className='text-3xl font-semibold'>DeFiChain Wallet</h1>
+    <Container className="py-12">
+      <h1 className="text-3xl font-semibold">DeFiChain Wallet</h1>
     </Container>
-  )
+  );
 }
