973bb91a236bdb7949b7dc64e94b513745948742
thedoublejay <work@josejai.me>
2022-09-02 13:23:07 +0800

    chore(deps): downgrade expo jest

--- mobile-app/app/screens/Main.test.tsx
+++ mobile-app/app/screens/Main.test.tsx
@@ -3,7 +3,7 @@ import { Main } from "./Main";
 
 jest.mock("@shared-contexts/ThemeProvider");
 
-describe.skip("<Main>", () => {
+describe("<Main>", () => {
   it("should match snapshot", async () => {
     const component = <Main />;
     const rendered = render(component);
