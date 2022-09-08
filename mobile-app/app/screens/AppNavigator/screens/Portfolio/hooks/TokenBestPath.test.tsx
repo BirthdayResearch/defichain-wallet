2a10c8b503b4234da51df8b597ff00c184559c29
JJ Adonis <work@josejai.me>
2022-09-08 09:14:16 +0800

    chore(deps): bump jellyfish (#3421)
    * chore(deps): bump jellyfish
    
    * chore(deps): update types
    
    * chore(deps): update types

--- mobile-app/app/screens/AppNavigator/screens/Portfolio/hooks/TokenBestPath.test.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Portfolio/hooks/TokenBestPath.test.tsx
@@ -96,6 +96,7 @@ describe("Token Best Path - Get Best Path (DEX)", () => {
           id: "1",
           reserve: pair1ReserveA,
           blockCommission: "0",
+          name: "Bitcoin",
         },
         tokenB: {
           symbol: "DFI",
@@ -103,6 +104,7 @@ describe("Token Best Path - Get Best Path (DEX)", () => {
           id: "0",
           reserve: pair1ReserveB,
           blockCommission: "0",
+          name: "DeFiChain",
         },
         priceRatio: {
           ab: "1",
@@ -142,6 +144,7 @@ describe("Token Best Path - Get Best Path (DEX)", () => {
           id: "14",
           reserve: pair2ReserveA,
           blockCommission: "0",
+          name: "Tether",
         },
         tokenB: {
           symbol: "DFI",
@@ -149,6 +152,7 @@ describe("Token Best Path - Get Best Path (DEX)", () => {
           id: "0",
           reserve: pair2ReserveB,
           blockCommission: "0",
+          name: "DeFiChain",
         },
         priceRatio: {
           ab: "10",
@@ -188,6 +192,7 @@ describe("Token Best Path - Get Best Path (DEX)", () => {
           id: "2",
           reserve: "100000",
           blockCommission: "0",
+          name: "Ethereum",
         },
         tokenB: {
           symbol: "DFI",
@@ -195,6 +200,7 @@ describe("Token Best Path - Get Best Path (DEX)", () => {
           id: "0",
           reserve: "1000",
           blockCommission: "0",
+          name: "DeFiChain",
         },
         priceRatio: {
           ab: "100",
