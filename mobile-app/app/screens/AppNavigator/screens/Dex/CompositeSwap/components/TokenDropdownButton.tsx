7732b3be0740b759b1dcd5be2e9758d7c0080e62
G9000 <leo.caesar@live.com>
2022-09-12 12:51:39 +0800

    fix(ui-ux): remove extra margin on token dropdown

--- mobile-app/app/screens/AppNavigator/screens/Dex/CompositeSwap/components/TokenDropdownButton.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Dex/CompositeSwap/components/TokenDropdownButton.tsx
@@ -49,7 +49,7 @@ export function TokenDropdownButton(props: {
         <>
           <Icon testID="tokenA_icon" height={28} width={28} />
           <ThemedTextV2
-            style={tailwind("ml-2 mr-3.5 text-sm font-semibold-v2")}
+            style={tailwind("ml-2 text-sm font-semibold-v2")}
             dark={tailwind("text-mono-dark-v2-900", {
               "text-opacity-30":
                 props.status === TokenDropdownButtonStatus.Disabled,
@@ -69,6 +69,10 @@ export function TokenDropdownButton(props: {
           iconType="Feather"
           name="chevron-down"
           size={24}
+          style={tailwind(
+            { "mr-2.5": props.symbol === undefined },
+            { "mr-3.5": props.symbol !== undefined }
+          )}
           dark={tailwind("text-mono-dark-v2-700")}
           light={tailwind("text-mono-light-v2-700")}
         />
