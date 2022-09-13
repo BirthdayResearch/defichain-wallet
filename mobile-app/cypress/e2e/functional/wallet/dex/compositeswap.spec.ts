0ec9367fb99d0059b78b096e7a7bb615bdd3d564
chloe <chloezhengxy@gmail.com>
2022-09-13 10:26:50 +0800

    chore(e2e): update swap test for price rates summary

--- mobile-app/cypress/e2e/functional/wallet/dex/compositeswap.spec.ts
+++ mobile-app/cypress/e2e/functional/wallet/dex/compositeswap.spec.ts
@@ -1,4 +1,5 @@
 import BigNumber from "bignumber.js";
+import { checkValueWithinRange } from "../../../../support/walletCommands";
 
 function setupWalletForConversion(): void {
   cy.createEmptyWallet(true);
@@ -11,6 +12,19 @@ function setupWalletForConversion(): void {
   cy.getByTestID("select_DFI").click().wait(1000);
   cy.getByTestID("token_select_button_TO").should("exist").click();
   cy.getByTestID("select_dLTC").click().wait(1000);
+}
+
+function setupWalletForConversionETH(): void {
+  cy.createEmptyWallet(true);
+  cy.sendDFItoWallet().sendDFITokentoWallet().wait(5000);
+
+  cy.getByTestID("bottom_tab_dex").click().wait(3000);
+  cy.getByTestID("composite_swap").click().wait(3000);
+  cy.getByTestID("token_select_button_FROM").should("exist").click();
+  cy.wait(3000);
+  cy.getByTestID("select_DFI").click().wait(1000);
+  cy.getByTestID("token_select_button_TO").should("exist").click();
+  cy.getByTestID("select_dETH").click().wait(1000);
 }
 
 function setCustomSlippage(customSlippage: string): void {
@@ -174,31 +188,6 @@ context("Wallet - DEX - Instant/Future Swap - tabs and dropdowns", () => {
       "have.text",
       "DFI"
     );
-  });
-
-  // the value of price rates keeps changing - can't assert an absolute value
-  it("should be able to display price rates if tokenA and tokenB is selected", () => {
-    cy.getByTestID("instant_swap_summary").should("exist");
-    cy.getByTestID("instant_swap_prices_0_label")
-      .should("exist")
-      .should("have.text", "1 dTU10 =");
-    cy.getByTestID("instant_swap_prices_0").should("exist").contains("DFI");
-    cy.getByTestID("instant_swap_prices_0_rhsUsdAmount")
-      .should("exist")
-      .contains("$");
-    cy.getByTestID("instant_swap_prices_1_label")
-      .should("exist")
-      .should("have.text", "1 DFI =");
-    cy.getByTestID("instant_swap_prices_1").should("exist").contains("dTU10");
-    cy.getByTestID("instant_swap_prices_1_rhsUsdAmount")
-      .should("exist")
-      .contains("$");
-    cy.getByTestID("swap_total_fees_label")
-      .should("exist")
-      .should("have.text", "Total fees");
-    cy.getByTestID("swap_total_fee_amount")
-      .should("exist")
-      .should("have.text", "-");
   });
 
   it("should be able to disable future swap tab if tokenA and tokenB is not a valid future swap pair", () => {
@@ -509,3 +498,41 @@ context(
     });
   }
 );
+
+// DFI -> dETH to show greater price rates difference
+context("Wallet - DEX - Instant Swap (DFI) - Summary", () => {
+  before(() => {
+    setupWalletForConversionETH();
+  });
+  it("should be able to display price rates if tokenA and tokenB is selected", () => {
+    cy.getByTestID("instant_swap_summary").should("exist");
+    cy.getByTestID("instant_swap_prices_0_label")
+      .should("exist")
+      .should("have.text", "1 DFI =");
+    cy.getByTestID("instant_swap_prices_0")
+      .should("exist")
+      .contains("100.00000000 dETH");
+    cy.getByTestID("instant_swap_prices_0_rhsUsdAmount")
+      .invoke("text")
+      .then((value) => {
+        checkValueWithinRange(value, "10,000.00");
+      });
+    cy.getByTestID("instant_swap_prices_1_label")
+      .should("exist")
+      .should("have.text", "1 dETH =");
+    cy.getByTestID("instant_swap_prices_1")
+      .should("exist")
+      .contains("0.01000000 DFI");
+    cy.getByTestID("instant_swap_prices_1_rhsUsdAmount")
+      .invoke("text")
+      .then((value) => {
+        checkValueWithinRange(value, "100.00");
+      });
+    cy.getByTestID("swap_total_fees_label")
+      .should("exist")
+      .should("have.text", "Total fees");
+    cy.getByTestID("swap_total_fee_amount")
+      .should("exist")
+      .should("have.text", "-");
+  });
+});
