225f2cc641dd5b3677f4039f674a7efce3380fc5
pierregee <pierre.gabaldon@gmail.com>
2022-09-15 18:20:51 +0800

    chore(debug): debug long running e2e

--- mobile-app/cypress/e2e/functional/wallet/dex/compositeswap.spec.ts
+++ mobile-app/cypress/e2e/functional/wallet/dex/compositeswap.spec.ts
@@ -1,5 +1,4 @@
 import BigNumber from "bignumber.js";
-import { checkValueWithinRange } from "../../../../support/walletCommands";
 
 function setupWalletForConversion(): void {
   cy.createEmptyWallet(true);
@@ -23,7 +22,6 @@ function setCustomSlippage(customSlippage: string): void {
   cy.getByTestID("set_slippage_button").click().wait(3000);
 }
 
-// TODO: @joshua update e2e
 context("Wallet - DEX - disabled pool pairs", () => {
   before(() => {
     cy.intercept("**/poolpairs?size=*", {
@@ -374,79 +372,6 @@ context("Wallet - DEX - Instant Swap (non-DFI)", () => {
   });
 });
 
-context.skip("Wallet - DEX - Instant Swap (non-DFI) - Confirm Txn", () => {
-  before(() => {
-    cy.createEmptyWallet(true);
-    cy.getByTestID("header_settings").click();
-    cy.sendDFItoWallet()
-      .sendDFITokentoWallet()
-      .sendTokenToWallet(["LTC", "USDC"])
-      .wait(5000);
-    cy.fetchWalletBalance();
-    cy.getByTestID("bottom_tab_portfolio").click();
-    cy.getByTestID("bottom_tab_dex").click();
-  });
-
-  // Swap LTC through -> LTC-DFI -> DFI-USDC -> to get USDC
-  it("should be able to swap tokens with 2 hops", () => {
-    cy.getByTestID("composite_swap").click().wait(5000);
-    cy.getByTestID("token_select_button_FROM").click();
-    cy.getByTestID("select_dLTC").click().wait(1000);
-    cy.getByTestID("token_select_button_TO").click();
-    cy.getByTestID("select_dUSDC").click().wait(1000);
-  });
-
-  it("should be able to select direct pair", () => {
-    cy.getByTestID("bottom_tab_portfolio").click();
-    cy.getByTestID("bottom_tab_dex").click();
-    cy.getByTestID("token_select_button_FROM").click();
-    cy.getByTestID("select_dLTC").click().wait(100);
-    cy.getByTestID("token_select_button_TO").click();
-    cy.getByTestID("select_DFI").click().wait(100);
-  });
-
-  it("should be able to switch tokens and reset values", () => {
-    cy.getByTestID("switch_button").click();
-    cy.getByTestID("text_input_tokenA").should("have.value", "");
-    cy.getByTestID("tokenB_value").contains("0.00");
-  });
-
-  it("should be able to cancel authorization", () => {
-    cy.getByTestID("text_input_tokenA").type("10");
-    setCustomSlippage("10");
-    cy.getByTestID("button_confirm_submit").click();
-
-    cy.getByTestID("confirm_slippage_fee").should("have.text", "10%");
-    cy.getByTestID("confirm_title").contains("You are swapping");
-    cy.getByTestID("button_confirm_swap").click().wait(3000);
-    // Cancel send on authorisation page
-    cy.getByTestID("cancel_authorization").click();
-  });
-
-  it("should be able to swap", () => {
-    cy.getByTestID("confirm_estimated_to_receive").then(() => {
-      cy.getByTestID("button_confirm_swap").click().wait(3000);
-      cy.closeOceanInterface();
-      cy.fetchWalletBalance();
-      cy.getByTestID("bottom_tab_portfolio").click();
-      cy.getByTestID("portfolio_row_4").should("exist");
-
-      /* Estimated return is not accurate yet due to tolerable slippage */
-      // const tokenValue = $txt[0].textContent
-      //   .replace(" dLTC", "")
-      //   .replace(",", "");
-      // cy.getByTestID("portfolio_row_4_amount").then(($txt: any) => {
-      //   const balanceAmount = $txt[0].textContent
-      //     .replace(" dLTC", "")
-      //     .replace(",", "");
-      //   expect(new BigNumber(balanceAmount).toNumber()).be.gte(
-      //     new BigNumber(tokenValue).toNumber()
-      //   );
-      // });
-    });
-  });
-});
-
 context(
   "Wallet - DEX - Instant Swap (DFI with Conversion/Reserved fees)",
   () => {
@@ -489,42 +414,3 @@ context(
     });
   }
 );
-
-// DFI -> dETH to show greater price rates difference
-context("Wallet - DEX - Instant Swap (DFI) - Summary", () => {
-  before(() => {
-    setupWalletForConversion();
-    setupFromAndToTokens("DFI", "dETH");
-  });
-  it("should be able to display price rates if tokenA and tokenB is selected", () => {
-    cy.getByTestID("instant_swap_summary").should("exist");
-    cy.getByTestID("instant_swap_prices_0_label")
-      .should("exist")
-      .should("have.text", "1 DFI =");
-    cy.getByTestID("instant_swap_prices_0")
-      .should("exist")
-      .contains("100.00000000 dETH");
-    cy.getByTestID("instant_swap_prices_0_rhsUsdAmount")
-      .invoke("text")
-      .then((value) => {
-        checkValueWithinRange(value, "10,000.00");
-      });
-    cy.getByTestID("instant_swap_prices_1_label")
-      .should("exist")
-      .should("have.text", "1 dETH =");
-    cy.getByTestID("instant_swap_prices_1")
-      .should("exist")
-      .contains("0.01000000 DFI");
-    cy.getByTestID("instant_swap_prices_1_rhsUsdAmount")
-      .invoke("text")
-      .then((value) => {
-        checkValueWithinRange(value, "100.00");
-      });
-    cy.getByTestID("swap_total_fees_label")
-      .should("exist")
-      .should("have.text", "Total fees");
-    cy.getByTestID("swap_total_fee_amount")
-      .should("exist")
-      .should("have.text", "-");
-  });
-});
