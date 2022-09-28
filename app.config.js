08eeca236f102974b6c7d44f53ec28e188994180
thedoublejay <work@josejai.me>
2022-09-28 15:24:49 +0800

    chore(deps): eas publish

--- app.config.js
+++ app.config.js
@@ -4,11 +4,6 @@ export default ({ config }) => {
     ...config.ios,
     bundleIdentifier: process.env.EXPO_BUNDLE_IDENTIFIER,
   };
-  config.updates = {
-    ...config.updates,
-    url: process.env.EAS_UPDATE_URL,
-  };
-  config.extra.eas.projectId = process.env.EAS_PROJECT_ID;
   return {
     ...config,
   };
