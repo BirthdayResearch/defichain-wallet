9494e588e2956ae808fe111c51c2bf2db712e222
chloe <chloezhengxy@gmail.com>
2022-09-30 16:17:26 +0800

    fix(ui-ux) fix ui issue

--- mobile-app/app/screens/AppNavigator/screens/Loans/components/LoanCardsV2.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Loans/components/LoanCardsV2.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import {
   ThemedFlashList,
   ThemedIcon,
+  ThemedScrollViewV2,
   ThemedText,
   ThemedTextV2,
   ThemedTouchableOpacityV2,
@@ -151,18 +152,20 @@ export function LoanCardsV2(props: LoanCardsProps): JSX.Element {
   };
 
   return (
-    <>
+    <ThemedScrollViewV2>
       {vaults.length === 0 && (
-        <View style={tailwind("mx-5 mt-8 rounded-lg-v2")}>
-          <VaultBanner
-            buttonLabel="Create a vault"
-            description="You need a vault with collaterals to borrow tokens"
-            onButtonPress={goToCreateVault}
-          />
+        <>
+          <ThemedViewV2 style={tailwind("mx-5 mt-8 rounded-lg-v2")}>
+            <VaultBanner
+              buttonLabel="Create a vault"
+              description="You need a vault with collaterals to borrow tokens"
+              onButtonPress={goToCreateVault}
+            />
+          </ThemedViewV2>
           <View style={tailwind("mt-2")}>
             <PriceOracleInfo onPress={onBottomSheetOraclePriceSelect} />
           </View>
-        </View>
+        </>
       )}
       {/* Known intermittent issue wherein the two-column layout is not followed in web - FlashList */}
       <ThemedFlashList
@@ -244,7 +247,7 @@ export function LoanCardsV2(props: LoanCardsProps): JSX.Element {
           }}
         />
       )}
-    </>
+    </ThemedScrollViewV2>
   );
 }
 
