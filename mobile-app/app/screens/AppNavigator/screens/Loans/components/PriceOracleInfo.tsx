599ba1003e3a11ecb928c654b18f9e03eb67aef7
chloe <chloezhengxy@gmail.com>
2022-10-04 13:32:36 +0800

    fix(ui-ux): icon alignment for loans

--- mobile-app/app/screens/AppNavigator/screens/Loans/components/PriceOracleInfo.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Loans/components/PriceOracleInfo.tsx
@@ -10,28 +10,26 @@ interface PriceOracleInfoProps {
 export function PriceOracleInfo(props: PriceOracleInfoProps): JSX.Element {
   return (
     <View style={tailwind("items-center")}>
-      <TouchableOpacity
-        onPress={props.onPress}
-        style={tailwind("text-center flex-row items-center")}
-      >
+      <TouchableOpacity onPress={props.onPress}>
         <ThemedTextV2
           dark={tailwind("text-mono-dark-v2-500")}
           light={tailwind("text-mono-light-v2-500")}
-          style={tailwind("font-normal-v2 text-sm mr-1")}
+          style={tailwind("font-normal-v2 text-sm")}
         >
           {translate(
             "screens/LoansScreen",
             "Loan tokens get their prices from oracles."
           )}
+          <View style={tailwind("pl-1 pt-1")}>
+            <ThemedIcon
+              dark={tailwind("text-mono-dark-v2-700")}
+              light={tailwind("text-mono-light-v2-700")}
+              iconType="MaterialIcons"
+              name="info-outline"
+              size={16}
+            />
+          </View>
         </ThemedTextV2>
-
-        <ThemedIcon
-          dark={tailwind("text-mono-dark-v2-700")}
-          light={tailwind("text-mono-light-v2-700")}
-          iconType="MaterialIcons"
-          name="info-outline"
-          size={16}
-        />
       </TouchableOpacity>
     </View>
   );
