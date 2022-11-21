675219174b514aa98bef5c24c0c8fd50f9abef16
pierregee <pierre.gabaldon@gmail.com>
2022-11-21 10:55:02 +0800

    fix(unit-tests): add bottom sheet modal provider on empty vault test

--- mobile-app/app/screens/AppNavigator/screens/Loans/components/EmptyVault.test.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Loans/components/EmptyVault.test.tsx
@@ -1,16 +1,20 @@
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { render } from "@testing-library/react-native";
 
 import { EmptyVault } from "./EmptyVault";
 
 jest.mock("@shared-contexts/ThemeProvider");
 jest.mock("@react-navigation/native", () => ({
+  ...jest.requireActual("@react-navigation/native"),
   useNavigation: jest.fn(),
 }));
 
 describe("Empty vault", () => {
   it("should match snapshot", async () => {
     const rendered = render(
-      <EmptyVault handleRefresh={() => jest.fn} isLoading={false} />
+      <BottomSheetModalProvider>
+        <EmptyVault handleRefresh={() => jest.fn} isLoading={false} />
+      </BottomSheetModalProvider>
     );
     expect(rendered.toJSON()).toMatchSnapshot();
   });
