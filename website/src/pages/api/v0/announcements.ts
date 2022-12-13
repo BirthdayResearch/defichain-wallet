10146ec1444942534b3b9f782b77788efe006f06
thedoublejay <work@josejai.me>
2022-12-13 16:23:57 +0800

    feature(setup): added `website`

--- website/src/pages/api/v0/announcements.ts
+++ website/src/pages/api/v0/announcements.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { AnnouncementData } from "@waveshq/wallet-core";
 import Cors from "cors";
 import { runMiddleware } from "../../../utils/middleware";
 
@@ -8,7 +9,7 @@ export const cors = Cors({
 
 export default async function handle(
   req: NextApiRequest,
-  res: NextApiResponse<any[]>
+  res: NextApiResponse<AnnouncementData[]>
 ): Promise<void> {
   await runMiddleware(req, res, cors);
   res.json([]);
