24c53232ca4df7392deecc37a6cbaa8dfc3874b5
thedoublejay <work@josejai.me>
2022-12-13 17:21:46 +0800

    feature(setup): removed `website` folder

--- jest.config.js
+++ jest.config.js
@@ -19,8 +19,7 @@ module.exports = {
     "!**/_shim.js",
     "!**/screens/PlaygroundNavigator/**",
     "!**/cypress-coverage/**",
-    "!**/jest-coverage/**",
-    "!**/website/**",
+    "!**/jest-coverage/**"
   ],
   coverageDirectory: "jest-coverage",
   setupFiles: ["./jest.setup.js"],
