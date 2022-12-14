801b0e4e9ef5ee9c6e2c36bf90b615cb61f333fd
thedoublejay <work@josejai.me>
2022-12-14 17:55:50 +0800

    feature(wallet-ui): moved blocks, ocean and transaction_queue

--- jest.config.js
+++ jest.config.js
@@ -23,4 +23,7 @@ module.exports = {
   ],
   coverageDirectory: "jest-coverage",
   setupFiles: ["./jest.setup.js"],
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@waveshq/.*)",
+  ],
 };
