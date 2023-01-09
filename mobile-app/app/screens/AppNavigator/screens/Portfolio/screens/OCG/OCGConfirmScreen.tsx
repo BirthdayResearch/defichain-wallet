29e777a6e54584973440793c7cde421c23fd7f57
Joshua <josh_817@hotmail.com>
2023-01-09 21:23:57 +0800

    translations: update strings

--- mobile-app/app/screens/AppNavigator/screens/Portfolio/screens/OCG/OCGConfirmScreen.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Portfolio/screens/OCG/OCGConfirmScreen.tsx
@@ -351,7 +351,7 @@ function FeeSection({
           title: translate("screens/OCGConfirmScreen", "Proposal Fees"),
           message: translate(
             "screens/OCGConfirmScreen",
-            "Proposals fees are implemented to  encourage more responsible submissions.\nProposal fees are calculated based on the type of proposal selected, and the requested amount (only for CFP).\nRefer to our FAQ section for more detailed breakdown."
+            "Proposals fees are implemented to encourage more responsible submissions.\nProposal fees are calculated based on the type of proposal selected, and the requested amount (only for CFP).\nRefer to our FAQ section for more detailed breakdown."
           ),
           iconStyle: {
             light: tailwind("text-mono-light-v2-500"),
