1a7b992c9951e4fd34eaf1d7b8d4215225d65ad6
Harsh <harshrathi.dev@gmail.com>
2023-01-18 12:41:47 +0530

    fix(ui-ux): fixed dex pool pair card price conversion to match with scan

--- mobile-app/app/screens/AppNavigator/screens/Dex/components/PoolPairCards/PoolPairCards.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Dex/components/PoolPairCards/PoolPairCards.tsx
@@ -8,7 +8,6 @@ import {
 import { PoolPairData } from "@defichain/whale-api-client/dist/api/poolpairs";
 import { tailwind } from "@tailwind";
 import { translate } from "@translations";
-import { useTokenBestPath } from "@screens/AppNavigator/screens/Portfolio/hooks/TokenBestPath";
 import React, { useEffect, useRef, useState } from "react";
 import { useScrollToTop } from "@react-navigation/native";
 import { WalletToken } from "@waveshq/walletkit-ui";
@@ -238,38 +237,15 @@ function PoolCard({
   onAdd,
   onRemove,
 }: PoolCardProps): JSX.Element {
-  const { calculatePriceRates } = useTokenBestPath();
   const { getTokenPrice } = useTokenPrice();
   const { poolpairs: pairs } = useSelector((state: RootState) => state.wallet);
-  const blockCount = useSelector((state: RootState) => state.block.count);
   const { data: yourPair } = item;
   const isFavoritePair = isFavouritePoolpair(yourPair.id);
-
-  const [priceRates, setPriceRates] = useState({
-    aToBPrice: new BigNumber(""),
-    bToAPrice: new BigNumber(""),
-    estimated: new BigNumber(""),
-  });
 
   const poolPairData = pairs.find(
     (pr) => pr.data.symbol === (yourPair as AddressToken).symbol
   );
   const mappedPair = poolPairData?.data;
-
-  useEffect(() => {
-    void getPriceRates();
-  }, [mappedPair, blockCount]);
-
-  const getPriceRates = async (): Promise<void> => {
-    if (mappedPair !== undefined) {
-      const priceRates = await calculatePriceRates(
-        mappedPair.tokenA.id,
-        mappedPair.tokenB.id,
-        new BigNumber("1")
-      );
-      setPriceRates(priceRates);
-    }
-  };
 
   const [symbolA, symbolB] =
     mappedPair?.tokenA != null && mappedPair?.tokenB != null
@@ -295,8 +271,8 @@ function PoolCard({
               symbolB={symbolB}
               pair={mappedPair}
               onSwap={() => onSwap(mappedPair, yourPair as WalletToken)}
-              aToBPrice={priceRates.aToBPrice}
-              bToAPrice={priceRates.bToAPrice}
+              aToBPrice={new BigNumber(mappedPair.priceRatio.ba)}
+              bToAPrice={new BigNumber(mappedPair.priceRatio.ab)}
               isFavouritePair={isFavoritePair}
               setFavouritePoolpair={setFavouritePoolpair}
               status={mappedPair.status}
