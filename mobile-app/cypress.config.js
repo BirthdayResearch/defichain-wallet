97514bdeb681327f2f98960725a6f33cdc17e542
Rampraesath <aries.rj@gmail.com>
2023-06-09 04:03:46 +0800

    added excludeSpecPattern into config file that will be read by github action

--- mobile-app/cypress.config.js
+++ mobile-app/cypress.config.js
@@ -20,5 +20,30 @@ module.exports = defineConfig({
     },
     baseUrl: "http://localhost:19006",
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+    excludeSpecPattern:[
+      //Functional/Wallet/Auctions
+      "cypress/e2e/functional/wallet/auctions/auctions.spec.ts",
+      "cypress/e2e/functional/wallet/auctions/auctions_bid_with_multiple_wallet.spec.ts",
+      //Functional/Wallet/Authorization
+      "cypress/e2e/functional/wallet/authorization/transactionAuthorization.spec.ts",
+      //Functional/Wallet/Dex
+      "cypress/e2e/functional/wallet/dex/add_liquidity.spec.ts",
+      "cypress/e2e/functional/wallet/dex/dex_poolpairs.spec.ts",
+      //Functional/Wallet/Dex/Swap
+      "cypress/e2e/functional/wallet/dex/swap/swap_future.spec.ts",
+      "cypress/e2e/functional/wallet/dex/swap/swap_future_withdraw.spec.ts",
+      //Functional/Wallet/Loans
+      "cypress/e2e/functional/wallet/loans/loans_payback.spec.ts",
+      "cypress/e2e/functional/wallet/loans/vault_detail.spec.ts",
+      //Functional/Wallet/Portfolio
+      "cypress/e2e/functional/wallet/portfolio/addresses.spec.ts",
+      "cypress/e2e/functional/wallet/portfolio/cfp_dfip.specs.ts",
+      "cypress/e2e/functional/wallet/portfolio/send.spec.ts",
+      // Functional/Wallet/Settings
+      "cypress/e2e/functional/wallet/settings/serviceProvider.spec.ts",
+      //Functional/Wallet/Transaction
+      "cypress/e2e/functional/wallet/transaction/list_empty.spec.spec.ts"
+    ]
+
   },
 });
