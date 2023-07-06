73c36b3e329a98374772199ea546d19e93d6f39e
elocin <elocinnat99@gmail.com>
2023-07-06 11:10:49 +0800

    Update mobile-app/app/components/icons/WalletIcon.tsx
    Co-authored-by: Lyka Labrada <lykalabrada@gmail.com>

--- mobile-app/app/components/icons/WalletIcon.tsx
+++ mobile-app/app/components/icons/WalletIcon.tsx
@@ -15,7 +15,7 @@ export function WalletIcon(props: SvgProps): JSX.Element {
       <Path
         d="M5.75 6.5C5.75 6.08579 6.08579 5.75 6.5 5.75H9.25V7.25H6.5C6.08579 7.25 5.75 6.91421 5.75 6.5Z"
         stroke={props.color}
-        stroke-width="1.5"
+        strokeWidth="1.5"
       />
     </Svg>
   );
