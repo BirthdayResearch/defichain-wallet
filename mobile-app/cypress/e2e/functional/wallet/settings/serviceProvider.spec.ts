580321af4e86d4e98acf3c064832ab22fdd29168
Rampraesath <aries.rj@gmail.com>
2023-07-07 14:39:30 +0800

    retest run

--- mobile-app/cypress/e2e/functional/wallet/settings/serviceProvider.spec.ts
+++ mobile-app/cypress/e2e/functional/wallet/settings/serviceProvider.spec.ts
@@ -100,7 +100,7 @@ defichainUrlEnvs.forEach((defichainUrlEnv) => {
           "aria-disabled"
         );
         cy.getByTestID("button_submit").click().wait(3000);
-        cy.getByTestID("pin_authorize").type("000000");
+        cy.getByTestID("pin_authorize").type("000000", { delay: 3000 });
         cy.wait(10000);
         cy.url().should("include", "app/portfolio");
         cy.getByTestID("bottom_tab_portfolio").click();
@@ -124,7 +124,7 @@ defichainUrlEnvs.forEach((defichainUrlEnv) => {
         cy.getByTestID("setting_navigate_service_provider").click();
         cy.getByTestID("edit_service_provider").click();
         cy.getByTestID("reset_button").should("exist").click().wait(3000);
-        cy.getByTestID("pin_authorize").type("000000");
+        cy.getByTestID("pin_authorize").type("000000", { delay: 3000 });
         cy.wait(5000);
         cy.getByTestID("bottom_tab_portfolio").click();
         cy.getByTestID("header_settings").click().wait(1000);
