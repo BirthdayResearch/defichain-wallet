13b47fe182a7312b82154236ee32fc1358322969
nattadex <elocinnat99@gmail.com>
2023-07-10 16:16:31 +0800

    reduced isEvmDomain conditions and css show/hide

--- mobile-app/app/components/DomainSwitch.tsx
+++ mobile-app/app/components/DomainSwitch.tsx
@@ -23,90 +23,76 @@ export function DomainSwitch({ testID }: { testID: string }): JSX.Element {
       })}
       testID={testID}
     >
-      <ThemedTouchableOpacityV2
-        activeOpacity={1}
-        onPress={async () => {
-          setDomain(
-            domain === DomainType.DFI ? DomainType.EVM : DomainType.DFI
-          );
-          await DomainPersistence.set(domain);
-        }}
-        style={tailwind(
-          "flex-1 flex-row justify-center rounded-full items-center p-0 absolute left-0"
-        )}
-      >
-        <View
+      {domain === DomainType.DFI && (
+        <ThemedTouchableOpacityV2
+          activeOpacity={1}
+          onPress={async () => {
+            setDomain(DomainType.EVM);
+            await DomainPersistence.set(domain);
+          }}
           style={tailwind(
-            "rounded-full p-1 flex-row items-center",
-            {
+            "flex-1 flex-row justify-center rounded-full items-center p-0 absolute left-0"
+          )}
+        >
+          <View
+            style={tailwind("rounded-full p-1 flex-row items-center", {
               "bg-transparent": isEvmDomain,
               "bg-mono-light-v2-1000": isLight && !isEvmDomain,
               "bg-mono-dark-v2-1000": !isLight && !isEvmDomain,
-            },
-            {
-              hidden: isEvmDomain,
-              flex: !isEvmDomain,
-            }
-          )}
-        >
-          <View style={tailwind("bg-brand-v2-500 p-1 rounded-full mr-1")}>
-            <DFIIcon width={12.5} height={12.5} color="#FFFFFF" />
-          </View>
-
-          <Text
-            style={tailwind("mr-2 text-xs", {
-              "text-white": isLight,
-              "text-black": !isLight,
             })}
           >
-            DFI
-          </Text>
-        </View>
-      </ThemedTouchableOpacityV2>
-      <ThemedTouchableOpacityV2
-        activeOpacity={1}
-        onPress={async () => {
-          setDomain(
-            domain === DomainType.DFI ? DomainType.EVM : DomainType.DFI
-          );
-          await DomainPersistence.set(domain);
-        }}
-        style={tailwind(
-          "flex-1 rounded-full flex-row justify-center items-center absolute right-0"
-        )}
-      >
-        <LinearGradient
-          colors={["#42F9C2", "#3B57CF"]}
-          start={[0, 0]}
-          end={[1, 1]}
-          style={tailwind("rounded-full")}
-        >
-          <View
-            style={tailwind(
-              "rounded-full p-1 flex-row items-center",
-              {
-                "bg-transparent": !isEvmDomain,
-              },
-              {
-                hidden: !isEvmDomain,
-                flex: isEvmDomain,
-              }
-            )}
-          >
-            <View
-              style={tailwind(
-                "flex items-center justify-center rounded-full w-5 h-5"
-              )}
-            >
-              <EvmDFIIcon width={20} height={20} />
+            <View style={tailwind("bg-brand-v2-500 p-1 rounded-full mr-1")}>
+              <DFIIcon width={12.5} height={12.5} color="#FFFFFF" />
             </View>
 
-            <Text style={tailwind("text-mono-light-v2-00 text-xs ml-1")}>
-              EVM
+            <Text
+              style={tailwind("mr-2 text-xs", {
+                "text-white": isLight,
+                "text-black": !isLight,
+              })}
+            >
+              DFI
             </Text>
           </View>
-        </LinearGradient>
-      </ThemedTouchableOpacityV2>
+        </ThemedTouchableOpacityV2>
+      )}
+      {domain === DomainType.EVM && (
+        <ThemedTouchableOpacityV2
+          activeOpacity={1}
+          onPress={async () => {
+            setDomain(DomainType.DFI);
+            await DomainPersistence.set(domain);
+          }}
+          style={tailwind(
+            "flex-1 rounded-full flex-row justify-center items-center absolute right-0"
+          )}
+        >
+          <LinearGradient
+            colors={["#42F9C2", "#3B57CF"]}
+            start={[0, 0]}
+            end={[1, 1]}
+            style={tailwind("rounded-full")}
+          >
+            <View
+              style={tailwind("rounded-full p-1 flex-row items-center", {
+                "bg-transparent": !isEvmDomain,
+              })}
+            >
+              <View
+                style={tailwind(
+                  "flex items-center justify-center rounded-full w-5 h-5"
+                )}
+              >
+                <EvmDFIIcon width={20} height={20} />
+              </View>
+
+              <Text style={tailwind("text-mono-light-v2-00 text-xs ml-1")}>
+                EVM
+              </Text>
+            </View>
+          </LinearGradient>
+        </ThemedTouchableOpacityV2>
+      )}
     </View>
   );
 }
