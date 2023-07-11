f7b49feb72cd65ca89b1413c9cedf9c025095bcd
elocin <elocinnat99@gmail.com>
2023-07-11 14:19:31 +0800

    feature(ui-ux): added dvm evm designs for onboarding (#3983)
    * added new image slide on the welcome screen
    
    * added condition to create wallet page
    
    * shortened import
    
    * update snapshot
    
    * added onboarding translations
    
    * added DE and FR translations
    
    * Update mobile-app/app/components/icons/WalletIcon.tsx
    
    Co-authored-by: Lyka Labrada <lykalabrada@gmail.com>
    
    * Update mobile-app/app/components/icons/WalletIcon.tsx
    
    Co-authored-by: Lyka Labrada <lykalabrada@gmail.com>
    
    * fixed logo cut off
    
    * updated snapshot
    
    * included contentFit contain
    
    * updated for consistency
    
    * Update shared/translations/languages/zh-Hant.json
    
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>
    
    * Update shared/translations/languages/zh-Hant.json
    
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>
    
    * Update shared/translations/languages/zh-Hant.json
    
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>
    
    * Update shared/translations/languages/zh-Hans.json
    
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>
    
    * Update shared/translations/languages/zh-Hans.json
    
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>
    
    * Update shared/translations/languages/zh-Hans.json
    
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>
    
    * Update shared/translations/languages/it.json
    
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>
    
    * Update shared/translations/languages/fr.json
    
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>
    
    * Update shared/translations/languages/it.json
    
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>
    
    * Update shared/translations/languages/fr.json
    
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>
    
    * Update shared/translations/languages/de.json
    
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>
    
    * Update shared/translations/languages/es.json
    
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>
    
    * Update shared/translations/languages/es.json
    
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>
    
    * Update shared/translations/languages/de.json
    
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>
    
    * updated ZHS and ZHT translations
    
    * Update mobile-app/app/screens/WalletNavigator/screens/CreateWallet/CreateWalletGuidelines.tsx
    
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>
    
    * removed unnecessary return
    
    ---------
    
    Co-authored-by: Lyka Labrada <lykalabrada@gmail.com>
    Co-authored-by: Harsh R <53080940+fullstackninja864@users.noreply.github.com>

---
+++ mobile-app/app/components/icons/WalletIcon.tsx
@@ -0,0 +1,22 @@
+import Svg, { Path, Rect, SvgProps } from "react-native-svg";
+
+export function WalletIcon(props: SvgProps): JSX.Element {
+  return (
+    <Svg width="12" height="12" viewBox="0 0 12 12" fill="none" {...props}>
+      <Rect
+        x="1"
+        y="2"
+        width="9.53"
+        height="9"
+        rx="1"
+        stroke={props.color}
+        strokeWidth="1.5"
+      />
+      <Path
+        d="M5.75 6.5C5.75 6.08579 6.08579 5.75 6.5 5.75H9.25V7.25H6.5C6.08579 7.25 5.75 6.91421 5.75 6.5Z"
+        stroke={props.color}
+        strokeWidth="1.5"
+      />
+    </Svg>
+  );
+}
