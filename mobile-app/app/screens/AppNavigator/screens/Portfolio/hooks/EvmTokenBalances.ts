229b36ac913b456fead522ceb74a39cb8af63b44
Lyka Labrada <lykalabrada@gmail.com>
2023-08-07 16:41:51 +0800

    replace token0 with CAS

--- mobile-app/app/screens/AppNavigator/screens/Portfolio/hooks/EvmTokenBalances.ts
+++ mobile-app/app/screens/AppNavigator/screens/Portfolio/hooks/EvmTokenBalances.ts
@@ -22,13 +22,13 @@ export function useEvmTokenBalances(): { evmTokens: AddressToken[] } {
     const evmAddressTokens: AddressToken[] = tokens
       // .filter(({ token }) => token.type === "DST20") // TODO (lyka): Add filter to only get DFI and DST20 tokens
       .map(({ token_id, value, token }) => ({
-        id: token.symbol === "DACKIE" ? "0" : token_id, // TODO (lyka): use `DFI` symbol instead
+        id: token.symbol === "CAS" ? "0" : token_id, // TODO (lyka): use `DFI` symbol instead
         amount: formatUnits(
           BigInt(value ?? "0"),
           Number(token.decimals ?? GWEI_DECIMAL)
         ),
         symbol: token.symbol,
-        displaySymbol: token.symbol === "DACKIE" ? "EvmDFI" : token.symbol, // TODO (lyka): use `DFI` symbol instead
+        displaySymbol: token.symbol === "CAS" ? "EvmDFI" : token.symbol, // TODO (lyka): use `DFI` symbol instead
         symbolKey: token.symbol,
         name: token.name,
         isDAT: false,
