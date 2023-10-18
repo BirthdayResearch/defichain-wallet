7c2d256ec2d16e4a8d6481235b5c0ce5142d52d8
Lyka Labrada <lykalabrada@gmail.com>
2023-10-18 18:59:53 +0800

    fix(core): update evm provider

--- mobile-app/app/contexts/EVMProvider.tsx
+++ mobile-app/app/contexts/EVMProvider.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useContext, useMemo, useState } from "react";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import { providers } from "ethers";
 import { useNetworkContext } from "@waveshq/walletkit-ui";
 import { useCustomServiceProviderContext } from "./CustomServiceProvider";
@@ -19,23 +25,30 @@ export function EVMProvider({
   const { ethRpcUrl } = useCustomServiceProviderContext();
   const { network } = useNetworkContext();
   const [chainId, setChainId] = useState<number>();
+  const [provider, setProvider] = useState<providers.JsonRpcProvider | null>(
+    null,
+  );
 
   const getProvider = async () => {
     try {
       const provider = new providers.JsonRpcProvider(ethRpcUrl);
       const { chainId } = await provider.getNetwork();
       setChainId(chainId);
-      return provider;
+      setProvider(provider);
     } catch (e) {
       // Note: Added this for cases wherein eth rpc url is invalid or unreachable
       setChainId(0);
-      return {};
+      setProvider(null);
     }
   };
 
+  useEffect(() => {
+    getProvider();
+  }, [ethRpcUrl]);
+
   const client = useMemo(
-    async () => ({
-      provider: await getProvider(),
+    () => ({
+      provider,
       chainId,
     }),
     [network, chainId, ethRpcUrl],
