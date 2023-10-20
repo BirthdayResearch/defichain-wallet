f5a52e910e13cb017b81a153fc8653e85a8dd926
Harsh <harshrathi.dev@gmail.com>
2023-10-20 10:53:35 +0530

    updated token listing condition

--- mobile-app/app/screens/AppNavigator/screens/Portfolio/screens/TokenSelectionScreen.tsx
+++ mobile-app/app/screens/AppNavigator/screens/Portfolio/screens/TokenSelectionScreen.tsx
@@ -324,12 +324,8 @@ function getTokensWithBalance(
         ? new BigNumber(t.amount).minus(reservedFees).toFixed(8)
         : t.amount,
     );
-    if (
-      available.isLessThan(0) ||
-      t.id === "0" ||
-      t.id === "0_utxo" ||
-      t.id === "0_evm"
-    ) {
+
+    if (available.isLessThanOrEqualTo(0) || t.id === "0" || t.id === "0_utxo") {
       return;
     }
 
