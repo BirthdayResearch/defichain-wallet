2f9742383020a682d00a578106cf70f8b490f10f
Lyka Labrada <lykalabrada@gmail.com>
2023-10-20 10:03:37 +0800

    change delay back to 3000

--- mobile-app/cypress/e2e/functional/wallet/settings/serviceProvider.spec.ts
+++ mobile-app/cypress/e2e/functional/wallet/settings/serviceProvider.spec.ts
@@ -105,7 +105,7 @@ defichainUrlEnvs.forEach((defichainUrlEnv) => {
           "aria-disabled",
         );
         cy.getByTestID("button_submit").click().wait(3000);
-        cy.getByTestID("pin_authorize").type("000000", { delay: 1000 });
+        cy.getByTestID("pin_authorize").type("000000", { delay: 3000 });
         cy.wait(5000);
         cy.url().should("include", "app/portfolio");
         cy.getByTestID("bottom_tab_portfolio").click();
@@ -129,7 +129,7 @@ defichainUrlEnvs.forEach((defichainUrlEnv) => {
         cy.getByTestID("setting_navigate_service_provider").click();
         cy.getByTestID("DVM_edit_service_provider").click();
         cy.getByTestID("reset_button").should("exist").click().wait(3000);
-        cy.getByTestID("pin_authorize").type("000000", { delay: 1000 });
+        cy.getByTestID("pin_authorize").type("000000", { delay: 3000 });
         cy.wait(5000);
         cy.getByTestID("bottom_tab_portfolio").click();
         cy.getByTestID("header_settings").click().wait(1000);
