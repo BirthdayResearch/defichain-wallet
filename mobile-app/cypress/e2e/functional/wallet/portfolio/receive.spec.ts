1e3a248901c143f58e6e6f2f49eb6f71c1116c0a
Ramya <ramyasivan89@gmail.com>
2023-10-30 14:19:49 +0800

    chore(e2e): Receive e2e flow

--- mobile-app/cypress/e2e/functional/wallet/portfolio/receive.spec.ts
+++ mobile-app/cypress/e2e/functional/wallet/portfolio/receive.spec.ts
@@ -10,6 +10,19 @@ context("Wallet - Receive", () => {
     cy.getByTestID("receive_balance_button").click();
   });
 
+  it("should copy DVM address to clipboard when copy is clicked", () => {
+    cy.getByTestID("address_text").then(($txt: any) => {
+      const address = $txt[0].textContent;
+      cy.getByTestID("copy_button").should("exist").click();
+      cy.window().then((win) => {
+        win.navigator.clipboard.readText().then((text) => {
+          cy.log(`From clipboard: ${text}`);
+          expect(text).to.eq(address);
+        });
+      });
+    });
+  });
+
   it("should get address value and validate", () => {
     cy.getByTestID("qr_code_container").should("exist");
     cy.getByTestID("copy_button").should("exist");
@@ -24,7 +37,49 @@ context("Wallet - Receive", () => {
       cy.getByTestID("address_input").type(address);
       cy.getByTestID("button_confirm_send_continue").should(
         "not.have.attr",
-        "disabled"
+        "disabled",
+      );
+    });
+  });
+});
+
+context("Wallet - EVM Receive", () => {
+  it("should display valid address when clicked", () => {
+    cy.getByTestID("bottom_tab_portfolio").click();
+    cy.getByTestID("domain_switch").click();
+    cy.getByTestID("receive_balance_button").click();
+    cy.getByTestID("qr_code_container").should("exist");
+    cy.getByTestID("address_text").should("exist");
+    cy.getByTestID("copy_button").should("exist");
+    cy.getByTestID("share_button").should("exist");
+  });
+
+  it("should copy EVM address to clipboard when copy is clicked", () => {
+    cy.getByTestID("address_text").then(($txt: any) => {
+      const address = $txt[0].textContent;
+      cy.getByTestID("copy_button").should("exist").click();
+      cy.window().then((win) => {
+        win.navigator.clipboard.readText().then((text) => {
+          cy.log(`From clipboard: ${text}`);
+          expect(text).to.eq(address);
+        });
+      });
+    });
+  });
+
+  it("should get address value and validate", () => {
+    cy.getByTestID("address_text").then(($txt: any) => {
+      const address = $txt[0].textContent;
+      cy.go("back");
+      cy.getByTestID("domain_switch").click();
+      cy.getByTestID("dfi_total_balance_amount").contains("10.00000000");
+      cy.getByTestID("dfi_balance_card").should("exist").click();
+      cy.getByTestID("send_button").click();
+      cy.getByTestID("amount_input").clear().type("1");
+      cy.getByTestID("address_input").type(address);
+      cy.getByTestID("button_confirm_send_continue").should(
+        "not.have.attr",
+        "disabled",
       );
     });
   });
