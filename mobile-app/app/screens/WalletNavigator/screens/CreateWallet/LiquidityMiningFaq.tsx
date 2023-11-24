c2a3d41c40049c2bd5e3771d6d57d71a092ed7f0
Harsh <harshrathi.dev@gmail.com>
2023-11-24 10:06:44 +0530

    chore(ui-ux): remove bittrex from exchange list

--- mobile-app/app/screens/WalletNavigator/screens/CreateWallet/LiquidityMiningFaq.tsx
+++ mobile-app/app/screens/WalletNavigator/screens/CreateWallet/LiquidityMiningFaq.tsx
@@ -11,13 +11,13 @@ export function LiquidityMiningFaq(): JSX.Element {
     {
       title: translate(
         "components/LiquidityMiningFaq",
-        "How do I supply liquidity?"
+        "How do I supply liquidity?",
       ),
       content: [
         {
           text: translate(
             "components/LiquidityMiningFaq",
-            "After deciding which pool pair you would like to supply liquidity to, click ADD LIQUIDITY and enter the input amount for either of the tokens in the pair.\n\nOnce an amount is entered on one side of the pair, the app automatically balances the amount of the other side, based on the ratio of current liquidity in the pool."
+            "After deciding which pool pair you would like to supply liquidity to, click ADD LIQUIDITY and enter the input amount for either of the tokens in the pair.\n\nOnce an amount is entered on one side of the pair, the app automatically balances the amount of the other side, based on the ratio of current liquidity in the pool.",
           ),
           type: "paragraph",
         },
@@ -26,13 +26,13 @@ export function LiquidityMiningFaq(): JSX.Element {
     {
       title: translate(
         "components/LiquidityMiningFaq",
-        "How do I send tokens from my DFI wallet?"
+        "How do I send tokens from my DFI wallet?",
       ),
       content: [
         {
           text: translate(
             "components/LiquidityMiningFaq",
-            "Tokens on DeFiChain can only be sent to DFI addresses only. This means that you should not send directly to an exchange or a third party platform that does not support DeFiChain.\n\nSimilarly, liquidity tokens can only be sent to another DFI wallet address."
+            "Tokens on DeFiChain can only be sent to DFI addresses only. This means that you should not send directly to an exchange or a third party platform that does not support DeFiChain.\n\nSimilarly, liquidity tokens can only be sent to another DFI wallet address.",
           ),
           type: "paragraph",
         },
@@ -41,13 +41,13 @@ export function LiquidityMiningFaq(): JSX.Element {
     {
       title: translate(
         "components/LiquidityMiningFaq",
-        "What are some exchanges and platforms I can send to?"
+        "What are some exchanges and platforms I can send to?",
       ),
       content: [
         {
           text: translate(
             "components/LiquidityMiningFaq",
-            "1. Kucoin\n2. Bittrex\n3. Transak\n4. Bake\n\nView more at DeFiChain.com"
+            "1. Kucoin\n2. Huobi\n3. Transak\n4. Bake\n\nView more at DeFiChain.com",
           ),
           type: "paragraph",
         },
@@ -56,13 +56,13 @@ export function LiquidityMiningFaq(): JSX.Element {
     {
       title: translate(
         "components/LiquidityMiningFaq",
-        "Can I send liquidity pool tokens to an exchange or another entity?"
+        "Can I send liquidity pool tokens to an exchange or another entity?",
       ),
       content: [
         {
           text: translate(
             "components/LiquidityMiningFaq",
-            "Liquidity tokens can only be sent to another DFI wallet address, and not to an exchange of central entity."
+            "Liquidity tokens can only be sent to another DFI wallet address, and not to an exchange of central entity.",
           ),
           type: "paragraph",
         },
@@ -71,13 +71,13 @@ export function LiquidityMiningFaq(): JSX.Element {
     {
       title: translate(
         "components/LiquidityMiningFaq",
-        "How do I receive tokens on my DFI wallet?"
+        "How do I receive tokens on my DFI wallet?",
       ),
       content: [
         {
           text: translate(
             "components/LiquidityMiningFaq",
-            "You can receive DFI tokens into your wallet by sharing your address found in the Receive page, or by getting the sender to scan the QR code.\n\nNote that you will not be able to receive native tokens (e.g. BTC, ETH) directly into your wallet."
+            "You can receive DFI tokens into your wallet by sharing your address found in the Receive page, or by getting the sender to scan the QR code.\n\nNote that you will not be able to receive native tokens (e.g. BTC, ETH) directly into your wallet.",
           ),
           type: "paragraph",
         },
@@ -94,7 +94,7 @@ export function LiquidityMiningFaq(): JSX.Element {
       <ThemedTextV2 style={tailwind("text-base font-normal-v2 px-5")}>
         {translate(
           "components/LiquidityMiningFaq",
-          "Liquidity mining is a DeFi mechanism in which participants supply cryptocurrencies into liquidity pools, and in turn, be rewarded with fees and token based on their share of the total pool liquidity."
+          "Liquidity mining is a DeFi mechanism in which participants supply cryptocurrencies into liquidity pools, and in turn, be rewarded with fees and token based on their share of the total pool liquidity.",
         )}
       </ThemedTextV2>
 
@@ -103,7 +103,7 @@ export function LiquidityMiningFaq(): JSX.Element {
         activeSections={[0]}
         title={translate(
           "components/LiquidityMiningFaq",
-          "FREQUENTLY ASKED QUESTIONS"
+          "FREQUENTLY ASKED QUESTIONS",
         )}
         content={faqContent}
       />
