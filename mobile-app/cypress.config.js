804553b063f2cac45cec703a93ec09aa7dcbbff5
Harsh <harshrathi.dev@gmail.com>
2024-03-12 11:34:37 +0530

    added testIsolation as false

--- mobile-app/cypress.config.js
+++ mobile-app/cypress.config.js
@@ -14,6 +14,7 @@ module.exports = defineConfig({
   numTestsKeptInMemory: 0,
   chromeWebSecurity: false,
   e2e: {
+    testIsolation: false,
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
