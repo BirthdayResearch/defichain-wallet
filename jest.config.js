146fe49e638f9e49b9a1a157a14074235fce87be
Andrey Galysh <galysh.andrey@gmail.com>
2026-04-10 03:05:15 +0200

    dependent packages upgrades:
    3. `react-hook-form`
    
    - Previous version: `^7.47.0` / installed `7.51.0`
    - Current version: `^7.72.1`
    - Status: upgraded to a React 19-compatible 7.x release
    
    4. `react-number-format`
    
    - Previous version: `^5.3.3`
    - Current version: `^5.4.5`
    - Status: upgraded to a release with React 19 peer support
    
    5. `react-qr-code`
    
    - Previous version: `^2.0.12`
    - Current version: `^2.0.18`
    - Status: upgraded to a release that accepts the current React line
    
    6. `react-redux`
    
    - Previous version: `^8.1.3`
    - Current version: `^9.2.0`
    - Status: upgraded and forced through transitive dependencies using `overrides`
    - Additional fix required by the upgrade: Jest transform allowlist now includes `react-redux` and `use-sync-external-store`
    
    7. `@types/react`
    
    - Previous version: `18.2.64`
    - Current version: `^19.2.14`
    - Status: pinned in `devDependencies` and `overrides` to align with `react-native@0.79.6`

--- jest.config.js
+++ jest.config.js
@@ -24,6 +24,6 @@ module.exports = {
   coverageDirectory: "jest-coverage",
   setupFiles: ["./jest.setup.js"],
   transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@waveshq/.*|@reduxjs/toolkit|immer)",
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@waveshq/.*|@reduxjs/toolkit|immer|react-redux|use-sync-external-store)",
   ],
 };
